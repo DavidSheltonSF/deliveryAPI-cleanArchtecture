50fb8e4a08bf87aaf604919bf552362258660c6f
DavidSheltonSF <david.faria012@gmail.com>
2025-02-20 18:03:01 -0300

    test(entities/validators): Testing phone validator

---
+++ src/entities/validators/phone/phone.test.ts
@@ -0,0 +1,51 @@
+import { Phone } from "./phone"
+
+describe("Testing Phone validator", () => {
+  test("Trying to create a valid phone", () => {
+    const validPhone = '21-555875028';
+    const phoneOrError = Phone.create(validPhone);
+    const gotPhone = phoneOrError.getRight();
+
+    expect(phoneOrError.isRight()).toBeTruthy();
+    expect(validPhone).toBe(gotPhone.get());
+  });
+
+
+  test("Trying to create phone without simbols", () => {
+    const validPhone = '21555875028';
+    const phoneOrError = Phone.create(validPhone);
+    const gotPhone = phoneOrError.getRight();
+    
+    expect(phoneOrError.isRight()).toBeTruthy();
+    expect(validPhone).toBe(gotPhone.get());
+  });
+
+
+  test("Trying to create a phone with more than 12 characteres", () => {
+    let tooLongPhone = '';
+    
+    for (let i=0; i<15; i++) {
+      tooLongPhone += '8';
+    }
+
+    const phoneOrError = Phone.create(tooLongPhone);
+
+    expect(phoneOrError.isLeft()).toBeTruthy();
+  });
+
+  test("Trying to create phone with wrong format", () => {
+    let invalidPhone = '55*575577756';
+
+    const phoneOrError = Phone.create(invalidPhone);
+
+    expect(phoneOrError.isLeft()).toBeTruthy();
+  });
+
+  test("Trying to create phone with 10 digits (it should has at least 11)", () => {
+    let invalidPhone = '8848415415';
+
+    const phoneOrError = Phone.create(invalidPhone);
+
+    expect(phoneOrError.isLeft()).toBeTruthy();
+  });
+})
