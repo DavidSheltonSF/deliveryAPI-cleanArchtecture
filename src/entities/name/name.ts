bd76650e095ee4306c02a1ca34e2cef60f1e766e
DavidSheltonSF <david.faria012@gmail.com>
2025-02-20 16:27:57 -0300

    feat(entities/name/name.ts): Adding a regex to validate names

--- src/entities/name/name.ts
+++ src/entities/name/name.ts
@@ -11,10 +11,16 @@ export class Name {
 
   static validate(name: string): Boolean{
 
+    const nameRegex = /^[a-zA-ZÀ-ÿ]+(([',. -][a-zA-ZÀ-ÿ ])?[a-zA-ZÀ-ÿ]*)*$/;
+
     if (!name || name.trim().length < 2 || name.trim().length > 255) {
       return false;
     }
 
+    if(!name.match(nameRegex)) {
+      return false
+    }
+    
     return true;
   }
 
@@ -23,12 +29,10 @@ export class Name {
       return Either.left(new InvalidNameError(this.name));
     }
 
-    return Either.right(new Name(this.name));
+    return Either.right(new Name(name));
   }
 
   get(): string {
     return this.name;
   }
-
-  
 }
