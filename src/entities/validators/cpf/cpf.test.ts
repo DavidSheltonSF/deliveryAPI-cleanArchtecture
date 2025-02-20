fe8955a2bf8ba71c9f434f6d267f386c1c2426fd
DavidSheltonSF <david.faria012@gmail.com>
2025-02-20 17:53:59 -0300

    test(entities/validators): Testing cpf validator

---
+++ src/entities/validators/cpf/cpf.test.ts
@@ -0,0 +1,51 @@
+import { Cpf } from "./cpf"
+
+describe("Testing Cpf validator", () => {
+  test("Trying to create a valid cpf", () => {
+    const validCpf = '555.885.584-58';
+    const cpfOrError = Cpf.create(validCpf);
+    const gotCpf = cpfOrError.getRight();
+
+    expect(cpfOrError.isRight()).toBeTruthy();
+    expect(validCpf).toBe(gotCpf.get());
+  });
+
+
+  test("Trying to create cpf without simbols", () => {
+    const validCpf = '55588558458';
+    const cpfOrError = Cpf.create(validCpf);
+    const gotCpf = cpfOrError.getRight();
+    
+    expect(cpfOrError.isRight()).toBeTruthy();
+    expect(validCpf).toBe(gotCpf.get());
+  });
+
+
+  test("Trying to create a cpf with more than 14 characteres", () => {
+    let tooLongCpf = '';
+    
+    for (let i=0; i<256; i++) {
+      tooLongCpf += '8';
+    }
+
+    const cpfOrError = Cpf.create(tooLongCpf);
+
+    expect(cpfOrError.isLeft()).toBeTruthy();
+  });
+
+  test("Trying to create with special characteres", () => {
+    let invalidCpf = '@555.555.777-56';
+
+    const cpfOrError = Cpf.create(invalidCpf);
+
+    expect(cpfOrError.isLeft()).toBeTruthy();
+  });
+
+  test("Trying to create cpf with wrong format", () => {
+    let invalidCpf = '@555-555-777.56';
+
+    const cpfOrError = Cpf.create(invalidCpf);
+
+    expect(cpfOrError.isLeft()).toBeTruthy();
+  });
+})
