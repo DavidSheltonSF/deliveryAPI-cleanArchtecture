7549a842a0bb7a58c9361d5ab22b4a0c349a4b5e
DavidSheltonSF <david.faria012@gmail.com>
2025-02-20 16:59:21 -0300

    test(entities/validators): Testing Email validator

---
+++ src/entities/validators/email/email.test.ts
@@ -0,0 +1,35 @@
+import { Email } from "./email"
+
+describe("Testing Email validator", () => {
+  test("Trying to create a valid email", () => {
+    const validEmail = 'maria@bugmail.com';
+    const emailOrError = Email.create(validEmail);
+    const gotEmail = emailOrError.getRight();
+
+    expect(emailOrError.isRight()).toBeTruthy();
+    expect(validEmail).toBe(gotEmail.get());
+  });
+
+
+  test("Trying to create email without @", () => {
+    const invalidEmail = 'mariabugmail.com';
+    const emailOrError = Email.create(invalidEmail);
+
+    expect(emailOrError.isLeft()).toBeTruthy();
+  });
+
+
+  test("Trying to create a email with more than 255 characteres", () => {
+    let tooLongEmail = '';
+    
+    for (let i=0; i<256; i++) {
+      tooLongEmail += 'N';
+    }
+
+    tooLongEmail += '@bugmail.com';
+
+    const emailOrError = Email.create(tooLongEmail);
+
+    expect(emailOrError.isLeft()).toBeTruthy();
+  });
+})
