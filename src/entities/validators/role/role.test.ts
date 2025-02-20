395ce6c1ebfd1c40921eccd117c84e41d036d2ef
DavidSheltonSF <david.faria012@gmail.com>
2025-02-20 18:41:54 -0300

    feat(entities/validators/role): Testing role validator

---
+++ src/entities/validators/role/role.test.ts
@@ -0,0 +1,28 @@
+import { Role } from "./role"
+
+const validRoles = [
+  'admin',
+  'customer',
+  'restaurant', 
+  'owner',
+  'driver'
+]
+
+describe("Testing Role validator", () => {
+  test("Trying to create a valid roles", () => {
+    const validRole = validRoles[0];
+    const roleOrError = Role.create(validRole);
+    const gotRole = roleOrError.getRight();
+
+    expect(roleOrError.isRight()).toBeTruthy();
+    expect(validRole).toBe(gotRole.get());
+  });
+
+  test("Trying to create an invalid role", () => {
+    const invalidRole = 'invalidRole';
+    const roleOrError = Role.create(invalidRole);
+
+    expect(roleOrError.isLeft()).toBeTruthy();
+  });
+
+})
