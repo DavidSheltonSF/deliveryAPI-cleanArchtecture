2d0c30ae31f3a7fb486f5e877bb5fb28206ae9c1
DavidSheltonSF <david.faria012@gmail.com>
2025-02-24 13:59:32 -0300

    feat(entities/validators/_enums): Creating enums folder to group enums for validation and adding in it PaymentMethod and UserRole enums

---
+++ src/entities/validators/_enums/index.ts
@@ -0,0 +1,14 @@
+export enum PaymentMethod {
+  CREDIT_CARD = 'credit_card',
+  DEBIT_CARD = 'debit_card',
+  PIX_KEY = 'pix_key',
+  BANK_TRANSFER = 'bank_transfer'
+}
+
+export enum UserRole {
+  admin = 'admin',
+  customer ='customer',
+  restaurant = 'restaurant', 
+  owner = 'owner',
+  driver = 'driver'
+}
