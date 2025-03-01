5b465caffcdcaf44e98f234d1b727b17711df796
DavidSheltonSF <david.faria012@gmail.com>
2025-03-01 19:46:12 -0300

    refactor(usecases/port): Adding findUserById() method and removing exists() method from UserRepository interface

--- src/usecases/ports/user-repository.ts
+++ src/usecases/ports/user-repository.ts
@@ -3,8 +3,8 @@ import {User as UserData} from '../../entities/user';
 export interface UserRepository {
   findAllUsers: () => Promise<UserData[]>
   findUserByEmail: (email: string) => Promise<UserData>
+  findUserById: (userId: string) => Promise<UserData>
   add: (user: UserData) => Promise<void>
   remove: (userId: string) => Promise<void>
   update: (userId: string, userData: UserData) => Promise<void>
-  exists: (email: string) => Promise<boolean>
 }
