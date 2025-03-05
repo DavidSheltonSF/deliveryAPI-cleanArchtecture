dc6707fcb6a5e20141d3192b990ce99b344df873
DavidSheltonSF <david.faria012@gmail.com>
2025-03-05 10:13:59 -0300

    feat(entities/validators/_interfaces): Implemented a file to groupy all field interfaces for the entities

---
+++ src/entities/validators/_interfaces/index.ts
@@ -0,0 +1,25 @@
+export interface Address {
+  ownerEntityId: string,
+  street: string,
+  city: string,
+  state: string,
+  zipCode: string
+}
+
+export interface PaymentInfo {
+  holderName: string,
+  cardNumber?: string,
+  expiryDate?: string,
+  cvv?: string
+  pix_key?: string,
+  bankAccount?: {
+    bankName: string,
+    accountNumber: string,
+  }
+}
+
+export interface Authentication {
+  password: string,
+  salt?: { type: String, select: false },
+  sesstionToken?: { type: String, select: false }
+}
