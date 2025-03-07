787ca28af97a4de6a517d9b348364db72b1d9c9e
DavidSheltonSF <david.faria012@gmail.com>
2025-03-07 16:32:07 -0300

    feat(usecases/ports/restaurant-repository RestaurantRepository interface): Added findRestaurantByAdminId method

--- src/usecases/ports/restaurant-repository.ts
+++ src/usecases/ports/restaurant-repository.ts
@@ -3,6 +3,7 @@ import {Restaurant as RestaurantData} from '../../entities/restaurant';
 export interface RestaurantRepository {
   findAllRestaurants: () => Promise<RestaurantData[]>
   findRestaurantById: (restaurantId: string) => Promise<RestaurantData>
+  findRestaurantByAdminId: (adminId: string) => Promise<RestaurantData>
   findRestaurantByZipCode: (zipCode: string) => Promise<RestaurantData>
   add: (restaurant: RestaurantData) => Promise<void>
   remove: (restaurantId: string) => Promise<void>
