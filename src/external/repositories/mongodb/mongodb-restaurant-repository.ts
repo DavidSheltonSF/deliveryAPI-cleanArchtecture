c0b37d2b230ff33c1a6b73b1e5384ce417e44649
DavidSheltonSF <david.faria012@gmail.com>
2025-03-07 15:19:33 -0300

    feat(external/repositories/ MongodbRestaurantRepository)

---
+++ src/external/repositories/mongodb/mongodb-restaurant-repository.ts
@@ -0,0 +1,66 @@
+import { RestaurantRepository } from "usecases/ports/restaurant-repository";
+import { Restaurant as RestaurantData } from "entities/restaurant";
+import { RestaurantCast } from "../../../entities/restaurant";
+import { mongoHelper } from "./helpers/mongo-helper";
+
+export class MongodbRestaurantRepository implements RestaurantRepository {
+  
+  async findAllRestaurants (): Promise<RestaurantData[]> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+    const result = await restaurantCollection.find().toArray();
+
+    if (result){
+      const restaurants = result.map((elem) => {
+        return RestaurantCast.toRestaurant(elem);
+      });
+
+      return restaurants;
+    }
+    return [];
+  }
+
+  async findRestaurantById (restaurantId: string): Promise<RestaurantData | null> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+    // Its necessary to cast the id string into an ObjectId
+    const objId = mongoHelper.toObjectId(restaurantId);
+    const result = await restaurantCollection?.findOne({_id: objId});
+
+    if (result){
+      return RestaurantCast.toRestaurant(result);
+    }
+
+    return null;
+  }
+
+  async findRestaurantByZipCode (zipCode: string): Promise<RestaurantData | null> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+
+    const result = await restaurantCollection?.findOne({"address.zipCode": zipCode});
+
+    if (result){
+      return RestaurantCast.toRestaurant(result);
+    }
+
+    return null;
+  }
+
+  async add (restaurant: RestaurantData): Promise<void> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+    await restaurantCollection?.insertOne(restaurant);
+  }
+
+  async update (restaurantId: string, restaurantData: RestaurantData): Promise<void> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+    await restaurantCollection.updateOne(
+      {_id: mongoHelper.toObjectId(restaurantId)},
+      {$set: restaurantData}
+    );
+  }
+
+  async remove (restaurantId: string): Promise<void> {
+    const restaurantCollection = mongoHelper.getCollection('restaurants');
+    await restaurantCollection.deleteOne(
+      {_id: mongoHelper.toObjectId(restaurantId)}
+    );
+  }
+}
