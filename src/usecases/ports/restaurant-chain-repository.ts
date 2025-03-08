73b22c3ebe8fbb98d2b638c7218dce5af05bfb7d
DavidSheltonSF <david.faria012@gmail.com>
2025-03-08 19:54:13 -0300

    fix(usecases/ports/ RestaurantChainRepository): removing findRestaurantChainByZipCode method

--- src/usecases/ports/restaurant-chain-repository.ts
+++ src/usecases/ports/restaurant-chain-repository.ts
@@ -4,7 +4,6 @@ export interface RestaurantChainRepository {
   findAllRestaurantChains: () => Promise<RestaurantChainData[]>
   findRestaurantChainById: (restaurantchainId: string) => Promise<RestaurantChainData>
   findRestaurantChainByAdminId: (adminId: string) => Promise<RestaurantChainData>
-  findRestaurantChainByZipCode: (zipCode: string) => Promise<RestaurantChainData>
   add: (restaurantchain: RestaurantChainData) => Promise<void>
   remove: (restaurantchainId: string) => Promise<void>
   update: (restaurantchainId: string, restaurantchainData: RestaurantChainData) => Promise<void>
