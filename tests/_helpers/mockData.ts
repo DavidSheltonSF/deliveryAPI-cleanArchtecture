318da1eb11176b6f80fe6647e88f174a0df9e4df
DavidSheltonSF <david.faria012@gmail.com>
2025-03-16 19:36:14 -0300

    test(_helpers): Implementing MockData class

---
+++ tests/_helpers/mockData.ts
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
+import { User as UserData } from '../../src/domain/entities/user';
+import { mongoHelper } from '../../src/infraestructure/repositories/mongodb/helpers/mongo-helper';
+import { UserRole } from '../../src/domain/entities/validators/_enums';
+
+export class MockData {
+
+  static generateHexId(): string {
+    return uuidv4().replace(/-/g, "").slice(0, 24);
+  }
+
+  static mockUser(): any {
+
+  return {
+      _id: mongoHelper.toObjectId(this.generateHexId()),
+      username: faker.person.firstName(),
+      email: faker.internet.email(),
+      cpf: faker.string.numeric({length: 11}),
+      phone: faker.string.numeric({length: 11}),
+      role: faker.helpers.enumValue(UserRole),
+      address: {
+        street: faker.location.street(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zipCode: faker.location.zipCode('########')
+      },
+      authentication: {
+        password: faker.internet.password({length: 8})
+      },
+    }
+  }
+}
