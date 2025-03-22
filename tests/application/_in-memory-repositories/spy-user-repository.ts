59f6aab726bc3bb824f897dd50b3356edabf6767
DavidSheltonSF <david.faria012@gmail.com>
2025-03-22 17:24:41 -0300

    fix(tests/application/_in-memory-repositories SpyUserRepository): Modifying UserData importation and specifying the typing of foundUser variable in findUserById and findUserByEmail methods

--- tests/application/_in-memory-repositories/spy-user-repository.ts
+++ tests/application/_in-memory-repositories/spy-user-repository.ts
@@ -1,5 +1,5 @@
 import { UserRepository } from "../../../src/application/usecases/ports/user-repository";
-import { User as UserData } from "domain/entities/user";
+import { User as UserData } from "../../../src/domain/entities/user";
 
 export class SpyUserRepository implements UserRepository {
   users: UserData[] = [];
@@ -20,11 +20,11 @@ export class SpyUserRepository implements UserRepository {
     return this.users;
   }
 
-  async findUserById(id: string): Promise<UserData> {
-    let foundUser = null;
+  async findUserById(id: string): Promise<UserData | null> {
+    let foundUser: UserData | null = null;
 
     for (let i=0; i<this.users.length; i++) {
-      if (this.users[i]._id.toString() === id){
+      if (this.users[i]._id?.toString() === id){
         foundUser = this.users[i];
       }
     }
@@ -32,8 +32,8 @@ export class SpyUserRepository implements UserRepository {
     return foundUser;
   }
 
-  async findUserByEmail(email: string): Promise<UserData> {
-    let foundUser = null;
+  async findUserByEmail(email: string): Promise<UserData | null> {
+    let foundUser: UserData | null = null;
 
     for (let i=0; i<this.users.length; i++) {
       if (this.users[i].email === email){
