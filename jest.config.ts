95053439ca16028fe9e914152b52b85de223347d
DavidSheltonSF <david.faria012@gmail.com>
2025-03-22 17:10:10 -0300

    chore: Tracking some configuration files

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  // Coleção de caminhos que serão acompanhados pelo Jest
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.ts',
+    '!<rootDir>/tests/main/**',
+    '!<rootDir>/tests/**/*-ports.ts',
+    '!**/ports/**',
+    '!**/tests/**',
+    '!**/config/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+
+  // Permite o test de arquivos TypeScript
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+}
