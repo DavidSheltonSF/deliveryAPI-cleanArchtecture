80682352dfca12fa526646715a7ed9ae9ee323bc
DavidSheltonSF <david.faria012@gmail.com>
2025-03-22 17:22:01 -0300

    fix(application/usecases/ports UserRepository interface): Seting that findUserByEmail and findUserById can retur UserData or null types

--- src/application/usecases/ports/user-repository.ts
+++ src/application/usecases/ports/user-repository.ts
@@ -2,8 +2,8 @@ import {User as UserData} from '../../../domain/entities/user';
 
 export interface UserRepository {
   findAllUsers: () => Promise<UserData[]>
-  findUserByEmail: (email: string) => Promise<UserData>
-  findUserById: (userId: string) => Promise<UserData>
+  findUserByEmail: (email: string) => Promise<UserData | null>
+  findUserById: (userId: string) => Promise<UserData | null>
   add: (user: UserData) => Promise<void>
   remove: (userId: string) => Promise<void>
   update: (userId: string, userData: UserData) => Promise<void>
