7ce4cbfb4db0022aef4c7f4fe3d168077e6aa9c3
DavidSheltonSF <david.faria012@gmail.com>
2025-03-29 14:41:36 -0300

    refactor(application/usecases/ports RestaurantChainRepository interface): Defined that find operations can return RestaurantChainData or null

--- src/application/usecases/ports/restaurant-chain-repository.ts
+++ src/application/usecases/ports/restaurant-chain-repository.ts
@@ -2,8 +2,8 @@ import {RestaurantChain as RestaurantChainData} from '../../../domain/entities/r
 
 export interface RestaurantChainRepository {
   findAllRestaurantChains: () => Promise<RestaurantChainData[]>
-  findRestaurantChainById: (restaurantchainId: string) => Promise<RestaurantChainData>
-  findRestaurantChainByAdminId: (adminId: string) => Promise<RestaurantChainData>
+  findRestaurantChainById: (restaurantchainId: string) => Promise<RestaurantChainData | null>
+  findRestaurantChainByAdminId: (adminId: string) => Promise<RestaurantChainData | null>
   add: (restaurantchain: RestaurantChainData) => Promise<void>
   remove: (restaurantchainId: string) => Promise<void>
   update: (restaurantchainId: string, restaurantchainData: Omit<RestaurantChainData, '_id'>) => Promise<void>
