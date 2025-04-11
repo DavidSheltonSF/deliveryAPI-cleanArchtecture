479aa26480137b892b03fc040b2f522152a5684e
DavidSheltonSF <david.faria012@gmail.com>
2025-04-11 20:21:59 -0300

    refactor(application/usecases/ports UserRepository interface): Adding exists() method to check if a user already exists

--- src/application/usecases/ports/user-repository.ts
+++ src/application/usecases/ports/user-repository.ts
@@ -4,6 +4,7 @@ export interface UserRepository {
   findAllUsers: () => Promise<UserData[]>
   findUserByEmail: (email: string) => Promise<UserData | null>
   findUserById: (userId: string) => Promise<UserData | null>
+  exists: (email: string) => Promise<boolean>
   add: (user: UserData) => Promise<void>
   remove: (userId: string) => Promise<void>
   update: (userId: string, userData: Omit<UserData, '_id'>) => Promise<void>
