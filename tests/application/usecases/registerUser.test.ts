5aae6705c6d8064b846a268af2c02740c2b7560f
DavidSheltonSF <david.faria012@gmail.com>
2025-04-11 21:04:44 -0300

    test(application/usecases RegisterUserUseCase)

---
+++ tests/application/usecases/registerUser.test.ts
@@ -0,0 +1,28 @@
+import { SpyUserRepository } from '../_in-memory-repositories/spy-user-repository';
+import { RegisterUser } from '../../../src/application/usecases/register-user/registerUser';
+import { MockData } from '../../_helpers/mockData';
+
+describe('Testing RegisterUserUseCase', () => {
+
+  test('Should register a new user', async () => {
+    const spyUserRepository = new SpyUserRepository();
+    const registerUserUseCase = new RegisterUser(spyUserRepository);
+
+    const mockedUser = MockData.mockUser();
+
+    await registerUserUseCase.register(mockedUser);
+
+    console.log(spyUserRepository.addParams);
+
+    expect(spyUserRepository.addParams.user?.username).toBe(mockedUser.username);
+    expect(spyUserRepository.addParams.user?.email).toBe(mockedUser.email);
+    expect(spyUserRepository.addParams.user?.cpf).toBe(mockedUser.cpf);
+    expect(spyUserRepository.addParams.user?.phone).toBe(mockedUser.phone);
+    expect(spyUserRepository.addParams.user?.role).toBe(mockedUser.role);
+    expect(spyUserRepository.addParams.user?.address.city).toBe(mockedUser.address.city);
+    expect(spyUserRepository.addParams.user?.address.street).toBe(mockedUser.address.street);
+    expect(spyUserRepository.addParams.user?.address.state).toBe(mockedUser.address.state);
+    expect(spyUserRepository.addParams.user?.address.zipCode).toBe(mockedUser.address.zipCode)
+  });
+  
+})
