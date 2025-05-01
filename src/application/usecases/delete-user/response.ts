f47c11212f11fd08de7a95dab038af0d5426684f
DavidSheltonSF <david.faria012@gmail.com>
2025-05-01 17:25:09 -0300

    refactor(application/usecases/delete-user/response.ts): Remove InvalidEmailError importation and remove it from DeleteUserResponse too

--- src/application/usecases/delete-user/response.ts
+++ src/application/usecases/delete-user/response.ts
@@ -1,7 +1,5 @@
 import { Either } from "../../../shared/either";
-import { InvalidEmailError } from "../../../domain/entities/_errors";
 import { NoResultError } from "../../../application/usecases/_erros";
 import { UserProps } from "../../../domain/entities/userProps";
 
-export type DeleteUserResponse = Either<InvalidEmailError
-  | NoResultError, UserProps>;
+export type DeleteUserResponse = Either<NoResultError, UserProps>;
