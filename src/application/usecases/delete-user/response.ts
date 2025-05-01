2c4b2e936ae1e11092d08757197c05ae4de621c9
DavidSheltonSF <david.faria012@gmail.com>
2025-05-01 16:06:22 -0300

    chore(application/usecases/delete-user/response.ts): Removing unused importation

--- src/application/usecases/delete-user/response.ts
+++ src/application/usecases/delete-user/response.ts
@@ -1,6 +1,6 @@
 import { Either } from "../../../shared/either";
 import { InvalidEmailError } from "../../../domain/entities/_errors";
-import { DuplicatedDataError, NoResultError} from "../../../application/usecases/_erros";
+import { NoResultError } from "../../../application/usecases/_erros";
 import { UserProps } from "../../../domain/entities/userProps";
 
 export type DeleteUserResponse = Either<InvalidEmailError
