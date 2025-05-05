298e2cda90615a1d4969ac217e948c7ed944a50b
DavidSheltonSF <david.faria012@gmail.com>
2025-05-05 18:25:03 -0300

    test(FindAllUsers use case)

---
+++ tests/application/usecases/FindAllUsers.test.ts
@@ -0,0 +1,36 @@
+import { SpyUserRepository } from '../_in-memory-repositories/spy-user-repository';
+import { FindUser } from '../../../src/application/usecases/find-all-users/FindAllUses';
+import { MockData } from '../../_helpers/mockData';
+import { UserProps } from '../../../src/domain/entities/user-props';
+
+
+const makeSut = () => {
+  const spyUserRepository = new SpyUserRepository();
+  const findAllUsersUseCase = new FindUser(spyUserRepository);
+
+  return {
+    findAllUsersUseCase,
+    spyUserRepository
+  }
+}
+
+describe('Testing FindAllUsersUseCase', () => {
+
+  test('Should find all users', async () => {
+    const { findAllUsersUseCase, spyUserRepository } = makeSut();
+
+    const mockedUsers: UserProps[] = []
+    
+    for (let i = 0; i < 5; i++) {
+      // Adding the user in the fake dadabase manualy
+      mockedUsers.push(MockData.mockUser());
+    }
+
+    spyUserRepository.userDatabase = mockedUsers;
+
+    const response = await findAllUsersUseCase.execute();
+
+    expect(response).toEqual(mockedUsers);
+  });
+
+})
