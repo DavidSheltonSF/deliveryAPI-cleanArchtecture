ba589725d3483a06866636a0f28690588c0d24d7
DavidSheltonSF <david.faria012@gmail.com>
2025-05-05 19:00:12 -0300

    test(FindUserById use case): Test the happy path

---
+++ tests/application/usecases/FindUserById.test.ts
@@ -0,0 +1,53 @@
+import { SpyUserRepository } from '../_in-memory-repositories/spy-user-repository';
+import { FindUserById } from '../../../src/application/usecases/find-user-by-id/FindUserById';
+import { MockData } from '../../_helpers/mockData';
+import { UserProps } from '../../../src/domain/entities/user-props';
+
+
+const makeSut = () => {
+  const spyUserRepository = new SpyUserRepository();
+  const findUserByIdUseCase = new FindUserById(spyUserRepository);
+
+  return {
+    findUserByIdUseCase,
+    spyUserRepository
+  }
+}
+
+describe('Testing FindUserByIdUseCase', () => {
+
+  test('Should find a user by id correctly', async () => {
+    const { findUserByIdUseCase, spyUserRepository } = makeSut();
+
+    const mockedUser = MockData.mockUser();
+
+    const userIdStr = mockedUser._id?.toString();
+
+    if (!userIdStr) {
+      throw new Error('User ID is not defined');
+    }
+
+    spyUserRepository.userDatabase.push(mockedUser);
+
+    const response = await findUserByIdUseCase.execute(userIdStr);
+    const foundUser = response.getRight()
+
+    expect(response.isRight()).toBeTruthy();
+    expect(foundUser._id?.toString()).toEqual(userIdStr);
+    expect(foundUser.username).toEqual(mockedUser.username);
+    expect(foundUser.email).toEqual(mockedUser.email);
+    expect(foundUser.authentication.password).toEqual(mockedUser.authentication.password);
+    expect(foundUser.cpf).toEqual(mockedUser.cpf);
+    expect(foundUser.phone).toEqual(mockedUser.phone);
+    expect(foundUser.role).toEqual(mockedUser.role);
+    expect(foundUser.address?.street).toEqual(mockedUser.address?.street);
+    expect(foundUser.address?.city).toEqual(mockedUser.address?.city);
+    expect(foundUser.address?.state).toEqual(mockedUser.address?.state);
+    expect(foundUser.address?.zipCode).toEqual(mockedUser.address?.zipCode);
+    
+    // Check if the user id was inserted in the spy repository
+    expect(spyUserRepository.findUserByIdParams.id).toBe(userIdStr);
+
+  });
+
+})
