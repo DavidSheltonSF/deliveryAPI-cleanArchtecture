02a0141413c8968e3040c3ef2dd01d94e41fb91b
DavidSheltonSF <david.faria012@gmail.com>
2025-05-05 16:29:10 -0300

    refactor(application/usecases/_ports UserRepository interface): Add findUserByEmail

--- src/application/usecases/_ports/user-repository.ts
+++ src/application/usecases/_ports/user-repository.ts
@@ -3,6 +3,7 @@ import { UserProps } from '../../../domain/entities/user-props';
 export interface UserRepository {
   findAllUsers: () => Promise<UserProps[]>;
   findUserById: (userId: string) => Promise<UserProps | null>;
+  findUserByEmail: (email: string) => Promise<UserProps | null>;
   add: (user: UserProps) => Promise<void>;
   remove: (userId: string) => Promise<void>;
   update: (userId: string, userData: Omit<UserProps, '_id'>) => Promise<void>;
