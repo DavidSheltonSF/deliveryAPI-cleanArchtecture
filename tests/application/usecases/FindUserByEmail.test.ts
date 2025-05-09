58eb240b0c48f2a3fffaa01d11724a15a88f02bb
DavidSheltonSF <david.faria012@gmail.com>
2025-05-09 19:22:37 -0300

    test(application): Test FindUserByEmail use case

---
+++ tests/application/usecases/FindUserByEmail.test.ts
@@ -0,0 +1,69 @@
+import { SpyUserRepository } from '../_in-memory-repositories/spy-user-repository';
+import { FindUserByEmail } from '../../../src/application/usecases/find-user-by-email/FindUserByEmail';
+import { MockData } from '../../_helpers/mockData';
+
+
+const makeSut = () => {
+  const spyUserRepository = new SpyUserRepository();
+  const findUserByEmailUseCase = new FindUserByEmail(spyUserRepository);
+
+  return {
+    findUserByEmailUseCase,
+    spyUserRepository
+  }
+}
+
+describe('Testing FindUserByEmailUseCase', () => {
+
+  test('Should find a user by email correctly', async () => {
+    const { findUserByEmailUseCase, spyUserRepository } = makeSut();
+
+    const mockedUser = MockData.mockUser();
+
+    const userEmailStr = mockedUser.email;
+
+    if (!userEmailStr) {
+      throw new Error('User EMAIL is not defined');
+    }
+
+    spyUserRepository.userDatabase.push(mockedUser);
+
+    const foundUser = await findUserByEmailUseCase.execute(userEmailStr);
+
+    if (!foundUser) {
+      throw new Error('User not found');
+    }
+
+    expect(foundUser.username).toEqual(mockedUser.username);
+    expect(foundUser.email).toEqual(mockedUser.email);
+    expect(foundUser.authentication.password).toEqual(mockedUser.authentication.password);
+    expect(foundUser.cpf).toEqual(mockedUser.cpf);
+    expect(foundUser.phone).toEqual(mockedUser.phone);
+    expect(foundUser.role).toEqual(mockedUser.role);
+    expect(foundUser.address?.street).toEqual(mockedUser.address?.street);
+    expect(foundUser.address?.city).toEqual(mockedUser.address?.city);
+    expect(foundUser.address?.state).toEqual(mockedUser.address?.state);
+    expect(foundUser.address?.zipCode).toEqual(mockedUser.address?.zipCode);
+    
+    // Check if the user email was inserted in the spy repository
+    expect(spyUserRepository.findUserByEmailParams.email).toBe(userEmailStr);
+  });
+
+  test('Should find a user by email correctly', async () => {
+    const { findUserByEmailUseCase, spyUserRepository } = makeSut();
+
+    const mockedUser = MockData.mockUser();
+
+    if (!mockedUser.email) {
+      throw new Error('User EMAIL is not defined');
+    }
+
+    const foundUser = await findUserByEmailUseCase.execute(mockedUser.email);
+
+    expect(foundUser).toBe(null);
+    
+    // Check if the user email was inserted in the spy repository
+    expect(spyUserRepository.findUserByEmailParams.email).toBe(mockedUser.email);
+  });
+
+})
