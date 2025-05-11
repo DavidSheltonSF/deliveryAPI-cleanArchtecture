146101b35913eee51ed7af0f8963f8163de84ad8
DavidSheltonSF <david.faria012@gmail.com>
2025-05-11 13:19:14 -0300

    feat(presentaion/_errors): implement ControllerError interface and some error classes
    Implemented MissingParamError, MissingRequestBodyError and ServerError

---
+++ src/presentation/_errors/server-error.ts
@@ -0,0 +1,8 @@
+import { ControllerError } from './controller-error'
+
+export class ServerError extends Error implements ControllerError {
+  constructor () {
+    super('Internal server error');
+    this.name = 'ServerError';
+  }
+}
