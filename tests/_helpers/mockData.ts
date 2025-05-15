61593f16285c7c657d900b966c88466ddf50844c
DavidSheltonSF <david.faria012@gmail.com>
2025-05-15 00:22:32 -0300

    feat(MockData): allow setting the quantity of fake data in all mock methods, except for mockUser, which already has this functionality.

--- tests/_helpers/mockData.ts
+++ tests/_helpers/mockData.ts
@@ -17,16 +17,14 @@ export class MockData {
   }
 
   static mockUser(count: number = 1): UserProps[] {
-
     const mockedUsers: UserProps[] = [];
-
-    for (let i=0; i<count; i++){
-      const user ={
+    for (let i = 0; i < count; i++) {
+      const user = {
         _id: mongoHelper.toObjectId(this.generateHexId()),
         username: faker.person.firstName(),
         email: faker.internet.email(),
-        cpf: faker.string.numeric({length: 11}),
-        phone: faker.string.numeric({length: 11}),
+        cpf: faker.string.numeric({ length: 11 }),
+        phone: faker.string.numeric({ length: 11 }),
         role: faker.helpers.enumValue(UserRole),
         address: {
           street: faker.location.street(),
@@ -35,101 +33,128 @@ export class MockData {
           zipCode: faker.location.zipCode('########')
         },
         authentication: {
-          //password: faker.internet.password({length: 8})
           password: 'Senh4**Corret4'
         },
-      }
+      };
       mockedUsers.push(user);
-    } 
-
+    }
     return mockedUsers;
   }
-  
 
-  static mockRestaurantChain(): RestaurantChainProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      name: faker.company.name(),
-      cnpj: faker.string.numeric({length: 11}),
-      iconUrl: faker.image.url(),
-      adminId: this.generateHexId(),
-    };
+  static mockRestaurantChain(count: number = 1): RestaurantChainProps[] {
+    const mockedChains: RestaurantChainProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const chain = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        name: faker.company.name(),
+        cnpj: faker.string.numeric({ length: 11 }),
+        iconUrl: faker.image.url(),
+        adminId: this.generateHexId(),
+      };
+      mockedChains.push(chain);
+    }
+    return mockedChains;
   }
 
-  static mockRestaurant(): RestaurantProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      restaurantChainId: this.generateHexId(),
-      adminId: this.generateHexId(),
-      isOpen: faker.helpers.arrayElement([true, false]),
-      imageUrl: faker.image.url(),
-      phone: faker.string.numeric({length: 11}),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zipCode: faker.location.zipCode('########')
-      },
-    };
-  }
-
-  static mockDish(): DishProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      name: faker.food.dish(),
-      description: faker.food.description(),
-      price: faker.helpers.arrayElement([40.5, 38, 11,55, 87, 120.99]),
-      restaurantId: this.generateHexId(),
-      imageUrl: faker.image.url(),
-    };
-  }
-
-  static mockPayment(): PaymentProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      orderId: this.generateHexId(),
-      paymentMethod: faker.helpers.enumValue(PaymentMethod),
-      status: faker.helpers.arrayElement(['paid', 'pending', 'failed']),
-    };
-  }
-
-  static mockOrder(): OrderProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      customerId: this.generateHexId(),
-      restaurantId: this.generateHexId(),
-      dishes: [
-        {
-          name: faker.food.dish(),
-          description: faker.food.description(),
-          price: faker.helpers.arrayElement([40.5, 38, 11,55, 87, 120.99]),
-          imageUrl: faker.image.url(),
+  static mockRestaurant(count: number = 1): RestaurantProps[] {
+    const mockedRestaurants: RestaurantProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const restaurant = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        restaurantChainId: this.generateHexId(),
+        adminId: this.generateHexId(),
+        isOpen: faker.helpers.arrayElement([true, false]),
+        imageUrl: faker.image.url(),
+        phone: faker.string.numeric({ length: 11 }),
+        address: {
+          street: faker.location.street(),
+          city: faker.location.city(),
+          state: faker.location.state(),
+          zipCode: faker.location.zipCode('########')
         },
-        {
-          name: faker.food.dish(),
-          description: faker.food.description(),
-          price: faker.helpers.arrayElement([40.5, 38, 11,55, 87, 120.99]),
-          imageUrl: faker.image.url(),
-        }
-      ],
-      totalPrice: faker.helpers.arrayElement([300, 200, 559.5]),
-      status: faker.helpers.enumValue(OrderStatus),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zipCode: faker.location.zipCode('########')
-      },
-    };
+      };
+      mockedRestaurants.push(restaurant);
+    }
+    return mockedRestaurants;
   }
 
-    static mockDelivery(): DeliveryProps {
-    return {
-      _id: mongoHelper.toObjectId(this.generateHexId()),
-      orderId: this.generateHexId(),
-      driverId: this.generateHexId(),
-      status: faker.helpers.enumValue(DeliveryStatus),
-      timeEstimate: faker.helpers.arrayElement([60, 30, 20])
-    };
+  static mockDish(count: number = 1): DishProps[] {
+    const mockedDishes: DishProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const dish = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        name: faker.food.dish(),
+        description: faker.food.description(),
+        price: faker.helpers.arrayElement([40.5, 38, 11, 55, 87, 120.99]),
+        restaurantId: this.generateHexId(),
+        imageUrl: faker.image.url(),
+      };
+      mockedDishes.push(dish);
+    }
+    return mockedDishes;
+  }
+
+  static mockPayment(count: number = 1): PaymentProps[] {
+    const mockedPayments: PaymentProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const payment = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        orderId: this.generateHexId(),
+        paymentMethod: faker.helpers.enumValue(PaymentMethod),
+        status: faker.helpers.arrayElement(['paid', 'pending', 'failed']),
+      };
+      mockedPayments.push(payment);
+    }
+    return mockedPayments;
+  }
+
+  static mockOrder(count: number = 1): OrderProps[] {
+    const mockedOrders: OrderProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const order = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        customerId: this.generateHexId(),
+        restaurantId: this.generateHexId(),
+        dishes: [
+          {
+            name: faker.food.dish(),
+            description: faker.food.description(),
+            price: faker.helpers.arrayElement([40.5, 38, 11, 55, 87, 120.99]),
+            imageUrl: faker.image.url(),
+          },
+          {
+            name: faker.food.dish(),
+            description: faker.food.description(),
+            price: faker.helpers.arrayElement([40.5, 38, 11, 55, 87, 120.99]),
+            imageUrl: faker.image.url(),
+          }
+        ],
+        totalPrice: faker.helpers.arrayElement([300, 200, 559.5]),
+        status: faker.helpers.enumValue(OrderStatus),
+        address: {
+          street: faker.location.street(),
+          city: faker.location.city(),
+          state: faker.location.state(),
+          zipCode: faker.location.zipCode('########')
+        },
+      };
+      mockedOrders.push(order);
+    }
+    return mockedOrders;
+  }
+
+  static mockDelivery(count: number = 1): DeliveryProps[] {
+    const mockedDeliveries: DeliveryProps[] = [];
+    for (let i = 0; i < count; i++) {
+      const delivery = {
+        _id: mongoHelper.toObjectId(this.generateHexId()),
+        orderId: this.generateHexId(),
+        driverId: this.generateHexId(),
+        status: faker.helpers.enumValue(DeliveryStatus),
+        timeEstimate: faker.helpers.arrayElement([60, 30, 20]),
+      };
+      mockedDeliveries.push(delivery);
+    }
+    return mockedDeliveries;
   }
 }
