1e80005d4837b8a6b0dd76e2a35c4b45a2fcfd11
DavidSheltonSF <david.faria012@gmail.com>
2025-05-15 19:29:03 -0300

    feat(DeleteUserController)

---
+++ src/presentation/controllers/user/DeleteUserController.ts
@@ -0,0 +1,36 @@
+import { DeleteUser } from "../../../application/usecases/user/delete-user/interface";
+import { HttpRequest } from "../../_ports/http";
+import { MissingParamError } from "../../_errors/missing-param";
+import { noContent, badRequest, unprocessableEntity, serverError } from "../../_helpers/http-helper";
+import { HttpResponse } from "../../_ports/http";
+
+export class DeleteUserController {
+
+  private readonly deleteUser: DeleteUser
+
+  constructor(deleteUser: DeleteUser) {
+    this.deleteUser = deleteUser;
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    try{
+      
+      if(!request.params.id){
+        return badRequest(new MissingParamError('id'));
+      }
+
+      const response = await this.deleteUser.execute(request.params.id);
+
+      if (response.isLeft()){
+        return unprocessableEntity(response.getLeft());
+      }
+
+      return noContent();
+
+    }catch(error){
+      console.log(error);
+      return serverError('internal');
+    }
+  }
+
+}
