4e5d607eee0333d590e1b377ba21a4d40dd6e485
DavidSheltonSF <david.faria012@gmail.com>
2025-05-19 17:43:59 -0300

    test(UpdateUserUseCase): update test file to match with new changes in the class tested

--- tests/application/usecases/UpdateUser.test.ts
+++ tests/application/usecases/UpdateUser.test.ts
@@ -14,16 +14,12 @@ const makeSut = () => {
 
 describe('Testing UpdateUserUseCase', () => {
 
-  test('Should update a new user', async () => {
+  test('Should update only the username', async () => {
     const {spyUserRepository, updateUser } = makeSut()
 
     const [mockedUser] = MockData.mockUser();
 
     spyUserRepository.userDatabase.push(mockedUser);
-
-    const updatedData = {...mockedUser };
-    updatedData.username = 'updatedUsername';
-    updatedData.email = 'updated@email.com';
 
     const userIdStr = mockedUser._id?.toString();
 
@@ -31,49 +27,40 @@ describe('Testing UpdateUserUseCase', () => {
       throw new Error('User ID is not a valid string');
     } 
 
-    const response = await updateUser.execute(userIdStr, updatedData);
+    const update = {
+      username: 'updatedName'
+    }
+
+    const response = await updateUser.execute(userIdStr, update);
 
     expect(spyUserRepository.updateParams.userId).toBe(userIdStr);
-    expect(spyUserRepository.updateParams.user?.username).toBe(updatedData.username);
-    expect(spyUserRepository.updateParams.user?.email).toBe(updatedData.email);
-    expect(spyUserRepository.updateParams.user?.role).toBe(updatedData.role);
-    expect(spyUserRepository.updateParams.user?.phone).toBe(updatedData.phone);
-    expect(spyUserRepository.updateParams.user?.cpf).toBe(updatedData.cpf);
-    expect(spyUserRepository.updateParams.user?.bankInfo?.paymentInfo).toBe(updatedData.bankInfo?.paymentInfo);
-    expect(spyUserRepository.updateParams.user?.bankInfo?.paymentMethod).toBe(updatedData.bankInfo?.paymentMethod);
-    expect(spyUserRepository.updateParams.user?.authentication.password).toBe(updatedData.authentication.password);
-    expect(spyUserRepository.updateParams.user?.address?.city).toBe(updatedData.address?.city);
-    expect(spyUserRepository.updateParams.user?.address?.state).toBe(updatedData.address?.state);
-    expect(spyUserRepository.updateParams.user?.address?.street).toBe(updatedData.address?.street);
-    expect(spyUserRepository.updateParams.user?.address?.zipCode).toBe(updatedData.address?.zipCode);
-
+    expect(spyUserRepository.updateParams.userData?.username).toBe(update.username);
   });
+
 
   test('Should return DuplicatedDataError when updating email to one already associated to another user', async () => {
     const {spyUserRepository, updateUser } = makeSut()
 
 
-    const [mockedUser1] = MockData.mockUser();
-    const [mockedUser2] = MockData.mockUser();
+    const [firstUser] = MockData.mockUser();
+    const [secondUser] = MockData.mockUser();
 
-    spyUserRepository.userDatabase.push(mockedUser1);
-    spyUserRepository.userDatabase.push(mockedUser2);
+    spyUserRepository.userDatabase.push(firstUser);
+    spyUserRepository.userDatabase.push(secondUser);
 
-    // Update the email of user2 to the same email as user1
-    const updatedUser2 = {...mockedUser2 };
-    updatedUser2.username = 'updatedUsername';
-    updatedUser2.email = mockedUser1.email;
+    // Update the email of secondUser to the same email as firstUser
+    const update = {
+      email: firstUser.email
+    }
 
-    const userIdStr = updatedUser2._id?.toString();
+    const userIdStr = secondUser._id?.toString();
 
     if (!userIdStr) {
       throw new Error('User ID is not a valid string');
     } 
 
-    const response = await updateUser.execute(userIdStr, updatedUser2);
+    const response = await updateUser.execute(userIdStr, update);
 
     expect(response.isLeft()).toBeTruthy();
-
   });
-
 })
