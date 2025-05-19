ff2ae08d9e9c3101c5860b84c5bb7bda79155846
DavidSheltonSF <david.faria012@gmail.com>
2025-05-19 18:12:37 -0300

    refactor(UpdateUser interface): type userData as Partial<Omit<UserProps, _idd>>

--- src/application/usecases/user/update-user/interface.ts
+++ src/application/usecases/user/update-user/interface.ts
@@ -2,5 +2,5 @@ import { UserProps} from "../../../../domain/entities/user-props";
 import { UpdateUserResponse } from "./response";
 
 export interface UpdateUser {
-  execute: (id: string, userData: Omit<UserProps, "_id">) => Promise<UpdateUserResponse>;
+  execute: (id: string, userData: Partial<Omit<UserProps, "_id">>) => Promise<UpdateUserResponse>;
 }
