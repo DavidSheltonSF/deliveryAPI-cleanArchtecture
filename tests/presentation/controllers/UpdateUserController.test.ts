a1bebd92e99a3b2a050bf659506c4023894528aa
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 00:12:13 -0300

    test(UpdateUserController): add tests for handling user updates with various scenarios

---
+++ tests/presentation/controllers/UpdateUserController.test.ts
@@ -0,0 +1,104 @@
+import { SpyUserRepository } from '../../application/_in-memory-repositories/spy-user-repository';
+import { UpdateUserUseCase as UpdateUserUseCase } from '../../../src/application/usecases/user/update-user/UpdateUserUseCase';
+import { UpdateUserController } from '../../../src/presentation/controllers/user/UpdateUserController';
+import { MockData } from '../../_helpers/mockData';
+
+
+const makeSut = () => {
+  const spyUserRepository = new SpyUserRepository();
+  const updateUserUseCase = new UpdateUserUseCase(spyUserRepository);
+  const updateUserController = new UpdateUserController(updateUserUseCase);
+
+  return {spyUserRepository, updateUserController};
+}
+
+describe('Testing UpdateUseController', () => {
+
+  test('Should return 200 OK for good request with correct data', async () => {
+    const { spyUserRepository, updateUserController } = makeSut();
+
+    const [mockedUser] = MockData.mockUser();
+
+    spyUserRepository.userDatabase.push(mockedUser);
+
+    const request = {
+      body: {
+        id: mockedUser._id,
+        userData: {
+          username: "Updatetname",
+          email: "updated@bugmail.com"
+        }
+      }
+    }
+
+    const response = await updateUserController.handle(request);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Should return 200 if update an email for the same email', async () => {
+    const { spyUserRepository, updateUserController } = makeSut();
+
+    const [mockedUser] = MockData.mockUser();
+
+    spyUserRepository.userDatabase.push(mockedUser);
+
+    const request = {
+      body: {
+        id: mockedUser._id,
+        userData: {
+          username: "Updatetname",
+          email: mockedUser.email
+        }
+      }
+    }
+
+    const response = await updateUserController.handle(request);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Should return 400 BAD REQUEST if some param is invalid', async () => {
+    const { spyUserRepository, updateUserController } = makeSut();
+
+    const [mockedUser] = MockData.mockUser();
+
+    spyUserRepository.userDatabase.push(mockedUser);
+
+    const request = {
+      body: {
+        id: mockedUser._id,
+        userData: {
+          invalidParam: "invaalid",
+          email: "updated@bugmail.com"
+        }
+      }
+    }
+
+    const response = await updateUserController.handle(request);
+
+    expect(response.statusCode).toBe(400);
+  });
+
+
+  test('Should return 422 UNPROCESSABLE ENTITY if user does not exist', async () => {
+    const updateUserController = makeSut().updateUserController;
+
+    const [mockedUser] = MockData.mockUser();
+    const {_id, address, ...userWithoutId} = mockedUser;
+
+    const request = {
+      body: {
+        id: mockedUser._id,
+        userData: {
+          userame: "Updatetname",
+          email: "updated@bugmail.com"
+        }
+      }
+    }
+
+    const response = await updateUserController.handle(request);
+
+    expect(response.statusCode).toBe(400);
+  });
+})
