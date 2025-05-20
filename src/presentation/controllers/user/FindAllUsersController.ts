2932568e0372edce19cad7719aa863245227f493
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 18:23:56 -0300

    refactor(FindAllUsersController): remove unused imports for cleaner code

--- src/presentation/controllers/user/FindAllUsersController.ts
+++ src/presentation/controllers/user/FindAllUsersController.ts
@@ -1,6 +1,6 @@
 import { FindAllUsers } from "../../../application/usecases/user/find-all-users/interface"
 import { HttpRequest } from "../../_ports/http";
-import { ok, badRequest, unprocessableEntity, serverError } from "../../_helpers/http-helper";
+import { ok, serverError } from "../../_helpers/http-helper";
 import { HttpResponse } from "../../_ports/http";
 
 export class FindAllUsersController {
