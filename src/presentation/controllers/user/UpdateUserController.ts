4a3f21088f69f2d747e7af9bfabf632f509fefd0
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 00:11:37 -0300

    feat(controller): implement UpdateUserController for handling user updates

---
+++ src/presentation/controllers/user/UpdateUserController.ts
@@ -0,0 +1,53 @@
+import { UpdateUser } from "../../../application/usecases/user/update-user/interface";
+import { HttpRequest } from "../../_ports/http";
+import { MissingParamError } from "../../_errors/missing-param";
+import { InvalidParamError } from "../../_errors/invalid-param"
+import { ok, badRequest, unprocessableEntity, serverError } from "../../_helpers/http-helper";
+import { HttpResponse } from "../../_ports/http";
+import { UserProps } from "../../../domain/entities/user-props";
+
+export class UpdateUserController {
+
+  private readonly updateUser: UpdateUser
+
+  constructor(updateUser: UpdateUser) {
+    this.updateUser = updateUser;
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    try{
+
+      const {id, userData} = request.body;
+
+      if (!id){
+        return badRequest(new MissingParamError('id'));
+      }
+
+      if (!userData){
+        return badRequest(new MissingParamError('userData'));
+      }
+
+      const userFields = ["username", "email", "cpf", "phone", "role", "address", "authentication", "bankInfo"];
+
+      for (const key of Object.keys(userData)){
+        if (!userFields.includes(key)){
+          return badRequest(new InvalidParamError(key));
+        }
+      }
+
+
+      const response = await this.updateUser.execute(id, userData);
+
+      if (response.isLeft()){
+        return unprocessableEntity(response.getLeft());
+      }
+
+      return ok(response);
+
+    }catch(error){
+      console.log(error);
+      return serverError('internal');
+    }
+  }
+
+}
