5b4af834f272a53c921ed1454d233805d238f303
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 00:00:48 -0300

    refactor(UpdateUserUseCase): refactor(UpdateUserUseCase): streamline user data validation by utilizing User.createPartial method

--- src/application/usecases/user/update-user/UpdateUserUseCase.ts
+++ src/application/usecases/user/update-user/UpdateUserUseCase.ts
@@ -1,6 +1,4 @@
-import { 
-  Name, Email, Cpf, Phone, Role, Address, Authentication, BankInfo 
-} from "../../../../domain/entities/validation";
+import { User } from "../../../../domain/entities/validation";
 import { UserProps } from "../../../../domain/entities/user-props";
 import { UserRepository } from "../../../_ports/user-repository";
 import { UpdateUser } from "./interface";
@@ -17,36 +15,22 @@ export class UpdateUserUseCase implements UpdateUser {
 
   async execute(id: string, userData: Partial<Omit<UserProps, "_id">>): Promise<UpdateUserResponse> {
 
-    const validators = {
-      username: (username: string) => Name.create(username),
-      email: (email: string) => Email.create(email),
-      cpf: (cpf: string) => Cpf.create(cpf),
-      phone: (phone: string) => Phone.create(phone),
-      role: (role: string) => Role.create(role),
-      address: (address: any) => Address.create(address),
-      authentication: (authentication: any) => Authentication.create(authentication),
-      bankInfo: (bankInfo: any) => BankInfo.create(bankInfo),
+    const userOrError = User.createPartial(userData);
+
+    if(userOrError.isLeft()){
+      return Either.left(userOrError.getLeft());
     }
-
-    // Validate each field in userData
-    for (const [key, value] of Object.entries(userData)) {
-
-      const fieldOrError = validators[key](value);
-
-      if(fieldOrError.isLeft()){
-        return Either.left(fieldOrError.getLeft());
-      }
-    } 
 
     const existingUser = await this.userRepository.findUserById(id);
 
     if(!existingUser){
-      return Either.left(new NoResultError(`User with email ${userData.email} does not existis.`))
+      return Either.left(new NoResultError(`User with id ${id} does not exists.`))
     }
 
     // Check if the email was updated, then check if the new email is already in use
-    if (existingUser.email != userData.email){
-      if (await this.userRepository.findUserByEmail(userData.email)){
+    if (userData.email && existingUser.email != userData.email){
+      const userWithEmail = await this.userRepository.findUserByEmail(userData.email);
+      if (userWithEmail && userWithEmail._id.toString() !== id){
         return Either.left(new DuplicatedDataError(`The email ${userData.email} already associated with another user.`));
       }
     }
