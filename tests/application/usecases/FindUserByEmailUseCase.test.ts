e9a0949be29d8f8ff456b5500195ae49bb5f2166
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 21:29:17 -0300

    test(FindUserByEmailUseCase): enhance tests to validate error handling for invalid emails

--- tests/application/usecases/FindUserByEmailUseCase.test.ts
+++ tests/application/usecases/FindUserByEmailUseCase.test.ts
@@ -1,6 +1,7 @@
 import { SpyUserRepository } from '../_in-memory-repositories/spy-user-repository';
 import { FindUserByEmailUseCase } from '../../../src/application/usecases/user/find-user-by-email/FindUserByEmailUseCase';
 import { MockData } from '../../_helpers/mockData';
+import { InvalidEmailError } from '../../../src/domain/entities/_errors';
 
 
 const makeSut = () => {
@@ -28,7 +29,9 @@ describe('Testing FindUserByEmailUseCase', () => {
 
     spyUserRepository.userDatabase.push(mockedUser);
 
-    const foundUser = await findUserByEmailUseCase.execute(userEmailStr);
+    const response = await findUserByEmailUseCase.execute(userEmailStr);
+
+    const foundUser = response.getRight()
 
     if (!foundUser) {
       throw new Error('User not found');
@@ -49,7 +52,7 @@ describe('Testing FindUserByEmailUseCase', () => {
     expect(spyUserRepository.findUserByEmailParams.email).toBe(userEmailStr);
   });
 
-  test('Should find a user by email correctly', async () => {
+  test('should return null if the user was not found', async () => {
     const { findUserByEmailUseCase, spyUserRepository } = makeSut();
 
     const [mockedUser] = MockData.mockUser();
@@ -58,12 +61,23 @@ describe('Testing FindUserByEmailUseCase', () => {
       throw new Error('User EMAIL is not defined');
     }
 
-    const foundUser = await findUserByEmailUseCase.execute(mockedUser.email);
+    const response = await findUserByEmailUseCase.execute(mockedUser.email);
+
+    const foundUser = response.getRight();
 
     expect(foundUser).toBe(null);
     
     // Check if the user email was inserted in the spy repository
     expect(spyUserRepository.findUserByEmailParams.email).toBe(mockedUser.email);
+  });
+
+  test('should return an error when an invalid email was provided', async () => {
+    const { findUserByEmailUseCase, spyUserRepository } = makeSut();
+
+    const foundUser = await findUserByEmailUseCase.execute('invalidEmail');
+
+    expect(foundUser.isLeft()).toBeTruthy();
+    expect(foundUser.getLeft()).toBeInstanceOf(InvalidEmailError);
   });
 
 })
