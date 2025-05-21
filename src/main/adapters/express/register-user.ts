1857f5cafde84d125c3596cb8e7debc8bd6edeb3
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 22:17:14 -0300

    feat(RegisterUserRoute): implement adapter for RegisterUserController

---
+++ src/main/adapters/express/register-user.ts
@@ -0,0 +1,16 @@
+import { RegisterUserController } from "../../../presentation/controllers/user/RegisterUserController";
+import { Request, Response } from "express";
+import { HttpRequest, HttpResponse } from "../../../presentation/_ports/http";
+
+export const adaptRegisterUserRoute =  (controller: RegisterUserController) => {
+  return async (req: Request, res: Response) => {
+  
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+  
+    const response: HttpResponse = await controller.handle(httpRequest);
+
+    res.status(response.statusCode).json(response.body);
+  }
+}
