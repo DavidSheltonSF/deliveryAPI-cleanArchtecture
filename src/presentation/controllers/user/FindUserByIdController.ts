b55c97beb8313d5fa95bce3911de024869f19f02
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 21:19:13 -0300

    feat(FindUserByIdController): implement FindUserByIdController with error handling and response management

---
+++ src/presentation/controllers/user/FindUserByIdController.ts
@@ -0,0 +1,47 @@
+import { FindUserById } from "../../../application/usecases/user/find-user-by-id/interface"
+import { HttpRequest } from "../../_ports/http";
+import { ok, notFound, badRequest, serverError, unprocessableEntity } from "../../_helpers/http-helper";
+import { HttpResponse } from "../../_ports/http";
+import { MissingParamError } from "../../_errors";
+import { InvalidIDError } from "../../../domain/entities/_errors/invalid-id";
+
+export class FindUserByIdController {
+
+  private readonly findUserById: FindUserById
+
+  constructor(findUserById: FindUserById) {
+    this.findUserById = findUserById;
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    try{
+
+      const { id } = request.params;
+
+      if(!id){
+        return badRequest(new MissingParamError("id"))
+      }
+
+      const response = await this.findUserById.execute(id);
+
+      if (response.isLeft()){
+        return unprocessableEntity(new InvalidIDError(id));
+      }
+
+      const user = response.getRight();
+
+      if (!user){
+        return notFound({
+          message: "User not found"
+        })
+      }
+
+      return ok(user);
+
+    }catch(error){
+      console.log(error);
+      return serverError('internal');
+    }
+  }
+
+}
