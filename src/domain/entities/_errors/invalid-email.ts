c35f627cb691f688e39a0beca24f639c4bcd8ee0
DavidSheltonSF <david.faria012@gmail.com>
2025-05-20 21:37:51 -0300

    fix(InvalidEmailError): update constructor parameter from 'name' to 'email' for clarity

--- src/domain/entities/_errors/invalid-email.ts
+++ src/domain/entities/_errors/invalid-email.ts
@@ -1,8 +1,8 @@
 import { DomainError } from './domain-error';
 
 export class InvalidEmailError extends Error implements DomainError {
-  constructor (name: string) {
-    super(`The email ${name} is invalid`);
+  constructor (email: string) {
+    super(`The email ${email} is invalid`);
     this.name = 'InvalidEmailError';
   }
 }
