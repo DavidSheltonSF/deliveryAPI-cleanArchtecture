46255bbe2b149efee9bedde569d3e26b9e2cc9b1
DavidSheltonSF <david.faria012@gmail.com>
2025-05-25 15:45:12 -0300

    refactor(routes): update import and usage of routeAdapter in register-user route

--- src/main/routes/user/register-user.ts
+++ src/main/routes/user/register-user.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { adaptRegisterUserRoute } from "../../adapter/register-user";
+import { routeAdapter } from "../../adapter/routeAdapter";
 import { makeRegisterUserController } from "../../factories/mongodb/user/make-register-user";
 import { pathName } from "./path-name";
 
 export default (router: Router): void => {
-  router.post(pathName, adaptRegisterUserRoute(makeRegisterUserController()))
+  router.post(pathName, routeAdapter(makeRegisterUserController()))
 }
