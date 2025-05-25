359234cacacc5dc0bd208cce70dab512fcc70e35
DavidSheltonSF <david.faria012@gmail.com>
2025-05-25 16:50:07 -0300

    feat(errors): enhance MissingParamError to accept multiple parameters

--- src/presentation/_errors/missing-param.ts
+++ src/presentation/_errors/missing-param.ts
@@ -1,8 +1,18 @@
 import { ControllerError } from './controller-error'
 
 export class MissingParamError extends Error implements ControllerError {
-  constructor (paramName: string) {
-    super(`Missing param: ${paramName}`);
+  constructor (params: string[]) {
+    let paramsStr = "";
+   
+    params.forEach((param, i) => {
+      paramsStr+=param;
+      
+      if(i+1 < params.length){
+        paramsStr + ', '
+      }
+    })
+
+    super(`Missing: ${paramsStr}`);
     this.name = 'MissingParamError';
   }
 }
