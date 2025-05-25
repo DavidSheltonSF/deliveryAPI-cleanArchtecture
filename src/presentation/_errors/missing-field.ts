88f40c65bba84e9f750986abbafb6598f938e6c3
DavidSheltonSF <david.faria012@gmail.com>
2025-05-25 16:47:44 -0300

    feat(errors): add MissingFieldError class for handling missing fields

---
+++ src/presentation/_errors/missing-field.ts
@@ -0,0 +1,18 @@
+import { ControllerError } from './controller-error'
+
+export class MissingFieldError extends Error implements ControllerError {
+  constructor (fields: string[]) {
+    let fieldsStr = "";
+   
+    fields.forEach((fields, i) => {
+      fieldsStr+=fields;
+      
+      if(i+1 < fields.length){
+        fieldsStr + ', '
+      }
+    })
+
+    super(`Missing: ${fieldsStr}`);
+    this.name = 'MissingFieldError';
+  }
+}
