a5fe6f7024efc5f5dd3f5f405e3f9d748d17403e
DavidSheltonSF <david.faria012@gmail.com>
2025-05-25 15:43:23 -0300

    chore(adapter): Rename adapter/regiter-user.ts to adapter/routeAdapter.ts

--- src/main/adapter/routeAdapter.ts
+++ src/main/adapter/routeAdapter.ts
@@ -2,7 +2,7 @@ import { Controller } from "../../presentation/controllers/Controller";
 import { Request, Response } from "express";
 import { HttpRequest, HttpResponse } from "../../presentation/_ports/http";
 
-export const adaptRegisterUserRoute =  (controller: Controller) => {
+export const routeAdapter =  (controller: Controller) => {
   return async (req: Request, res: Response) => {
   
     const httpRequest: HttpRequest = {
