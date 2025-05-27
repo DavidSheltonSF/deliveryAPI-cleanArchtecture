854f3eaf7df8d43bc06b91833fae9a6888a05a7f
DavidSheltonSF <david.faria012@gmail.com>
2025-05-27 19:04:16 -0300

    feat(routes): implement UserRoutes class to configure user-related routes

---
+++ src/main/routes/UserRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { routeAdapter } from "../adapter/routeAdapter";
+import { UserControllerFactory } from "../factories/mongodb/user/UserControllerFactory";
+
+export class UserRoutes {
+  private static pathName = "/users";
+
+  static configureRoutes(router: Router): void {
+    router.post(this.pathName, routeAdapter(UserControllerFactory.makeRegisterUserController()));
+    router.put(this.pathName + "/:id", routeAdapter(UserControllerFactory.makeUpdateUserController()));
+    router.delete(this.pathName, routeAdapter(UserControllerFactory.makeDeleteUserController()));
+    router.get(this.pathName, routeAdapter(UserControllerFactory.makeFindAllUsersController()));
+    router.get(this.pathName + "/id/:id", routeAdapter(UserControllerFactory.makeFindUserByIdController()));
+    router.get(this.pathName + "/email/:email", routeAdapter(UserControllerFactory.makeFindUserByEmailController()));
+  }
+}
