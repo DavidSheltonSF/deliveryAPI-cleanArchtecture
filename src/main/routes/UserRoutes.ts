b6e6060b1020a62bc54bd4f14f6073cb3effb873
DavidSheltonSF <david.faria012@gmail.com>
2025-05-27 19:23:09 -0300

    fix(routes): correct delete user route to include user ID in path

--- src/main/routes/UserRoutes.ts
+++ src/main/routes/UserRoutes.ts
@@ -8,7 +8,7 @@ export class UserRoutes {
   static configureRoutes(router: Router): void {
     router.post(this.pathName, routeAdapter(UserControllerFactory.makeRegisterUserController()));
     router.put(this.pathName + "/:id", routeAdapter(UserControllerFactory.makeUpdateUserController()));
-    router.delete(this.pathName, routeAdapter(UserControllerFactory.makeDeleteUserController()));
+    router.delete(this.pathName + "/:id", routeAdapter(UserControllerFactory.makeDeleteUserController()));
     router.get(this.pathName, routeAdapter(UserControllerFactory.makeFindAllUsersController()));
     router.get(this.pathName + "/id/:id", routeAdapter(UserControllerFactory.makeFindUserByIdController()));
     router.get(this.pathName + "/email/:email", routeAdapter(UserControllerFactory.makeFindUserByEmailController()));
