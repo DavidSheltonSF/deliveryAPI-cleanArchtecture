c80368cf3b32872b8dddee22a8a38a2bcf2f09e9
DavidSheltonSF <david.faria012@gmail.com>
2025-06-07 21:22:59 -0300

    style(domain/entities/validation/_enums): format file with prettier

--- src/domain/entities/validation/_enums/index.ts
+++ src/domain/entities/validation/_enums/index.ts
@@ -2,21 +2,21 @@ export enum PaymentMethod {
   CREDIT_CARD = 'credit_card',
   DEBIT_CARD = 'debit_card',
   PIX_KEY = 'pix_key',
-  BANK_TRANSFER = 'bank_transfer'
+  BANK_TRANSFER = 'bank_transfer',
 }
 
 export enum UserRole {
   admin = 'admin',
-  customer ='customer',
-  restaurant_admin = 'restaurant_admin', 
+  customer = 'customer',
+  restaurant_admin = 'restaurant_admin',
   restaurant_chain_admin = 'restaurant_chain_admin',
-  driver = 'driver'
+  driver = 'driver',
 }
 
 export enum PaymentStatus {
   PAYD = 'paid',
   PENDING = 'pending',
-  FAILED = 'failed'
+  FAILED = 'failed',
 }
 
 export enum OrderStatus {
@@ -29,5 +29,5 @@ export enum OrderStatus {
 export enum DeliveryStatus {
   ASSIGNED = 'assigned',
   ON_THE_WAY = 'on_the_way',
-  DELIVERED = 'delivered'
+  DELIVERED = 'delivered',
 }
