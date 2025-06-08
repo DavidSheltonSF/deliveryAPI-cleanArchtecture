300807126d85403fc6a4aaa54c4d91da58ba716c
DavidSheltonSF <david.faria012@gmail.com>
2025-06-08 15:02:55 -0300

    style(domain/entities/validation/_interfaces): fix formatting by replacing commas with semicolons

--- src/domain/entities/validation/_interfaces/index.ts
+++ src/domain/entities/validation/_interfaces/index.ts
@@ -1,29 +1,29 @@
 export interface AddressProps {
-  street: string,
-  city: string,
-  state: string,
-  zipCode: string
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
 }
 
 export interface PaymentInfoProps {
-  holderName: string,
-  cardNumber?: string,
-  expiryDate?: string,
-  cvv?: string
-  pix_key?: string,
+  holderName: string;
+  cardNumber?: string;
+  expiryDate?: string;
+  cvv?: string;
+  pix_key?: string;
   bankAccount?: {
-    bankName: string,
-    accountNumber: string,
-  }
+    bankName: string;
+    accountNumber: string;
+  };
 }
 
 export interface AuthenticationProps {
-  password: string,
-  salt?: { type: String, select: false },
-  sesstionToken?: { type: String, select: false }
+  password: string;
+  salt?: { type: String; select: false };
+  sesstionToken?: { type: String; select: false };
 }
 
 export interface BankInfoProps {
-  paymentMethod: string,
-  paymentInfo: PaymentInfoProps
+  paymentMethod: string;
+  paymentInfo: PaymentInfoProps;
 }
