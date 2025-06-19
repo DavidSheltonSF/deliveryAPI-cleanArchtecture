28a50bf7c37775538b60958ae7e2d635f4cdc146
DavidSheltonSF <david.faria012@gmail.com>
2025-06-19 17:45:07 -0300

    refactor(SpyUserRepository tests): remove unused tests and update user addition test to use CustomerDTO

--- tests/application/_in-memory-repositories/tests/spy-user-repository.test.ts
+++ tests/application/_in-memory-repositories/tests/spy-user-repository.test.ts
@@ -4,54 +4,12 @@ import { MockData } from '../../../_helpers/mockData'
 
 describe('Testing SpyUserRepository', () => {
 
-  test('Should return all users in the FAKE database', async () => {
-
-    const spyUserRepository = new SpyUserRepository();
-
-    const allUsers = await spyUserRepository.findAllUsers();
-    
-    expect(allUsers).toBeTruthy();
-  });
-
-  test('Should find a user by id', async () => {
-
-    const spyUserRepository = new SpyUserRepository();
-
-    const [mockedUser] = MockData.mockUser();
-
-    spyUserRepository.userDatabase.push(mockedUser);
-
-    const userIdStr = mockedUser.id?.toString();
-
-    if(!userIdStr){
-      throw Error('User id is undefined');
-    }
-
-    const foundUser = await spyUserRepository.findUserById(userIdStr);
-    
-    expect(spyUserRepository.findUserByIdParams.id).toEqual(userIdStr);
-    expect(foundUser?.id).toBe(mockedUser.id);
-  });
-  
-  test('Should find a user by email', async () => {
-
-    const spyUserRepository = new SpyUserRepository();
-
-    const [mockedUser] = MockData.mockUser();
-
-    spyUserRepository.userDatabase.push(mockedUser);
-
-    const foundUser = await spyUserRepository.findUserByEmail(mockedUser.email);
-    
-    expect(spyUserRepository.findUserByEmailParams.email).toEqual(mockedUser.email);
-    expect(foundUser?.email).toBe(mockedUser.email);
-  });
 
   test('Should add a new', async () => {
 
     const spyUserRepository = new SpyUserRepository();
 
-    const [mokedUser] = MockData.mockUser();
+    const [mokedUser] = MockData.mockCustomerDTO();
 
     await spyUserRepository.add(mokedUser);
 
@@ -75,73 +33,6 @@ describe('Testing SpyUserRepository', () => {
       .toBe(mokedUser.address?.zipCode);
     expect(userInserted?.authentication?.password)
       .toBe(mokedUser.authentication.password);
-  });
-
-
-  test('Should update user by id', async () => {
-
-    const spyUserRepository = new SpyUserRepository();
-
-    const updatedData = {
-      id: null,
-      username: 'username-updated',
-      email: 'email@bugmail.com-updated',
-      cpf: '88888888888-updated',
-      phone: '21555777777-updated',
-      role: 'admin-updated',
-      address: {
-        street: 'test streed-updated',
-        city: 'Belford Roxo-updated',
-        state: 'Rio de Janeiro-updated',
-        zipCode: '22222220-updated'
-      },
-      authentication: {
-        password: 'lOk*#gnOs15'
-      },
-    }
-
-    const fakeUserId = MockData.generateHexId();
-
-    await spyUserRepository.update(fakeUserId, updatedData);
-
-    const userUpdatedId = spyUserRepository.updateParams.userId
-    const updatedUser = spyUserRepository.updateParams.userData;
-
-    expect(userUpdatedId?.toString())
-      .toBe(fakeUserId);
-    
-    expect(updatedUser?.username)
-      .toBe(updatedData.username);
-    expect(updatedUser?.email)
-      .toBe(updatedData.email);
-    expect(updatedUser?.cpf)
-      .toBe(updatedData.cpf);
-    expect(updatedUser?.phone)
-      .toBe(updatedData.phone);
-    expect(updatedUser?.address?.street)
-      .toBe(updatedData.address.street);
-    expect(updatedUser?.address?.city)
-      .toBe(updatedData.address.city);
-      expect(updatedUser?.address?.state)
-      .toBe(updatedData.address.state);
-    expect(updatedUser?.address?.zipCode)
-      .toBe(updatedData.address.zipCode);
-    expect(updatedUser?.authentication?.password)
-      .toBe(updatedData.authentication.password);
-  });
-  
-  test('Should remove user by id', async () => {
-
-    const spyUserRepository = new SpyUserRepository();
-
-    const fakeUserId = MockData.generateHexId();
-
-    await spyUserRepository.remove(fakeUserId);
-
-    const removedUserId = spyUserRepository.removeParams.userId;
-    
-    expect(removedUserId)
-      .toBe(fakeUserId);
   });
 
 });
