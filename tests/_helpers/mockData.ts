403c4338b40f453dc5f1a407bc6d311484616a42
DavidSheltonSF <david.faria012@gmail.com>
2025-06-19 17:42:52 -0300

    refactor(MockData): rename mockUser to mockCustomerDTO and make it mock a CustomerDTO

--- tests/_helpers/mockData.ts
+++ tests/_helpers/mockData.ts
@@ -1,37 +1,34 @@
 import { faker } from '@faker-js/faker';
 import { v4 as uuidv4 } from 'uuid';
-import { UserProps } from '../../src/domain/entities/user-props';
+import {CustomerDTO} from "../../src/presentation/dtos/custumer-dto"
 import { UserRole } from '../../src/domain/entities/validation/_enums';
 
 export class MockData {
-
   static generateHexId(): string {
-    return uuidv4().replace(/-/g, "").slice(0, 24);
+    return uuidv4().replace(/-/g, '').slice(0, 24);
   }
 
-  static mockUser(count: number = 1): UserProps[] {
-    const mockedUsers: UserProps[] = [];
+  static mockCustomerDTO(count: number = 1): CustomerDTO[] {
+    const mockedUsers: CustomerDTO[] = [];
     for (let i = 0; i < count; i++) {
       const user = {
-        id: this.generateHexId(),
         username: faker.person.firstName(),
         email: faker.internet.email(),
         cpf: faker.string.numeric({ length: 11 }),
         phone: faker.string.numeric({ length: 11 }),
-        role: faker.helpers.enumValue(UserRole),
+        role: "customer",
         address: {
           street: faker.location.street(),
           city: faker.location.city(),
           state: faker.location.state(),
-          zipCode: faker.location.zipCode('########')
+          zipCode: faker.location.zipCode('########'),
         },
         authentication: {
-          password: 'Senh4**Corret4'
+          password: 'Senh4**Corret4',
         },
       };
       mockedUsers.push(user);
     }
     return mockedUsers;
   }
-
 }
