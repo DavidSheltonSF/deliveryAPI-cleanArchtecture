2662a3b310003a644bf934acc033548103b6a891
DavidSheltonSF <david.faria012@gmail.com>
2025-06-19 22:38:34 -0300

    refactor(factory): update UserControllerFactory to use RegisterCustomerUseCase and MongodbCustomerRepository

--- src/main/factories/mongodb/user/UserControllerFactory.ts
+++ src/main/factories/mongodb/user/UserControllerFactory.ts
@@ -1,16 +1,16 @@
-import { RegisterUserUseCase } from '../../../../application/usecases/user/register-user/RegisterUserUseCase';
-import { MongodbUserRepository } from '../../../../infrastructure/repositories/mongodb/mongodb-user-repository';
+import { RegisterCustomerUseCase } from '../../../../application/usecases/user/register-user/RegisterCustomerUseCase';
+import { MongodbCustomerRepository } from '../../../../infrastructure/repositories/mongodb/mongodb-customer-repository';
 import { RegisterCustomerController } from '../../../../presentation/controllers/customer/RegisterCustomerController';
 
 export class UserControllerFactory {
-  private static userRepository = new MongodbUserRepository();
+  private static userRepository = new MongodbCustomerRepository();
 
   private constructor() {
     throw new Error('This class is static and cannot be instantied.');
   }
 
   static makeRegisterCustomerController(): RegisterCustomerController {
-    const useCase = new RegisterUserUseCase(this.userRepository);
+    const useCase = new RegisterCustomerUseCase(this.userRepository);
     return new RegisterCustomerController(useCase);
   }
 }
