f9ded48f2976be3a223e43fdc85ff4dfe3ed7115
DavidSheltonSF <david.faria012@gmail.com>
2025-06-21 16:21:21 -0300

    fix(BcryptHasher): ensure await is used for bcrypt hash and compare methods

--- src/infrastructure/cryptography/BcryptHasher.ts
+++ src/infrastructure/cryptography/BcryptHasher.ts
@@ -6,10 +6,10 @@ export class BcryptHasher implements Hasher, Comparer {
   constructor(private readonly salt: number = 12) {}
 
   async hash(raw: string): Promise<string> {
-    return bcrypt.hash(raw, this.salt);
+    return await bcrypt.hash(raw, this.salt);
   }
 
   async compare(raw: string, hash: string): Promise<boolean>{
-    return bcrypt.compare(raw, hash);
+    return await bcrypt.compare(raw, hash);
   }
 }
