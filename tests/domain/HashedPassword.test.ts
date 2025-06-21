38eb174542e4d4e97e2f29b788891884156f7b19
DavidSheltonSF <david.faria012@gmail.com>
2025-06-21 17:15:14 -0300

    refactor(HashedPassword.test): replace string password with Password object for consistency

--- tests/domain/HashedPassword.test.ts
+++ tests/domain/HashedPassword.test.ts
@@ -1,27 +1,25 @@
 import bcrypt from 'bcryptjs';
 import { HashedPassword } from '../../src/domain/entities/value-objects';
+import { Password } from '../../src/domain/entities/value-objects';
 import { BcryptHasher } from '../../src/infrastructure/cryptography/BcryptHasher';
 
 describe('Testing Phone validator', () => {
   test('Should create a hash password', async () => {
-    const validPassword = 'Test123*@!55';
+    const password = Password.create('Test123*@!55').getRight();
     const hasher = new BcryptHasher(12);
-    const hashedPasswordOrError = await HashedPassword.create(
-      validPassword,
-      hasher
-    );
+    const hashedPasswordOrError = await HashedPassword.create(password, hasher);
 
     expect(hashedPasswordOrError.isRight()).toBeTruthy();
     expect(
-      bcrypt.compare(validPassword, hashedPasswordOrError.getRight().get())
+      bcrypt.compare(password.get(), hashedPasswordOrError.getRight().get())
     ).toBeTruthy();
   });
 
   test('Should compare a raw password and a hashed one properly', async () => {
-    const validPassword = 'Test123*@!55';
+    const password = Password.create('Test123*@!55').getRight();
     const comparer = new BcryptHasher();
-    const hashedPassword = await bcrypt.hash(validPassword, 12);
+    const hashedPassword = await bcrypt.hash(password.get(), 12);
 
-    expect(comparer.compare(validPassword, hashedPassword)).toBeTruthy();
+    expect(comparer.compare(password.get(), hashedPassword)).toBeTruthy();
   });
 });
