02f65540c866a91bb0c88fad9bbb407cc3d45fb9
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 18:00:11 -0300

    feat(Customer domain entity)

---
+++ src/domain/entities/customer/Customer.ts
@@ -0,0 +1,91 @@
+import { Email, Cpf, Phone, Role, Address, Password, Name, UserName, HashedPassword } from "../value-objects";
+import { CustomerProps } from "./CustomerProps";
+
+export class Customer {
+  private _username: UserName;
+  private _name: Name;
+  private _email: Email;
+  private _cpf: Cpf;
+  private _phone: Phone;
+  private _role: Role;
+  private _address: Address;
+  private _authentication: {
+    hashedPassword: HashedPassword;
+    sessionToken?: string;
+  };
+
+  constructor(customerData: CustomerProps) {
+    this._username = customerData.username;
+    this._name = customerData.name;
+    this._email = customerData.email;
+    this._cpf = customerData.cpf;
+    this._phone = customerData.phone;
+    this._role = customerData.role;
+    this._address = customerData.address;
+    this._authentication = customerData.authentication;
+  }
+
+  get username(): UserName {
+    return this._username;
+  }
+  set username(value: UserName) {
+    this._username = value;
+  }
+
+  get name(): Name {
+    return this._name;
+  }
+  set name(value: Name) {
+    this._name = value;
+  }
+
+  get email(): Email {
+    return this._email;
+  }
+  set email(value: Email) {
+    this._email = value;
+  }
+
+  get cpf(): Cpf {
+    return this._cpf;
+  }
+  set cpf(value: Cpf) {
+    this._cpf = value;
+  }
+
+  get phone(): Phone {
+    return this._phone;
+  }
+  set phone(value: Phone) {
+    this._phone = value;
+  }
+
+  get role(): Role {
+    return this._role;
+  }
+  set role(value: Role) {
+    this._role = value;
+  }
+
+  get address(): Address {
+    return this._address;
+  }
+  set address(value: Address) {
+    this._address = value;
+  }
+
+  get hashedPassword(): HashedPassword {
+    return this._authentication.hashedPassword;
+  }
+  set hashedPassword(value: HashedPassword) {
+    this._authentication.hashedPassword = value;
+  }
+
+  activeSession(sessionToken: string){
+    this._authentication.sessionToken = sessionToken;
+  }
+
+  desactiveSession() {
+    this._authentication.sessionToken = undefined;
+  }
+}
---
+++ tests/domain/entities/Customer.test.ts
@@ -0,0 +1,48 @@
+import { Customer } from '../../../src/domain/entities/customer/Customer';
+import { CustomerProps } from '../../../src/domain/entities/customer/CustomerProps';
+import {
+  Address,
+  Cpf,
+  Email,
+  HashedPassword,
+  Name,
+  Password,
+  Phone,
+  Role,
+  UserName,
+} from '../../../src/domain/entities/value-objects';
+import { BcryptHasher } from '../../../src/infrastructure/cryptography/BcryptHasher';
+
+describe('Customer Model', () => {
+  it('should create a customer with valid properties', async () => {
+    const hasher = new BcryptHasher(12);
+    const password = Password.create('Dae84*jifsjf21').getRight();
+    const hashedPassword = (
+      await HashedPassword.create(password, hasher)
+    ).getRight();
+
+    const customerProps: CustomerProps = {
+      username: UserName.create('Carlos').getRight(),
+      name: Name.create('Carlos Montenegro').getRight(),
+      email: Email.create('carlos@bugmail.com').getRight(),
+      cpf: Cpf.create('12588774825').getRight(),
+      phone: Phone.create('21585874596').getRight(),
+      role: Role.create('admin').getRight(),
+      address: Address.create({
+        street: 'Rua dos Morangos',
+        city: 'Cidade dos Anjos',
+        state: 'Rio de Janeiro',
+        zipCode: '25874129',
+      }).getRight(),
+      authentication: {
+        hashedPassword: hashedPassword,
+      },
+    };
+
+    const customer = new Customer(customerProps);
+
+    expect(customer.name.get()).toBe(customerProps.name.get());
+  });
+
+  // Add more tests as needed
+});
