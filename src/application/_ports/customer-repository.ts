6832e6da2cac7aff05e74068be52987be1a2b739
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 19:53:09 -0300

    refactor: update CustomerRepository interface to return CustomerModel for findCustomerByEmail

--- src/application/_ports/customer-repository.ts
+++ src/application/_ports/customer-repository.ts
@@ -1,7 +1,8 @@
 import { Customer } from '../../domain/entities/customer/Customer';
+import { CustomerModel } from '../../infrastructure/models/mongodb/CustomerModel';
 import { CustomerUseCaseDto } from '../useCaseDtos/CustomerUseCaseDto';
 
 export interface CustomerRepository {
-  findCustomerByEmail: (email: string) => Promise<CustomerUseCaseDto>
-  add: (custumerData: Customer) => Promise<any>;
+  findCustomerByEmail: (email: string) => Promise<CustomerModel>;
+  add: (custumerData: Customer) => Promise<Partial<CustomerModel>>;
 }
