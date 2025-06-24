80a2fc4664cfbf2743de49eda0f58f93553155ec
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 18:40:18 -0300

    fix: update RegisterCustomerResponse to use CustomerUseCaseDto and include id field

--- src/application/usecases/user/register-user/response.ts
+++ src/application/usecases/user/register-user/response.ts
@@ -1,9 +1,12 @@
 import { Either } from '../../../../shared/either';
 import { DuplicatedDataError } from '../../../_errors/duplicated-data';
-import { CustomerProps } from '../../../../domain/entities/customer-props';
 import { customerValidationError } from '../../../../domain/entities/errors/customerValidationError';
+import { CustomerUseCaseDto } from '../../../useCaseDtos/CustomerUseCaseDto';
 
 export type RegisterCustomerResponse = Either<
   customerValidationError | DuplicatedDataError,
-  CustomerProps
+  {
+    id: string;
+    customerData: CustomerUseCaseDto;
+  }
 >;
