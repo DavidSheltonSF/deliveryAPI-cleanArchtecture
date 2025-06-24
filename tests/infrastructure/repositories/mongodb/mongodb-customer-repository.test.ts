1252f637c0d97a69193d1921dd5ad3bfef94c900
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 20:10:34 -0300

    test(MongodbCustomerRepository): update test file and test findCustomerByEmail

--- tests/infrastructure/repositories/mongodb/mongodb-customer-repository.test.ts
+++ tests/infrastructure/repositories/mongodb/mongodb-customer-repository.test.ts
@@ -4,6 +4,7 @@ import { MongodbCustomerRepository } from '../../../../src/infrastructure/reposi
 import { generateHexId } from '../../../../src/shared/generateHexId';
 import { CustomerDtoMapper } from '../../../../src/presentation/mappers/CustomerDtoMapper';
 import { CustomerDTO } from '../../../../src/presentation/dtos/custumer-dto';
+import { Customer } from '../../../../src/domain/entities/customer/Customer';
 
 config();
 
@@ -12,6 +13,7 @@ const repository = new MongodbCustomerRepository();
 const customers: CustomerDTO[] = [
   {
     username: 'Marta',
+    name: 'Maria dos Anjos',
     email: 'mart@bugmail.com',
     cpf: '88888888888',
     phone: '21555777777',
@@ -28,6 +30,7 @@ const customers: CustomerDTO[] = [
   },
   {
     username: 'Oswaldo',
+    name: 'Oswaldo Sanches',
     email: 'oswald@bugmail.com',
     cpf: '88888888588',
     phone: '21558777777',
@@ -63,21 +66,44 @@ describe('Testing MongodbCustomerRepository', () => {
     await mongoHelper.clearCollection('customers');
   });
 
+  test('Should  find a user by email', async () => {
+    const customerCollection = mongoHelper.getCollection('customers');
+
+    const customer = customers[0];
+
+    // Adding new customers to database
+    await customerCollection.insertOne(customer);
+
+
+    const foundCustomer = await repository.findCustomerByEmail(customer.email);;
+
+    expect(foundCustomer?.username).toBe(customer.username);
+    expect(foundCustomer?.name).toBe(customer.name);
+    expect(foundCustomer?.email).toBe(customer.email);
+    expect(foundCustomer?.cpf).toBe(customer.cpf);
+    expect(foundCustomer?.phone).toBe(customer.phone);
+    expect(foundCustomer?.role).toBe(customer.role);
+  });
+
   test('Should add a new customer in the database', async () => {
     const customerCollection = mongoHelper.getCollection('customers');
 
     const customer = customers[0];
 
-    const customerOrError = await CustomerDtoMapper.fromDtoToProps(customer);
+    const customerOrError = (await CustomerDtoMapper.fromDtoToProps(customer)).getRight();
+
+    const customerEntity = new Customer(customerOrError)
 
     // Adding new customers to database
-    const response = await repository.add(customerOrError.getRight());
+    const response = await repository.add(customerEntity);
 
     const foundCustomer = await customerCollection.findOne({
       email: customer.email,
     });
 
-    expect(foundCustomer?.customername).toBe(customer.username);
+    console.log(foundCustomer?.email);
+
+    expect(foundCustomer?.username).toBe(customer.username);
     expect(foundCustomer?.email).toBe(customer.email);
     expect(foundCustomer?.cpf).toBe(customer.cpf);
     expect(foundCustomer?.phone).toBe(customer.phone);
