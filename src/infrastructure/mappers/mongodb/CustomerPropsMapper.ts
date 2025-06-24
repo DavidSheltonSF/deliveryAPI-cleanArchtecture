b5bac6ffd1eb784ccc0d7f08e007ac827d299549
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 19:06:35 -0300

    refactor: update CustomerPropsMapper to use CustomerProps and streamline model mapping

--- src/infrastructure/mappers/mongodb/CustomerPropsMapper.ts
+++ src/infrastructure/mappers/mongodb/CustomerPropsMapper.ts
@@ -1,4 +1,4 @@
-import { CustomerProps } from '../../../domain/entities/customer-props';
+import { CustomerProps } from '../../../domain/entities/customer/CustomerProps';
 import { customerValidationError } from '../../../domain/entities/errors/customerValidationError';
 import {
   Address,
@@ -10,88 +10,43 @@ import {
   Email,
   HashedPassword,
 } from '../../../domain/entities/value-objects';
-import { CustomerModel } from '../../models/mongodb/customer-model';
+import { CustomerModel } from '../../models/mongodb/CustomerModel';
 import { Either } from '../../../shared/either';
 import { mongoHelper } from '../../repositories/mongodb/helpers/mongo-helper';
 import { BcryptHasher } from '../../cryptography/BcryptHasher';
+import { CustomerUseCaseDto } from '../../../application/useCaseDtos/CustomerUseCaseDto';
+import { Customer } from '../../../domain/entities/customer/Customer';
 
 export class CustomerPropsMapper {
-  static fromPropsToModel(
-    customerId: string,
-    customerData: CustomerProps
-  ): Omit<CustomerModel, 'createdAt'> {
+  static fromEntityToModel(
+    customerData: Customer
+  ): Omit<CustomerModel, 'createdAt' | '_id'> {
     return {
-      _id: mongoHelper.toObjectId(customerId),
       username: customerData.username.get(),
+      name: customerData.name.get(),
       email: customerData.email.get(),
       cpf: customerData.cpf.get(),
       phone: customerData.phone.get(),
       role: customerData.role.get(),
       address: customerData.address.get(),
       authentication: {
-        passwordHash: customerData.authentication.hashedPassword.get(),
-        sessionToken: customerData.authentication.sessionToken,
+        passwordHash: customerData.hashedPassword.get(),
+        sessionToken: customerData.sessionToken,
       },
     };
   }
 
-  static async fromModelToProps(
+  static async fromModelToUseCaseDto(
     customerData: CustomerModel
-  ): Promise<Either<customerValidationError, CustomerProps>> {
-    const usernameOrError = Name.create(customerData.username);
-    const emailOrError = Email.create(customerData.email);
-    const cpfOrError = Cpf.create(customerData.cpf);
-    const phoneOrError = Phone.create(customerData.phone);
-    const roleOrError = Role.create(customerData.role);
-    const addressOrError = Address.create(customerData.address);
-    const passwordOrError = Password.create(
-      customerData.authentication.passwordHash
-    );
-
-    if (usernameOrError.isLeft()) {
-      return Either.left(usernameOrError.getLeft());
-    }
-    if (emailOrError.isLeft()) {
-      return Either.left(emailOrError.getLeft());
-    }
-    if (cpfOrError.isLeft()) {
-      return Either.left(cpfOrError.getLeft());
-    }
-    if (phoneOrError.isLeft()) {
-      return Either.left(phoneOrError.getLeft());
-    }
-    if (roleOrError.isLeft()) {
-      return Either.left(roleOrError.getLeft());
-    }
-    if (addressOrError.isLeft()) {
-      return Either.left(addressOrError.getLeft());
-    }
-    if (passwordOrError.isLeft()) {
-      return Either.left(passwordOrError.getLeft());
-    }
-
-    const salt = 12;
-    const hasher = new BcryptHasher(salt);
-    const hashedPasswordOrError = await HashedPassword.create(
-      passwordOrError.getRight(),
-      hasher
-    );
-
-    if (hashedPasswordOrError.isLeft()){
-      return Either.left(hashedPasswordOrError.getLeft());
-    }
-
-    return Either.right({
-      username: usernameOrError.getRight(),
-      email: emailOrError.getRight(),
-      cpf: cpfOrError.getRight(),
-      phone: phoneOrError.getRight(),
-      role: roleOrError.getRight(),
-      address: addressOrError.getRight(),
-      authentication: {
-        hashedPassword: hashedPasswordOrError.getRight(),
-        sessionToken: customerData.authentication?.sessionToken,
-      },
-    });
+  ): Promise<CustomerUseCaseDto> {
+    return {
+      id: customerData._id.toString(),
+      username: customerData.username,
+      name: customerData.name,
+      email: customerData.email,
+      phone: customerData.phone,
+      role: customerData.role,
+      address: customerData.address,
+    };
   }
 }
