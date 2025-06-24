c4602a1247bc7f9e4cd06a40de99f8e99258086d
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 18:33:16 -0300

    feat: add AddressUseCaseDto and CustomerUseCaseDto interfaces

---
+++ src/application/useCaseDtos/AddressUseCaseDto.ts
@@ -0,0 +1,6 @@
+export interface AddressUseCaseDto {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
---
+++ src/application/useCaseDtos/CustomerUseCaseDto.ts
@@ -0,0 +1,12 @@
+import { AddressUseCaseDto } from './AddressUseCaseDto';
+
+export interface CustomerUseCaseDto {
+  id?: string;
+  username: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  role: string;
+  address: AddressUseCaseDto;
+}
