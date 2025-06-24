8b34ef9a1539a043cb9504fe7aef6d2a318dea34
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 17:45:34 -0300

    feat(username): implement Username value object with validation and creation methods

---
+++ src/domain/entities/value-objects/userName.ts
@@ -0,0 +1,41 @@
+import { Either } from '../../../shared/either';
+import { InvalidUserNameError } from '../errors';
+
+export class Username {
+  private readonly username: string;
+
+  constructor(username: string) {
+    this.username = username;
+    Object.freeze(this);
+  }
+
+  static validate(username: string): Boolean {
+    const usernameRegex = /^[a-zA-ZÀ-ÿ]+(([',. -][a-zA-ZÀ-ÿ ])?[a-zA-ZÀ-ÿ]*)*$/;
+
+    if (
+      !username ||
+      username.trim().length < 2 ||
+      username.trim().length > 255
+    ) {
+      return false;
+    }
+
+    if (!username.match(usernameRegex)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  static create(username: string): Either<InvalidUserNameError, Username> {
+    if (!this.validate(username)) {
+      return Either.left(new InvalidUserNameError(username));
+    }
+
+    return Either.right(new Username(username));
+  }
+
+  get(): string {
+    return this.username;
+  }
+}
