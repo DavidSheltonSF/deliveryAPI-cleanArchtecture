65541e81cb36ae6d16bfbc40f1901f8fff35d503
DavidSheltonSF <david.faria012@gmail.com>
2025-06-24 17:53:11 -0300

    refactor: delete customer-props

--- src/domain/entities/customer-props.ts
+++
@@ -1,9 +0,0 @@
-import { BaseUserProps } from './base-user-props';
-import { Address, Cpf, Phone, Role } from './value-objects';
-
-export interface CustomerProps extends BaseUserProps {
-  cpf: Cpf;
-  phone: Phone;
-  role: Role;
-  address: Address;
-}
