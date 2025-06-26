3154f3f665debe36dac9e5295527a2c5a96a340c
DavidSheltonSF <david.faria012@gmail.com>
2025-06-26 18:47:54 -0300

    refactor: remove serializeUser helper function as it is no longer needed

--- src/presentation/controllers/helpers/serializeUser.ts
+++
@@ -1,7 +0,0 @@
-import { UserProps } from '../../../domain/entities/user-props';
-
-export function serializeUser(userProps: UserProps) {
-  const { authentication, bankInfo, ...safeUser } = userProps;
-
-  return safeUser;
-}
