48b6f168e15473fa0e3f1027984d61ea952db5de
DavidSheltonSF <david.faria012@gmail.com>
2025-06-26 19:52:23 -0300

    refactor: remove unused repository interfaces for delivery, dish, order, payment, restaurant chain, and restaurant

--- src/application/_ports/dish-repository.ts
+++
@@ -1,9 +0,0 @@
-import { DishProps } from '../../domain/entities/dish-props';
-
-export interface DishRepository {
-  findAllDishs: () => Promise<DishProps[]>;
-  findDishById: (dishId: string) => Promise<DishProps | null>;
-  add: (dish: DishProps) => Promise<DishProps>;
-  remove: (dishId: string) => Promise<void>;
-  update: (dishId: string, dishData: Omit<DishProps, '_id'>) => Promise<void>;
-}
