d6d13175e6340b329e54f7d73fcbe876bb50049d
DavidSheltonSF <david.faria012@gmail.com>
2025-06-26 20:31:34 -0300

    feat: add InvalidBirthdayError class for handling invalid birthday validation

--- src/domain/entities/errors/InvalidBirthdayError.ts
+++ src/domain/entities/errors/InvalidBirthdayError.ts
@@ -1,6 +1,6 @@
 import { DomainError } from './DomainError';
 
-export class InvalidBirthDayError extends Error implements DomainError {
+export class InvalidBirthdayError extends Error implements DomainError {
   constructor(birthday: string) {
     super(`The birthday ${birthday} is invalid`);
     this.name = 'InvalidBirthDayError';
