2970bf5b6a7c2520c9ee420bf38c80b9a5af1b91
DavidSheltonSF <david.faria012@gmail.com>
2025-06-26 21:03:34 -0300

    feat: add Birthday value object to CustomerProps interface

--- src/domain/entities/customer/CustomerProps.ts
+++ src/domain/entities/customer/CustomerProps.ts
@@ -1,10 +1,11 @@
 import { BaseUserProps } from '../base-user-props';
-import { Address, Cpf, Name, Phone, Role } from '../value-objects';
+import { Address, Cpf, Name, Phone, Role, Birthday } from '../value-objects';
 
 export interface CustomerProps extends BaseUserProps {
   name: Name;
   cpf: Cpf;
   phone: Phone;
   role: Role;
+  birthDay: Birthday
   address: Address;
 }
