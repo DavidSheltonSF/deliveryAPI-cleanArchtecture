90c2c6ff180aefe17497d7554ce46d27262b694e
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 14:46:17 -0300

    feat(zipCode): rename zipCode to ZipCode

--- src/domain/entities/address/Address.ts
+++ src/domain/entities/address/Address.ts
@@ -1,4 +1,4 @@
-import { ZipCode } from '../value-objects/zipCode';
+import { ZipCode } from '../value-objects/ZipCode';
 import { AddressProps } from './AddressProps';
 
 export class Address {
--- src/domain/entities/address/AddressProps.ts
+++ src/domain/entities/address/AddressProps.ts
@@ -1,4 +1,4 @@
-import { ZipCode } from "../value-objects/zipCode";
+import { ZipCode } from '../value-objects/ZipCode';
 
 export interface AddressProps {
   street: string;
