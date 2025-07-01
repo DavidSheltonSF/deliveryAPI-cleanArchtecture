13c2b971feb8f46dbda24feae4662f57db2ea09f
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 18:52:16 -0300

    feat(User): implement User class with authentication and user properties

---
+++ src/domain/entities/user/User.ts
@@ -0,0 +1,87 @@
+import {
+  Email,
+  Cpf,
+  Role,
+  Name,
+  UserName,
+  PasswordHash,
+} from '../../value-objects';
+import { Authentication } from '../authentication/Authentication';
+
+export class User {
+  private _id: string;
+  private _username: UserName;
+  private _name: Name;
+  private _email: Email;
+  private _cpf: Cpf;
+  private _role: Role;
+  private _authentication: Authentication;
+
+  constructor(
+    id: string,
+    username: UserName,
+    name: Name,
+    email: Email,
+    cpf: Cpf,
+    role: Role,
+    authentication: Authentication
+  ) {
+    this._id = id;
+    this._username = username;
+    this._name = name;
+    this._email = email;
+    this._cpf = cpf;
+    this._role = role;
+    this._authentication = authentication;
+  }
+  get id(): string {
+    return this._id;
+  }
+  get username(): UserName {
+    return this._username;
+  }
+  set username(value: UserName) {
+    this._username = value;
+  }
+  get name(): Name {
+    return this._name;
+  }
+  set name(value: Name) {
+    this._name = value;
+  }
+  get email(): Email {
+    return this._email;
+  }
+  set email(value: Email) {
+    this._email = value;
+  }
+
+  get cpf(): Cpf {
+    return this._cpf;
+  }
+  set cpf(value: Cpf) {
+    this._cpf = value;
+  }
+
+  get role(): Role {
+    return this._role;
+  }
+  set role(value: Role) {
+    this._role = value;
+  }
+
+  get passwordHash(): PasswordHash {
+    return this._authentication.passwordHash;
+  }
+  get sessionToken(): string {
+    return this._authentication.sessionToken;
+  }
+
+  activeSession(sessionToken: string) {
+    this._authentication.updateSessionToken(sessionToken);
+  }
+
+  desactiveSession() {
+    this._authentication.updateSessionToken(undefined);
+  }
+}
