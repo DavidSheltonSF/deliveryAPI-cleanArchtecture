88df2502828a4a0c8fe0fd1a5934f18761b79378
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 18:04:58 -0300

    refactor(BankAccountProps): rename bank property to bankCode for clarity

--- src/domain/entities/bankAccount/BankAccountProps.ts
+++ src/domain/entities/bankAccount/BankAccountProps.ts
@@ -3,7 +3,7 @@ import { Cpf, Name } from '../../value-objects';
 export interface BankAccountProps {
   holderName: Name;
   cpf: Cpf;
-  bank: string;
+  bankCode: string;
   agency: string;
   accountNumber: string;
   accountType: string;
