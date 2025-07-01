96688051ceffd21ab85a617b9a66895437401d5f
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 14:41:09 -0300

    feat(zipCode): implement ZipCode valuo object with validation and creation methods

---
+++ src/domain/entities/value-objects/zipCode.ts
@@ -0,0 +1,34 @@
+import { Either } from '../../../shared/either';
+import { InvalidAddressError } from '../errors/InvalidAddressError';
+
+
+export class ZipCode {
+  private readonly zipCode: string;
+
+  constructor(zipCode: string) {
+    this.zipCode = zipCode;
+    Object.freeze(this);
+  }
+
+  static validate(zipCode: string): Boolean {
+    const zipCodeRegex = /^\d{5}\-?\d{3}$/;
+
+    if (!zipCode.match(zipCodeRegex)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  static create(zipCode: string): Either<InvalidAddressError, ZipCode> {
+    if (!this.validate(zipCode)) {
+      return Either.left(new InvalidAddressError(zipCode));
+    }
+
+    return Either.right(new ZipCode(zipCode));
+  }
+
+  get(): string {
+    return this.zipCode;
+  }
+}
