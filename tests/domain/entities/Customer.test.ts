07a7e5bd4ba5b9dfcc16069ddeef40e7fc11f68a
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 21:04:41 -0300

    refactor(Customer.test): enhance test structure by creating makeSut function and improving authentication handling

--- tests/domain/entities/Customer.test.ts
+++ tests/domain/entities/Customer.test.ts
@@ -1,49 +1,126 @@
+import { v4 as uuidv4 } from 'uuid';
 import { Customer } from '../../../src/domain/entities/customer/Customer';
-import { CustomerProps } from '../../../src/domain/entities/customer/CustomerProps';
 import {
   Address,
   Birthday,
   Cpf,
   Email,
-  HashedPassword,
   Name,
   Password,
+  PasswordHash,
   Phone,
   Role,
   UserName,
 } from '../../../src/domain/value-objects';
 import { BcryptHasher } from '../../../src/infrastructure/cryptography/BcryptHasher';
+import { Authentication } from '../../../src/domain/entities/authentication/Authentication';
 
 describe('Customer Model', () => {
-  it('should create a customer with valid properties', async () => {
+  async function makeSut() {
+    const id = uuidv4();
+    const username = UserName.create('Carlos').getRight();
+    const name = Name.create('Carlos Montenegro').getRight();
+    const email = Email.create('carlos@bugmail.com').getRight();
+    const cpf = Cpf.create('12588774825').getRight();
+    const role = Role.create('admin').getRight();
+    const phone = Phone.create('21858740555').getRight();
+    const birthday = Birthday.create('2002-02-26').getRight();
+    const address = new Address({
+      street: 'fake street',
+      city: 'fake city',
+      state: 'fakestate',
+      zipCode: '26555485',
+    });
+
+    // Creating hash password
     const hasher = new BcryptHasher(12);
     const password = Password.create('Dae84*jifsjf21').getRight();
-    const hashedPassword = (
-      await HashedPassword.create(password, hasher)
+    const passwordHash = (
+      await PasswordHash.create(password, hasher)
     ).getRight();
 
-    const customerProps: CustomerProps = {
-      username: UserName.create('Carlos').getRight(),
-      name: Name.create('Carlos Montenegro').getRight(),
-      email: Email.create('carlos@bugmail.com').getRight(),
-      cpf: Cpf.create('12588774825').getRight(),
-      phone: Phone.create('21585874596').getRight(),
-      role: Role.create('admin').getRight(),
-      birthday: Birthday.create('2002-02-26').getRight(),
-      address: Address.create({
-        street: 'Rua dos Morangos',
-        city: 'Cidade dos Anjos',
-        state: 'Rio de Janeiro',
-        zipCode: '25874129',
-      }).getRight(),
-      authentication: {
-        hashedPassword: hashedPassword,
-      },
+    // Creating authntication
+    const authenticationId = uuidv4();
+    const authentication = new Authentication(
+      authenticationId,
+      email,
+      passwordHash
+    );
+
+    return {
+      id,
+      username,
+      name,
+      email,
+      cpf,
+      role,
+      phone,
+      birthday,
+      address,
+      authentication,
     };
+  }
 
-    const customer = new Customer(customerProps);
+  test('should create a customer with valid properties', async () => {
+    const customerData = await makeSut();
 
-    expect(customer.name.get()).toBe(customerProps.name.get());
+    const customer = new Customer(
+      customerData.id,
+      customerData.username,
+      customerData.name,
+      customerData.email,
+      customerData.cpf,
+      customerData.role,
+      customerData.phone,
+      customerData.birthday,
+      customerData.address,
+      customerData.authentication
+    );
+
+    expect(customer.id).toBe(customerData.id);
+    expect(customer.username.get()).toBe(customerData.username.get());
+    expect(customer.name.get()).toBe(customerData.name.get());
+    expect(customer.email.get()).toBe(customerData.email.get());
+    expect(customer.cpf.get()).toBe(customerData.cpf.get());
+    expect(customer.role.get()).toBe(customerData.role.get());
+    expect(customer.phone.get()).toBe(customerData.phone.get());
+    expect(customer.birthday.get()).toBe(customerData.birthday.get());
+    expect(customer.passwordHash).toBe(
+      customerData.authentication.passwordHash
+    );
+  });
+
+  test('should check if a customer is an adult properly', async () => {
+    const customerData = await makeSut();
+
+    const adultCustomer = new Customer(
+      customerData.id,
+      customerData.username,
+      customerData.name,
+      customerData.email,
+      customerData.cpf,
+      customerData.role,
+      customerData.phone,
+      Birthday.create('2000-02-23').getRight(),
+      customerData.address,
+      customerData.authentication
+    );
+
+    const nonAdultCustomer = new Customer(
+      customerData.id,
+      customerData.username,
+      customerData.name,
+      customerData.email,
+      customerData.cpf,
+      customerData.role,
+      customerData.phone,
+      Birthday.create('2015-02-23').getRight(),
+      customerData.address,
+      customerData.authentication
+    );
+
+    expect(adultCustomer.isAdult()).toBeTruthy();
+    expect(nonAdultCustomer.isAdult()).toBeFalsy();
   });
 
   // Add more tests as needed
