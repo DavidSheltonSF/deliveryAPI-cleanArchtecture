f43fab309b9ce9605682f01758c88437032eed3e
DavidSheltonSF <david.faria012@gmail.com>
2025-07-01 22:21:12 -0300

    feat(UserModel): add UserProps interface for user data structure

---
+++ src/infrastructure/models/mongodb/UserModel.ts
@@ -0,0 +1,12 @@
+import { ObjectId } from 'mongodb';
+
+export interface UserProps {
+  _id: ObjectId;
+  username: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone?: string;
+  birthday?: Date;
+  role: string;
+}
