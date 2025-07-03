d92b5a575a313c8c1d4a6c891fdbe50f6b680c3e
DavidSheltonSF <david.faria012@gmail.com>
2025-07-03 14:15:11 -0300

    feat(MongodbUserRepository): implement user repository with findUserByEmail and add methods

---
+++ src/infrastructure/repositories/mongodb/MongodbUserRepository.ts
@@ -0,0 +1,62 @@
+import { UserRepository } from '../../../application/_ports/UserRepository';
+import { Customer } from '../../../domain/entities/user/customer/Customer';
+import { UserModel } from '../../models/mongodb/UserModel';
+import { mongoHelper } from './helpers/mongo-helper';
+
+export class MongodbUserRepository implements UserRepository {
+  async findUserByEmail(email: string): Promise<UserModel | null> {
+    const userCollection = mongoHelper.getCollection('users');
+    const foundUser = await userCollection.findOne({ email });
+
+    return {
+      _id: foundUser._id,
+      username: foundUser.username,
+      name: foundUser.name,
+      email: foundUser.email,
+      cpf: foundUser.cpf,
+      phone: foundUser.phone,
+      role: foundUser.role,
+      birthday: foundUser.birthday,
+      createdAt: foundUser.createdAt,
+    };
+  }
+
+  async add(user: Customer): Promise<UserModel | null> {
+    const userCollection = mongoHelper.getCollection('user');
+    const userUserModel: UserModel = {
+      _id: mongoHelper.toObjectId(user.id),
+      username: user.username.get(),
+      name: user.name.get(),
+      email: user.email.get(),
+      cpf: user.cpf.get(),
+      phone: user.phone.get(),
+      role: user.role.get(),
+      birthday: user.birthday.get(),
+      createdAt: new Date(),
+    };
+
+    const newUserId = await userCollection
+      .insertOne(userUserModel)
+      .then((result: any) => result.insertedId);
+
+    const registeredUser = await userCollection.findOne({
+      _id: newUserId,
+    });
+
+    if (registeredUser === null) {
+      return null;
+    }
+
+    return {
+      _id: newUserId,
+      username: registeredUser.username,
+      name: registeredUser.name,
+      email: registeredUser.email,
+      cpf: registeredUser.cpf,
+      phone: registeredUser.phone,
+      role: registeredUser.role,
+      birthday: registeredUser.bithday,
+      createdAt: registeredUser.createdAt,
+    };
+  }
+}
