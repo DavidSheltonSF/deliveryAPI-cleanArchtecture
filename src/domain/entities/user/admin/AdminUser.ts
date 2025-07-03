26fe611cf154651ed581ffdea4d99b3c1965b29a
DavidSheltonSF <david.faria012@gmail.com>
2025-07-03 18:22:06 -0300

    feat(entities): add AdminUser domain entity

---
+++ src/domain/entities/user/admin/AdminUser.ts
@@ -0,0 +1,37 @@
+import {
+  Phone,
+  Birthday,
+  UserName,
+  Name,
+  Role,
+  Email,
+  Cpf,
+} from '../../../value-objects';
+import { Authentication } from '../../authentication/Authentication';
+import { User } from '../User';
+
+export class AdminUser extends User {
+  constructor(
+    id: string,
+    username: UserName,
+    name: Name,
+    email: Email,
+    cpf: Cpf,
+    phone: Phone,
+    role: Role,
+    birthday: Birthday,
+    authentication: Authentication
+  ) {
+    super(
+      id,
+      username,
+      name,
+      email,
+      cpf,
+      phone,
+      role,
+      birthday,
+      authentication
+    );
+  }
+}
