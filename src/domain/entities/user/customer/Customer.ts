7139276a4d8ad7d46ed62027d298ba8aa4f15734
DavidSheltonSF <david.faria012@gmail.com>
2025-07-03 16:24:21 -0300

    refactor(Customer): streamline constructor and remove redundant properties

--- src/domain/entities/user/customer/Customer.ts
+++ src/domain/entities/user/customer/Customer.ts
@@ -12,38 +12,30 @@ import { Authentication } from '../../authentication/Authentication';
 import { User } from '../User';
 
 export class Customer extends User {
-  private _phone: Phone;
-  private _birthday: Birthday;
-  private _address: Address;
-
   constructor(
     id: string,
     username: UserName,
     name: Name,
     email: Email,
     cpf: Cpf,
-    role: Role,
     phone: Phone,
+    role: Role,
     birthday: Birthday,
     address: Address,
     authentication: Authentication
   ) {
-    super(id, username, name, email, cpf, role, authentication);
-    this._phone = phone;
-    this._birthday = birthday;
-    this._address = address;
-  }
-
-  get phone(): Phone {
-    return this._phone;
-  }
-
-  get birthday(): Birthday {
-    return this._birthday;
-  }
-
-  get address(): Address {
-    return this._address;
+    super(
+      id,
+      username,
+      name,
+      email,
+      cpf,
+      phone,
+      role,
+      birthday,
+      address,
+      authentication
+    );
   }
 
   isAdult(): boolean {
