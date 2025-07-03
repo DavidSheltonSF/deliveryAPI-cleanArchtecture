3264c84d9816d4f2451a6c545c5ad17c78729656
DavidSheltonSF <david.faria012@gmail.com>
2025-07-03 13:37:06 -0300

    fix(UserRepository): correct import path for UserUseCaseDto

--- src/application/_ports/UserRepository.ts
+++ src/application/_ports/UserRepository.ts
@@ -1,6 +1,6 @@
 import { Customer } from '../../domain/entities/user/customer/Customer';
 import { UserModel } from '../../infrastructure/models/mongodb/UserModel';
-import { UserUseCaseDto } from '../useCaseDtos/CustomerUseCaseDto';
+import { UserUseCaseDto } from '../useCaseDtos/UserUseCaseDto';
 
 export interface UserRepository {
   findCustomerByEmail: (email: string) => Promise<UserModel | null>;
