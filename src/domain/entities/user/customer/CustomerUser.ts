1cd42d40104eab5d54971cb6fa7cfc22e87a891a
DavidSheltonSF <david.faria012@gmail.com>
2025-07-18 16:42:53 -0300

    refactor(CustomerUser): make it to be constructed with CustomerProps

--- src/domain/entities/user/customer/CustomerUser.ts
+++ src/domain/entities/user/customer/CustomerUser.ts
@@ -1,46 +1,25 @@
-import {
-  Phone,
-  Birthday,
-  UserName,
-  Name,
-  Role,
-  Email,
-  Cpf,
-} from '../../../value-objects';
 import { Address } from '../../address/Address';
-import { Authentication } from '../../authentication/Authentication';
 import { User } from '../User';
+import { CustomerProps } from './CustomerProps';
 
 export class CustomerUser extends User {
   private _address: Address;
-  constructor(
-    id: string,
-    username: UserName,
-    name: Name,
-    email: Email,
-    cpf: Cpf,
-    phone: Phone,
-    role: Role,
-    birthday: Birthday,
-    address: Address,
-    authentication: Authentication
-  ) {
-    super(
-      id,
-      username,
-      name,
-      email,
-      cpf,
-      phone,
-      role,
-      birthday,
-      authentication
-    );
-    this._address = address;
+  constructor(customerData: CustomerProps) {
+    super({
+      id: customerData.id,
+      username: customerData.username,
+      name: customerData.name,
+      email: customerData.email,
+      cpf: customerData.cpf,
+      phone: customerData.phone,
+      role: customerData.role,
+      birthday: customerData.birthday,
+      authentication: customerData.authentication,
+    });
+    this._address = customerData.address;
   }
 
   get address(): Address {
     return this._address;
   }
-
 }
