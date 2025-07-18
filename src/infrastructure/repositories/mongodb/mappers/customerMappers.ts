cbc8e4e4407c4e137d9dd280a8355abfa7205449
DavidSheltonSF <david.faria012@gmail.com>
2025-07-18 14:26:12 -0300

    refactor(mappers): add entityToCustomerModel mapper

--- src/infrastructure/repositories/mongodb/mappers/customerMappers.ts
+++ src/infrastructure/repositories/mongodb/mappers/customerMappers.ts
@@ -1,5 +1,7 @@
 import { WithId, Document } from 'mongodb';
 import { CustomerModel } from '../../../models/mongodb/CustomerModel';
+import { stringToObjectId } from '../helpers/stringToObjectId';
+import { CustomerUser } from '../../../../domain/entities/user/customer/CustomerUser';
 
 export function persistenceToCustomerModel(
   document: WithId<Document>
@@ -18,5 +20,23 @@ export function persistenceToCustomerModel(
     role: document.role,
     birthday: document.birthday,
     createdAt: document.createdAt,
+  };
+}
+
+export function entityToCustomerModel(customer: CustomerUser): CustomerModel {
+  if (document === null) {
+    throw Error('No entity provided.');
+  }
+
+  return {
+    _id: stringToObjectId(customer.id),
+    username: customer.username.get(),
+    name: customer.name.get(),
+    email: customer.email.get(),
+    cpf: customer.cpf.get(),
+    phone: customer.phone.get(),
+    role: customer.role.get(),
+    birthday: customer.birthday.get(),
+    createdAt: customer.createdAt,
   };
 }
