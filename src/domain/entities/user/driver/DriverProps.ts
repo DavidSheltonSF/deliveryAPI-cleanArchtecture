f79abb2387bf74780ab48797e85bd35b6512b6a5
DavidSheltonSF <david.faria012@gmail.com>
2025-07-18 23:17:32 -0300

    refactor(props): delete DriverProps

--- src/domain/entities/user/driver/DriverProps.ts
+++
@@ -1,8 +0,0 @@
-import { BankAccountProps } from '../../bankAccount/BankAccountProps';
-import { UserProps } from '../UserProps';
-import { Address } from '../../address/Address';
-
-export interface CustomerProps extends UserProps {
-  address?: Address;
-  bankAccount?: BankAccountProps;
-}
