fd83c5c0eba2bead250547379c75f9f78f10e0e4
DavidSheltonSF <david.faria012@gmail.com>
2025-07-18 23:17:15 -0300

    refactor(CustomerUser): make CustomerUser do not be build by props

--- src/domain/entities/user/customer/CustomerUser.ts
+++ src/domain/entities/user/customer/CustomerUser.ts
@@ -1,22 +1,34 @@
+import { Birthday, Cpf, Email, Name, Phone, Role, UserName } from '../../../value-objects';
 import { Address } from '../../address/Address';
+import { Authentication } from '../../authentication/Authentication';
 import { User } from '../User';
-import { CustomerProps } from './CustomerProps';
 
 export class CustomerUser extends User {
   private _address: Address;
-  constructor(customerData: CustomerProps) {
+  constructor(
+    username: UserName,
+    name: Name,
+    email: Email,
+    cpf: Cpf,
+    phone: Phone,
+    role: Role,
+    birthday: Birthday,
+    address: Address,
+    authentication: Authentication,
+    id?: string
+  ) {
     super({
-      id: customerData.id,
-      username: customerData.username,
-      name: customerData.name,
-      email: customerData.email,
-      cpf: customerData.cpf,
-      phone: customerData.phone,
-      role: customerData.role,
-      birthday: customerData.birthday,
-      authentication: customerData.authentication,
+      id: id,
+      username: username,
+      name: name,
+      email: email,
+      cpf: cpf,
+      phone: phone,
+      role: role,
+      birthday: birthday,
+      authentication: authentication,
     });
-    this._address = customerData.address;
+    this._address = address;
   }
 
   get address(): Address {
