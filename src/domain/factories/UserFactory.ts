560e661513b95a619cca75ea8764f921e7b39b54
DavidSheltonSF <david.faria012@gmail.com>
2025-07-18 23:18:53 -0300

    feat(UserFactory): use AddressFactory and AuthenticationFactory

--- src/domain/factories/UserFactory.ts
+++ src/domain/factories/UserFactory.ts
@@ -1,55 +1,13 @@
 import { Either } from '../../shared/either';
-import { IdGenerator } from '../contracts/IdGenerator';
-import { Address } from '../entities/address/Address';
-import { Authentication } from '../entities/authentication/Authentication';
 import { CustomerUser } from '../entities/user/customer/CustomerUser';
 import { UserProps } from '../entities/user/UserProps';
+import { AddressFactory } from './AddressFactory';
+import { AuthenticationFactory } from './AuthenticationFactory';
 
 export class UserFactory {
-  static create(
-    idGenerator: IdGenerator,
-    userData: UserProps
-  ): CustomerUser | null {
-    const {
-      username,
-      name,
-      email,
-      cpf,
-      phone,
-      role,
-      birthday,
-      address,
-      bankAccount,
-      authentication,
-    } = userData;
-
-    if (role.get() == 'admin') {
-      return null;
-    }
-
-    if (role.get() == 'restaurantAdmin') {
-      return null;
-    }
-
-    if (role.get() == 'driver') {
-      return null;
-    }
-
-    if (role.get() === 'customer') {
-      const newAddress = new Address(
-        idGenerator.generate(),
-        address.street,
-        address.city,
-        address.state,
-        address.zipCode
-      );
-      const newAuthentication = new Authentication(
-        idGenerator.generate(),
-        email,
-        authentication.passwordHash
-      );
-      return new CustomerUser(
-        idGenerator.generate(),
+  static create(userProps: UserProps): CustomerUser | null {
+    if (userProps.role.get() === 'customer') {
+      const {
         username,
         name,
         email,
@@ -57,8 +15,23 @@ export class UserFactory {
         phone,
         role,
         birthday,
-        newAddress,
-        newAuthentication
+        address,
+        authentication,
+      } = userProps;
+
+      const addressEntity = AddressFactory.create(address);
+      const authenticationEntity = AuthenticationFactory.create(authentication);
+
+      return new CustomerUser(
+        username,
+        name,
+        email,
+        cpf,
+        phone,
+        role,
+        birthday,
+        addressEntity,
+        authenticationEntity
       );
     }
   }
