d4f62bb564666666e3d08b319d6a9711411cdbd3
DavidSheltonSF <david.faria012@gmail.com>
2025-07-22 19:49:45 -0300

    test(UserName value object)

---
+++ tests/domain/value-objects/userName.test.ts
@@ -0,0 +1,12 @@
+import { UserName } from '../../../src/domain/value-objects';
+
+describe('Testing UserName validator', () => {
+  test('Trying to create a valid username', () => {
+    const validName = 'Maria Sílva de Oliveira';
+    const nameOrError = UserName.create(validName);
+    const gotName = nameOrError.getRight();
+
+    expect(nameOrError.isRight()).toBeTruthy();
+    expect(validName).toBe(gotName.getValue());
+  });
+});
