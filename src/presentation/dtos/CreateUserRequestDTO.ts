4c11b6b8fd9355a57a74aa272384c187d9cbef50
DavidSheltonSF <david.faria012@gmail.com>
2025-07-22 18:45:27 -0300

    feat(dtos):  add CreateUserRequestDTO

---
+++ src/presentation/dtos/CreateUserRequestDTO.ts
@@ -0,0 +1,16 @@
+import { AddressDTO } from './AddressDTO';
+import { AuthenticationDTO } from './AuthenticationDTO';
+import { BankAccountDTO } from './BankAccountDTO';
+
+export interface CreateUserRequestDTO {
+  username: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birthday: string;
+  role: string;
+  address?: AddressDTO;
+  bankAccount?: BankAccountDTO;
+  authentication: AuthenticationDTO;
+}
