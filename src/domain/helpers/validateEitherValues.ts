5e5806c60180450e99c5d44e3c183a8716a26e67
DavidSheltonSF <david.faria012@gmail.com>
2025-07-23 00:30:12 -0300

    feat(domain/helpers): add validateEitherValue function to validate an object of Either values

---
+++ src/domain/helpers/validateEitherValues.ts
@@ -0,0 +1,18 @@
+import { Either } from '../../shared/either';
+import { addressErrorType, userValidationErrorType } from '../errors/errorTypes';
+import { ValueObject } from '../value-objects/ValueObject';
+
+export function validateEitherValues(
+  validations: Record<string, Either<any, any>>
+): Either<userValidationErrorType | addressErrorType, Map<string, ValueObject>> {
+  const validValues: Map<string, ValueObject> = new Map();
+
+  for (const [k, v] of Object.entries(validations)) {
+    if (v.isLeft()) {
+      return Either.left(v.getLeft());
+    }
+    validValues.set(k, v.getRight());
+  }
+
+  return Either.right(validValues);
+}
