ce9c2d80e58a3d30e595ae4a06a00d6640fa02fb
DavidSheltonSF <david.faria012@gmail.com>
2025-07-23 02:04:40 -0300

    refactor(factories): enhance UserFactory, adding validation and tests

---
+++ src/domain/factories/UserFactory.test.ts
@@ -0,0 +1,41 @@
+import { UserFactory } from './UserFactory';
+
+describe('Testing UserFactory class', () => {
+  const user = {
+    username: 'David55',
+    name: 'David',
+    email: 'david@bugmai.com',
+    cpf: '14555774748',
+    role: 'customer',
+    phone: '21445854745',
+    birthday: '2000-03-02',
+    address: {
+      street: 'Das Dores',
+      city: 'Nova Iguaçu',
+      state: 'Rio de Janeiro',
+      zipCode: '45852258',
+    },
+    authentication: {
+      password: '3$rrreaRsrefesa22',
+    },
+  };
+
+  test('Should create an user with customer role', () => {
+
+    const userEntity = UserFactory.create(user).getRight();
+
+    expect(userEntity.username).toBe(user.username);
+    expect(userEntity.name).toBe(user.name);
+    expect(userEntity.email).toBe(user.email);
+    expect(userEntity.cpf).toBe(user.cpf);
+    expect(userEntity.role).toBe(user.role);
+    expect(userEntity.phone).toBe(user.phone);
+    expect(userEntity.birthday.getTime()).toBe(new Date(user.birthday).getTime());
+    expect(userEntity.email).toBe(user.email);
+    expect(userEntity.address.street).toBe(user.address.street);
+    expect(userEntity.address.city).toBe(user.address.city);
+    expect(userEntity.address.state).toBe(user.address.state);
+    expect(userEntity.address.zipCode).toBe(user.address.zipCode);
+
+  });
+});
--- src/domain/factories/UserFactory.ts
+++ src/domain/factories/UserFactory.ts
@@ -1,43 +1,73 @@
+import { CreateUserDTO } from '../../presentation/dtos/CreateUserDTO';
 import { Either } from '../../shared/either';
 import { CustomerUser } from '../entities/user/customer/CustomerUser';
-import { UserProps } from '../entities/user/UserProps';
+import {
+  addressErrorType,
+  authenticationErrorType,
+  userValidationErrorType,
+} from '../errors/errorTypes';
+import { validateUser } from '../helpers/validateUser';
+import {
+  Birthday,
+  Cpf,
+  Email,
+  Name,
+  Phone,
+  Role,
+  UserName,
+} from '../value-objects';
+import { ValueObject } from '../value-objects/ValueObject';
 import { AddressFactory } from './AddressFactory';
 import { AuthenticationFactory } from './AuthenticationFactory';
 
 export class UserFactory {
-  static create(userProps: UserProps): CustomerUser {
-    if (userProps.role === 'customer') {
-      const {
-        id,
-        username,
-        name,
-        email,
-        cpf,
-        phone,
-        role,
-        birthday,
-        address,
-        authentication,
-      } = userProps;
+  static create(
+    userDTO: CreateUserDTO
+  ): Either<
+    userValidationErrorType | addressErrorType | authenticationErrorType,
+    CustomerUser
+  > {
+    const validUserOrError = validateUser(userDTO);
 
-      const addressEntity = AddressFactory.create(address);
-      const authenticationEntity = AuthenticationFactory.create(
+    if (validUserOrError.isLeft()) {
+      return Either.left(validUserOrError.getLeft());
+    }
+
+    const validUser = validUserOrError.getRight();
+
+    const { email, role, address, authentication } = userDTO;
+
+    if (role === 'customer') {
+      const validAddressOrError = AddressFactory.create(address);
+      const validAuthenticationOrError = AuthenticationFactory.create(
         authentication,
         email
       );
 
-      return new CustomerUser(
-        username,
-        name,
-        email,
-        cpf,
-        phone,
-        role,
-        birthday,
-        addressEntity,
-        authenticationEntity,
-        id
+      if (validAddressOrError.isLeft()) {
+        return Either.left(validAddressOrError.getLeft());
+      }
+
+      if (validAuthenticationOrError.isLeft()) {
+        return Either.left(validAuthenticationOrError.getLeft());
+      }
+
+      const validAddress = validAddressOrError.getRight();
+      const validAuthentication = validAuthenticationOrError.getRight();
+
+      const customer = new CustomerUser(
+        validUser.get('username').getValue(),
+        validUser.get('name').getValue(),
+        validUser.get('email').getValue(),
+        validUser.get('cpf').getValue(),
+        validUser.get('phone').getValue(),
+        validUser.get('role').getValue(),
+        validUser.get('birthday').getValue(),
+        validAddress,
+        validAuthentication
       );
+
+      return Either.right(customer);
     }
   }
 }
