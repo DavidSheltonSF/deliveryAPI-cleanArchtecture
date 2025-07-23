2f4bd7e78158c8737b7677a8e86d7bb2d375f9ad
DavidSheltonSF <david.faria012@gmail.com>
2025-07-22 23:43:35 -0300

    refactor(AddressFactory): make it receives DTO and validate it before creating an Address

--- src/domain/factories/AddressFactory.ts
+++ src/domain/factories/AddressFactory.ts
@@ -1,12 +1,40 @@
+import { AddressDTO } from '../../presentation/dtos/AddressDTO';
+import { Either } from '../../shared/either';
 import { Address } from '../entities/address/Address';
-import { AddressProps } from '../entities/address/AddressProps';
+import { addressErrorType } from '../errors/errorTypes';
+import { AddressZipCode } from '../value-objects';
+import { AddressCity } from '../value-objects/AddressCity';
+import { AddressState } from '../value-objects/AddressState';
+import { AddressStreet } from '../value-objects/AddressStreet';
+import { ValueObject } from '../value-objects/ValueObject';
 
 export class AddressFactory {
-  static create(addressProps: AddressProps): Address {
-    const { street, city, state, zipCode } = addressProps;
+  static create(addressDTO: AddressDTO): Either<addressErrorType, Address> {
+    const { street, city, state, zipCode } = addressDTO;
 
-    const addressEntity = new Address(street, city, state, zipCode);
+    const validations = {
+      street: AddressStreet.create(street),
+      city: AddressCity.create(city),
+      state: AddressState.create(state),
+      zipCode: AddressZipCode.create(zipCode),
+    };
 
-    return addressEntity;
+    const validValues: Map<string, ValueObject> = new Map();
+
+    for (const [k, v] of Object.entries(validations)) {
+      if (v.isLeft()) {
+        return Either.left(v.getLeft());
+      }
+      validValues.set(k, v.getRight());
+    }
+
+    const addressEntity = new Address(
+      validValues.get('street').getValue(),
+      validValues.get('city').getValue(),
+      validValues.get('state').getValue(),
+      validValues.get('zipCode').getValue()
+    );
+
+    return Either.right(addressEntity);
   }
 }
