6cb463f35cf8848d3dfd87f63a3e62c303d49cf0
DavidSheltonSF <david.faria012@gmail.com>
2025-07-23 01:04:50 -0300

    refactor(domain/factories): remove validation responsability from AddressFactory using validateAddress function

--- src/domain/factories/AddressFactory.ts
+++ src/domain/factories/AddressFactory.ts
@@ -2,31 +2,17 @@ import { AddressDTO } from '../../presentation/dtos/AddressDTO';
 import { Either } from '../../shared/either';
 import { Address } from '../entities/address/Address';
 import { addressErrorType } from '../errors/errorTypes';
-import { AddressZipCode } from '../value-objects';
-import { AddressCity } from '../value-objects/AddressCity';
-import { AddressState } from '../value-objects/AddressState';
-import { AddressStreet } from '../value-objects/AddressStreet';
-import { ValueObject } from '../value-objects/ValueObject';
+import { validateAddress } from '../helpers/validateAddress';
 
 export class AddressFactory {
   static create(addressDTO: AddressDTO): Either<addressErrorType, Address> {
-    const { street, city, state, zipCode } = addressDTO;
+    const addressOrError = validateAddress(addressDTO);
 
-    const validations = {
-      street: AddressStreet.create(street),
-      city: AddressCity.create(city),
-      state: AddressState.create(state),
-      zipCode: AddressZipCode.create(zipCode),
-    };
-
-    const validValues: Map<string, ValueObject> = new Map();
-
-    for (const [k, v] of Object.entries(validations)) {
-      if (v.isLeft()) {
-        return Either.left(v.getLeft());
-      }
-      validValues.set(k, v.getRight());
+    if (addressOrError.isLeft()) {
+      return Either.left(addressOrError.getLeft());
     }
+
+    const validValues = addressOrError.getRight();
 
     const addressEntity = new Address(
       validValues.get('street').getValue(),
