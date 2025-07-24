d6688f27704b2f4d8d0c5f716a9e4422386361b9
DavidSheltonSF <david.faria012@gmail.com>
2025-07-23 23:08:41 -0300

    refactor(domain/helpers): make validateAddress function return Either<addressErrorType, AddressProps>

--- src/domain/helpers/validateAddress.ts
+++ src/domain/helpers/validateAddress.ts
@@ -1,5 +1,6 @@
 import { AddressDTO } from '../../presentation/dtos/AddressDTO';
 import { Either } from '../../shared/either';
+import { AddressProps } from '../entities/props/AddressProps';
 import { addressErrorType } from '../errors/errorTypes';
 import { AddressZipCode } from '../value-objects';
 import { AddressCity } from '../value-objects/AddressCity';
@@ -10,7 +11,7 @@ import { validateEitherValues } from './validateEitherValues';
 
 export function validateAddress(
   addressDTO: AddressDTO
-): Either<addressErrorType, Map<string, ValueObject>> {
+): Either<addressErrorType, AddressProps> {
   const { street, city, state, zipCode } = addressDTO;
 
   const validations = {
@@ -28,5 +29,12 @@ export function validateAddress(
 
   const validValues = validValuesOrError.getRight();
 
-  return Either.right(validValues);
+  const addressProps = {
+    street: validValues.get('street') as AddressStreet,
+    city: validValues.get('city') as AddressCity,
+    state: validValues.get('state') as AddressState,
+    zipCode: validValues.get('zipCode') as AddressZipCode,
+  };
+
+  return Either.right(addressProps);
 }
