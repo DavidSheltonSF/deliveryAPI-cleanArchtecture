cf3f3eb1b132c3031103fd309ead16dd7b847ebe
DavidSheltonSF <david.faria012@gmail.com>
2025-07-25 19:50:05 -0300

    fix(entities): add userId and createdAt properties to Authentication

--- src/domain/entities/Authentication.ts
+++ src/domain/entities/Authentication.ts
@@ -3,17 +3,23 @@ export class Authentication {
   private _email: string;
   private _passwordHash: string;
   private _sessionToken?: string;
+  private _userId: string;
+  private _createdAt?: Date;
 
   constructor(
     email: string,
     passwordHash: string,
+    userId: string,
     sessionToken?: string,
+    createdAt?: Date,
     id?: string
   ) {
     this._id = id;
     this._email = email;
     this._passwordHash = passwordHash;
     this._sessionToken = sessionToken;
+    this._createdAt = createdAt;
+    this._userId = userId;
   }
 
   get id(): string {
