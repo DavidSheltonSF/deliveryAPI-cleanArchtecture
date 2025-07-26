c460dedd84cd869ba183e9a57e6e94f8189de016
DavidSheltonSF <david.faria012@gmail.com>
2025-07-26 19:41:32 -0300

    refactor(AddressProps): make it use use raw data instead of value objects

--- src/domain/entities/props/AddressProps.ts
+++ src/domain/entities/props/AddressProps.ts
@@ -1,11 +1,6 @@
-import { AddressZipCode } from '../../value-objects';
-import { AddressCity } from '../../value-objects/AddressCity';
-import { AddressState } from '../../value-objects/AddressState';
-import { AddressStreet } from '../../value-objects/AddressStreet';
-
 export interface AddressProps {
-  street: AddressStreet;
-  city: AddressCity;
-  state: AddressState;
-  zipCode: AddressZipCode;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
 }
