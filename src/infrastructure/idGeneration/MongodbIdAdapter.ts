2c521b36d5be712890c25fa35261fbb1a553ad3c
DavidSheltonSF <david.faria012@gmail.com>
2025-07-25 22:49:49 -0300

    refactor(MongodbIdAdapter): make it implements IdService

--- src/infrastructure/idGeneration/MongodbIdAdapter.ts
+++ src/infrastructure/idGeneration/MongodbIdAdapter.ts
@@ -1,9 +1,17 @@
-ObjectId
-import { ObjectId } from "mongodb";
-import { IdGenerator } from "../../domain/contracts/IdGenerator";
+import { ObjectId } from 'mongodb';
+import { IdService } from '../../domain/contracts/IdService';
 
-export class MongodbIdAdapter implements IdGenerator {
+export class MongodbIdAdapter implements IdService {
   generate(): string {
     return new ObjectId().toString();
+  }
+
+  validate(id: string): boolean {
+    try {
+      new ObjectId(id);
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 }
