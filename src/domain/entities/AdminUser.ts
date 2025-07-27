8617e06795d73c90a1ba5cb909671d072f655ac4
DavidSheltonSF <david.faria012@gmail.com>
2025-07-26 22:31:29 -0300

    refactor(AdminUser): make it to be constructed with an AdminUserProps

--- src/domain/entities/AdminUser.ts
+++ src/domain/entities/AdminUser.ts
@@ -1,31 +1,12 @@
 import { Address } from './Address';
 import { Authentication } from './Authentication';
+import { AdminUserProps } from './props/AdminUserProps';
 import { User } from './User';
 
 export class AdminUser extends User {
   private _address: Address;
-  constructor(
-    username: string,
-    name: string,
-    email: string,
-    cpf: string,
-    phone: string,
-    role: string,
-    birthday: Date,
-    authentication: Authentication,
-    id?: string
-  ) {
-    super(
-      username,
-      name,
-      email,
-      cpf,
-      phone,
-      role,
-      birthday,
-      authentication,
-      id
-    );
+  constructor(admin: AdminUserProps) {
+    super(admin);
   }
 
   get address(): Address {
