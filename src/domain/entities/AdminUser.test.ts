d9bc1be94062daf7a175f89fc572e86526a7da5e
DavidSheltonSF <david.faria012@gmail.com>
2025-07-27 19:56:57 -0300

    refactor(AdminUser): make it receivers role in contructor

--- src/domain/entities/AdminUser.test.ts
+++ src/domain/entities/AdminUser.test.ts
@@ -1,8 +1,7 @@
-import { BcryptHasher } from '../../infrastructure/services/BcryptHasher';
 import { UserRole } from '../_enums';
-import { Address } from './Address';
 import { Authentication } from './Authentication';
 import { AdminUser } from './AdminUser';
+import { BcryptHasher } from '../../infrastructure/services/BcryptHasher';
 
 describe('Testing User entity', () => {
   const userProps = {
@@ -11,33 +10,64 @@ describe('Testing User entity', () => {
     email: 'jo@bugmail.com',
     cpf: '14555774778',
     phone: '21554744555',
-    role: UserRole.admin,
     birthday: new Date('2000-02-02'),
   };
 
-  const password = 'afdsfsdfaf'
   const authenticationProps = {
-    id: 'dsfsdfa',
-    userID: 'FMDSKFNASKFA',
     passwordHash: '',
+  };
+
+  const hasher = new BcryptHasher(12);
+
+  async function makeSutAdmin(
+    password: string = 'Drejkakn$%!2',
+    userRole: string = UserRole.admin
+  ) {
+    authenticationProps.passwordHash = await hasher.hash(password);
+    const authentication = new Authentication(authenticationProps, hasher);
+    const user = new AdminUser(userProps, userRole, authentication);
+    return user;
   }
 
-  test('Should be a valid AdminUser entity', async () => {
+  test('Should be a valid User entity', async () => {
+    const password = 'fdafsadfa';
+    const userRole = UserRole.admin;
+    const user = await makeSutAdmin(password, userRole);
 
-    const hasher = new BcryptHasher(12);
-    authenticationProps.passwordHash = await hasher.hash(password);
+    expect(user.username).toBe(userProps.username);
+    expect(user.name).toBe(userProps.name);
+    expect(user.email).toBe(userProps.email);
+    expect(user.cpf).toBe(userProps.cpf);
+    expect(user.phone).toBe(userProps.phone);
+    expect(user.role).toBe(userRole);
+    expect(user.birthday).toBe(userProps.birthday);
+    expect(await user.passwordIsValid(password)).toBeTruthy();
+  });
 
-    const authentication = new Authentication(authenticationProps, hasher);
+  test('Should set Id and createdAt properly', async () => {
+    const user = await makeSutAdmin();
+    const userId = 'id0316151';
+    const createdAt = new Date('2000-01-01');
 
-    const admin = new AdminUser(userProps, authentication);
+    user.setId(userId);
+    user.setCreatedAt(createdAt);
 
-    expect(admin.username).toBe(userProps.username);
-    expect(admin.name).toBe(userProps.name);
-    expect(admin.email).toBe(userProps.email);
-    expect(admin.cpf).toBe(userProps.cpf);
-    expect(admin.phone).toBe(userProps.phone);
-    expect(admin.role).toBe(userProps.role);
-    expect(admin.birthday).toBe(userProps.birthday);
-    expect(admin.passwordIsValid(password)).toBeTruthy();
+    expect(user.id).toBe(userId);
+    expect(user.createdAt).toBe(createdAt);
+  });
+
+  test('Should return error when trying to set Id or createdAt if they are alerady set', async () => {
+    const user = await makeSutAdmin();
+    const userId = 'id0316151';
+    const createdAt = new Date('2000-01-01');
+
+    user.setId(userId);
+    user.setCreatedAt(createdAt);
+
+    const idOrError = user.setId(userId);
+    const createdAtOrError = user.setCreatedAt(createdAt);
+
+    expect(idOrError.isLeft()).toBeTruthy();
+    expect(createdAtOrError.isLeft()).toBeTruthy();
   });
 });
--- src/domain/entities/AdminUser.ts
+++ src/domain/entities/AdminUser.ts
@@ -3,7 +3,7 @@ import { UserProps } from './props/UserProps';
 import { User } from './User';
 
 export class AdminUser extends User {
-  constructor(props: UserProps, authentication: Authentication) {
-    super(props, authentication);
+  constructor(props: UserProps, role: string, authentication: Authentication) {
+    super(props, role, authentication);
   }
 }
