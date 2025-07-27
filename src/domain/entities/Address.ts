6a17ccb04eecd8bb46b0acd758fd38bb59b3237a
DavidSheltonSF <david.faria012@gmail.com>
2025-07-27 16:39:36 -0300

    refactor(Address entity): unify properties inside props property

--- src/domain/entities/Address.ts
+++ src/domain/entities/Address.ts
@@ -1,58 +1,32 @@
 import { AddressProps } from './props/AddressProps';
 
 export class Address {
-  private readonly _id?: string;
-  private _street: string;
-  private _city: string;
-  private _state: string;
-  private _zipCode: string;
-
-  constructor(address: AddressProps) {
-    const { id, street, city, state, zipCode } = address;
-    this._id = id;
-    this._street = street;
-    this._city = city;
-    this._state = state;
-    this._zipCode = zipCode;
-  }
+  constructor(private props: AddressProps) {}
 
   get id(): string {
-    return this._id;
+    return this.props.id;
   }
 
   get street(): string {
-    return this._street;
+    return this.props.street;
   }
 
   get city(): string {
-    return this._city;
+    return this.props.city;
   }
 
   get state(): string {
-    return this._state;
+    return this.props.state;
   }
 
   get zipCode(): string {
-    return this._zipCode;
+    return this.props.zipCode;
   }
 
   update(address: Partial<AddressProps>) {
-    const { street, city, state, zipCode } = address;
-
-    if (street !== undefined) {
-      this._street = street;
-    }
-
-    if (city !== undefined) {
-      this._city = city;
-    }
-
-    if (state !== undefined) {
-      this._state !== undefined;
-    }
-
-    if (zipCode !== undefined) {
-      this._zipCode = zipCode;
-    }
+    this.props = {
+      ...this.props,
+      ...address,
+    };
   }
 }
