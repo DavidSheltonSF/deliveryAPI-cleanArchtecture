d50c6802845b1622afaab3e7b6a1472efdaf45a1
DavidSheltonSF <david.faria012@gmail.com>
2025-07-27 21:56:38 -0300

    refactor(application): rename UserUseCaseDTO to CreateUserUseCaseDTO

--- src/application/useCaseDtos/CreateUserUseCaseDTO.ts
+++ src/application/useCaseDtos/CreateUserUseCaseDTO.ts
@@ -1,11 +1,12 @@
 import { AddressUseCaseDTO } from './AddressUseCaseDTO';
 
-export interface UserUseCaseDTO {
-  id?: string;
+export interface CreateUserUseCaseDTO {
+  id: string;
   username: string;
   name: string;
   email: string;
   phone: string;
   role: string;
   address: AddressUseCaseDTO;
+  createdAt: string;
 }
