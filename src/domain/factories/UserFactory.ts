ae203be32703d34cb4f764efb41e5f4705ee2b9b
DavidSheltonSF <david.faria012@gmail.com>
2025-07-27 21:02:22 -0300

    refactor(UserFactory): make create method accept role, Authentication and Address as params

--- src/domain/factories/UserFactory.ts
+++ src/domain/factories/UserFactory.ts
@@ -1,27 +1,31 @@
-import { CustomerModel } from '../../infrastructure/models/mongodb/CustomerModel';
-import { AddressDTO } from '../../presentation/dtos/AddressDTO';
-import { CreateUserDTO } from '../../presentation/dtos/CreateUserDTO';
 import { UserRole } from '../_enums';
-import { Hasher } from '../contracts/Hasher';
 import { Address } from '../entities/Address';
 import { AdminUser } from '../entities/AdminUser';
 import { Authentication } from '../entities/Authentication';
 import { CustomerUser } from '../entities/CustomerUser';
 import { UserProps } from '../entities/props/UserProps';
-import { AddressFactory } from './AddressFactory';
-import { AuthenticationFactory } from './AuthenticationFactory';
 
 export class UserFactory {
-  static async create(user: UserProps): Promise<CustomerUser | AdminUser> {
-    const { email, role, address, authentication } = user;
+  static async create(
+    userProps: UserProps,
+    role: string,
+    authentication: Authentication,
+    address?: Address
+  ): Promise<CustomerUser | AdminUser> {
+    const { email } = userProps;
 
     let createdUser = undefined;
     switch (role) {
       case UserRole.customer:
-        createdUser = new CustomerUser(user);
+        createdUser = new CustomerUser(
+          userProps,
+          role,
+          address,
+          authentication
+        );
         break;
       case UserRole.admin:
-        createdUser = new AdminUser(user);
+        createdUser = new AdminUser(userProps, role, authentication);
         break;
     }
 
