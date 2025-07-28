57eacf627d883c78c6cf248467eabd94610dd4d9
DavidSheltonSF <david.faria012@gmail.com>
2025-07-28 18:43:08 -0300

    refactor: delete AdminUser

--- src/domain/entities/AdminUser.ts
+++
@@ -1,9 +0,0 @@
-import { Authentication } from './Authentication';
-import { UserProps } from './props/UserProps';
-import { User } from './User';
-
-export class AdminUser extends User {
-  constructor(props: UserProps, role: string, authentication: Authentication) {
-    super(props, role, authentication);
-  }
-}
