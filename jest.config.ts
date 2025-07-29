9f2ec28c81de01e84fbfb0c5818eb582d8343913
DavidSheltonSF <david.faria012@gmail.com>
2025-07-29 18:49:18 -0300

    chore: make jest focus on /src folder

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  roots: ['<rootDir>/src'],
   // Coleção de caminhos que serão acompanhados pelo Jest
   collectCoverageFrom: [
-    '<rootDir>/tests/**/*.ts',
-    '!<rootDir>/tests/main/**',
-    '!<rootDir>/tests/**/*-ports.ts',
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-ports.ts',
     '!**/ports/**',
-    '!**/tests/**',
-    '!**/config/**'
+    '!**/src/**',
+    '!**/config/**',
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
 
   // Permite o test de arquivos TypeScript
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest',
   },
-}
+};
