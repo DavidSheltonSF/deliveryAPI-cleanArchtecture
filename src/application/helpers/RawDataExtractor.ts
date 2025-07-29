f280ebf969298f35c1b1434dceae05aeb42b5f37
DavidSheltonSF <david.faria012@gmail.com>
2025-07-29 11:48:45 -0300

    feat(domain/helpers): add RawDataExtractor

---
+++ src/application/helpers/RawDataExtractor.ts
@@ -0,0 +1,36 @@
+import { RawAddressProps } from '../../domain/entities/rawProps/RawAddressProps';
+import { RawAuthenticationProps } from '../../domain/entities/rawProps/RawAuthenticationProps';
+import { RawUserProps } from '../../domain/entities/rawProps/RawUserProps';
+import { CreateUserDTO } from '../../presentation/dtos/CreateUserDTO';
+
+export class RawDataExtractor {
+  static extractUser(data: CreateUserDTO): RawUserProps {
+    const { username, name, email, cpf, phone, birthday } = data;
+    return {
+      username,
+      name,
+      email,
+      cpf,
+      phone,
+      birthday: new Date(birthday),
+    };
+  }
+
+  static extractAddess(data: CreateUserDTO): RawAddressProps {
+    const { street, city, state, zipCode } = data.address;
+    return {
+      street,
+      city,
+      state,
+      zipCode,
+    };
+  }
+
+  static extractAuthentication(data: CreateUserDTO): RawAuthenticationProps {
+    const { password, sessionToken } = data.authentication;
+    return {
+      password,
+      sessionToken
+    };
+  }
+}
