1caced111deaf9f6a2a1285a44f1dbfc234a3390
DavidSheltonSF <david.faria012@gmail.com>
2025-07-29 12:50:27 -0300

    refactor(Authentication): remove setId method

--- src/domain/entities/Authentication.ts
+++ src/domain/entities/Authentication.ts
@@ -1,8 +1,11 @@
 import { Either } from '../../shared/either';
 import { HashService } from '../contracts/HashService';
 import { PropertyAlreadySetError } from '../errors';
+import { authenticationErrorType } from '../errors/errorTypes';
+import { buildAuthenticationProps } from '../helpers/buildAuthenticationProps';
 import { Password } from '../value-objects';
 import { AuthenticationProps } from './props/AuthenticationProps';
+import { RawAuthenticationProps } from './rawProps/RawAuthenticationProps';
 
 export class Authentication {
   private _id?: string;
@@ -25,6 +28,21 @@ export class Authentication {
     this._createdAt = createdAt ?? new Date();
   }
 
+  static async create(
+    props: RawAuthenticationProps,
+    hasher: HashService,
+  ): Promise<Either<authenticationErrorType, Authentication>> {
+    const validPropsOrError = await buildAuthenticationProps(props, hasher);
+
+    if (validPropsOrError.isLeft()) {
+      return Either.left(validPropsOrError.getLeft());
+    }
+
+    const validProps = validPropsOrError.getRight();
+
+    return Either.right(new Authentication(validProps, hasher));
+  }
+
   get id(): string {
     return this._id;
   }
@@ -45,14 +63,6 @@ export class Authentication {
     return this._createdAt;
   }
 
-  setId(id: string): Either<PropertyAlreadySetError, string> {
-    if (this._id !== undefined) {
-      return Either.left(new PropertyAlreadySetError('id'));
-    }
-    this._id = id;
-    return Either.right(this._id);
-  }
-
   startSession(sessionToken: string) {
     this._sessionToken = sessionToken;
   }
@@ -61,11 +71,20 @@ export class Authentication {
     this._sessionToken = undefined;
   }
 
-  updatePasswordHash(passwordHash: string) {
-    this._passwordHash = passwordHash;
+  async updatePasswordHash(password: string) {
+    const passwordHashOrError = await Password.create(
+      password,
+      this._hashservice
+    );
+
+    if (passwordHashOrError.isLeft()) {
+      return Either.left(passwordHashOrError.getLeft());
+    }
+
+    this._passwordHash = passwordHashOrError.getRight();
   }
 
   async compare(password: string): Promise<boolean> {
-    return await this._hashservice.compare(password, this._passwordHash);
+    return await this._passwordHash.compare(password, this._hashservice);
   }
 }
