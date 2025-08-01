e0be2d442af4d5c00638d44f2ed28de47352d40b
DavidSheltonSF <david.faria012@gmail.com>
2025-07-31 23:31:28 -0300

    refactor(utils): simplify validateEitherValues making it receives an array of Either<any, any> and return Eithr<Error, boolean>

--- src/utils/validateEitherValues.ts
+++ src/utils/validateEitherValues.ts
@@ -1,24 +1,12 @@
 import { Either } from '../shared/either';
-import {
-  addressErrorType,
-  userValidationErrorType,
-} from '../domain/errors/errorTypes';
-import { ValueObject } from '../domain/value-objects/ValueObject';
 
 export function validateEitherValues(
-  validations: Record<string, Either<any, any>>
-): Either<
-  userValidationErrorType | addressErrorType,
-  Map<string, ValueObject>
-> {
-  const validValues: Map<string, ValueObject> = new Map();
-
-  for (const [k, v] of Object.entries(validations)) {
-    if (v.isLeft()) {
-      return Either.left(v.getLeft());
+  validations: Either<any, any>[]
+): Either<Error, boolean> {
+  for (const validation of validations) {
+    if (validation.isLeft()) {
+      return Either.left(validation.getLeft());
     }
-    validValues.set(k, v.getRight());
   }
-
-  return Either.right(validValues);
+  return Either.right(true);
 }
