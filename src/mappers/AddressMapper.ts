7922819ad0a15a8c76fda086aa21074c1bd67d87
DavidSheltonSF <david.faria012@gmail.com>
2025-07-31 23:13:40 -0300

    refactor(mappers): make AddressMapper.rawToProps return Either<addressErrorType, AddressProps>

--- src/mappers/AddressMapper.ts
+++ src/mappers/AddressMapper.ts
@@ -1,20 +1,30 @@
 import { AddressResponseDTO } from '../application/useCaseDtos/AddressResponseDTO';
 import { AddressProps } from '../domain/entities/props/AddressProps';
+import { addressErrorType } from '../domain/errors/errorTypes';
+import { AddressZipCode } from '../domain/value-objects';
 import { AddressModel } from '../infrastructure/models/mongodb/AddressModel';
 import { AddressDTO } from '../presentation/dtos/AddressDTO';
+import { Either } from '../shared/either';
 
 export class AddressMapper {
-  static rawToProps(addressDTO: AddressDTO): AddressProps {
+  static rawToProps(
+    addressDTO: AddressDTO
+  ): Either<addressErrorType, AddressProps> {
     const { street, city, state, zipCode } = addressDTO;
+
+    const zipCodeOrError = AddressZipCode.create(zipCode);
+    if (zipCodeOrError.isLeft()) {
+      return Either.left(zipCodeOrError.getLeft());
+    }
 
     const addressProps = {
       street: street,
       city: city,
       state: state,
-      zipCode: zipCode,
+      zipCode: zipCodeOrError.getRight(),
     };
 
-    return addressProps;
+    return Either.right(addressProps);
   }
 
   static modelToResponseDTO(addressModel: AddressModel): AddressResponseDTO {
