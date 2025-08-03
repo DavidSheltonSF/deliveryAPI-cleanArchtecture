c1d0b4b3fdb781fc75daa0e5d9ceaf1c1ff30c1f
DavidSheltonSF <david.faria012@gmail.com>
2025-08-03 18:41:40 -0300

    test(MongodbCustomerRepository): update test file for maching recent changes

--- src/infrastructure/repositories/mongodb/MongodbCustomerRepository.test.ts
+++ src/infrastructure/repositories/mongodb/MongodbCustomerRepository.test.ts
@@ -2,11 +2,15 @@ import { mongoHelper } from '../../../../src/infrastructure/repositories/mongodb
 import { config } from 'dotenv';
 import { MongodbCustomerRepository } from '../../../../src/infrastructure/repositories/mongodb/MongodbCustomerRepository';
 import { CustomerUser } from '../../../domain/entities/CustomerUser';
-import { RawDataExtractor } from '../../../application/helpers/RawDataExtractor';
 import { Address } from '../../../domain/entities/Address';
 import { makeMockHasher } from '../../../tests/mocks/mockHasher';
 import { Authentication } from '../../../domain/entities/Authentication';
-import { Role } from '../../../domain/_enums';
+import { AddressMapper } from '../../../mappers/AddressMapper';
+import { AuthenticationMapper } from '../../../mappers/AuthenticationMapper';
+import { UserMocker } from '../../../tests/mocks/UserMocker';
+import { AddressMocker } from '../../../tests/mocks/AddressMocker';
+import { AuthenticationMocker } from '../../../tests/mocks/AuthenticationMocker';
+import { CustomerMapper } from '../../../mappers/CustomerMapper';
 config();
 
 const repository = new MongodbCustomerRepository();
@@ -30,81 +34,51 @@ describe('Testing MongodbCustomerRepository', () => {
     await mongoHelper.clearCollection('users');
   });
 
-  const hasher = makeMockHasher();
-  const userRole = Role.customer;
-
-  const users = [
-    {
-      username: 'John58633',
-      name: 'John',
-      email: 'jojo@email.com',
-      cpf: '12587458567',
-      phone: '21585874788',
-      role: 'customer',
-      birthday: '2002-05-25',
-      address: {
-        street: 'Test Street',
-        city: 'Maringá',
-        state: 'Pará',
-        zipCode: '25874476',
-      },
-      authentication: {
-        password: 'D@41refsFFesfsfa',
-      },
-    },
-    {
-      username: 'Dane77',
-      name: 'Daniel',
-      email: 'daniel@email.com',
-      cpf: '12587458567',
-      phone: '23885874788',
-      role: 'customer',
-      birthday: '2002-04-25',
-      address: {
-        street: 'Test Street',
-        city: 'Maringá',
-        state: 'Pará',
-        zipCode: '25888476',
-      },
-      authentication: {
-        password: 'D@41refsFF22fsfa',
-      },
-    },
-  ];
-
-  test('should create a new customer in the repository', async () => {
+  async function makeSut() {
+    const hasher = makeMockHasher();
     const userCollection = mongoHelper.getCollection('users');
 
-    const createUserDTO = users[0];
-    const rawAddressProps = RawDataExtractor.extractAddess(createUserDTO);
-    const rawAutenticationProps =
-      RawDataExtractor.extractAuthentication(createUserDTO);
-    const rawUserProps = RawDataExtractor.extractUser(createUserDTO);
+    const userData = UserMocker.mockUserDTO();
+    const addressData = AddressMocker.mockAddressDTO();
+    const authData = AuthenticationMocker.mockAuthenticationDTO();
 
-    const addressOrError = Address.create(rawAddressProps);
-    const address = addressOrError.getRight();
-    const authenticationOrError = await Authentication.create(
-      rawAutenticationProps,
+    const createUserData = {
+      user: userData,
+      address: addressData,
+      authentication: authData,
+    };
+
+    const addressPropsOrError = AddressMapper.rawToProps(addressData);
+    const authPropsOrError = await AuthenticationMapper.rawToProps(
+      authData,
       hasher
     );
-    const authentication = authenticationOrError.getRight();
+    const userPropsOrError = CustomerMapper.rawToProps(userData);
 
-    const customerOrError = CustomerUser.create(
-      rawUserProps,
-      address,
-      authentication
-    );
-    const customer = customerOrError.getRight()
+    const addressProps = addressPropsOrError.getRight();
+    const authProps = authPropsOrError.getRight();
+    const userProps = userPropsOrError.getRight();
+
+    const address = Address.create(addressProps);
+    const authentication = Authentication.create(authProps, hasher);
+    const customer = CustomerUser.create(userProps, address, authentication);
+
+    return { customer, createUserData, hasher, userCollection };
+  }
+
+  test('should create a new customer in the repository', async () => {
+    const { customer, userCollection } = await makeSut();
 
     const newCustomer = await repository.create(customer);
 
-    if(newCustomer === null) {
+    if (newCustomer === null) {
       throw Error('User not created');
     }
 
     const id = newCustomer._id;
 
-    const foundCustomer =  await userCollection.findOne({_id: id})
+    const foundCustomer = await userCollection.findOne({ _id: id });
+    console.log(foundCustomer);
 
     expect(newCustomer.username).toBe(foundCustomer?.username);
     expect(newCustomer.name).toBe(foundCustomer?.name);
@@ -113,23 +87,4 @@ describe('Testing MongodbCustomerRepository', () => {
     expect(newCustomer.phone).toBe(foundCustomer?.phone);
     expect(newCustomer.role).toBe(foundCustomer?.role);
   });
-  // test('should create a new user in the repository', async () => {
-  //   const userCollection = mongoHelper.getCollection('users');
-
-  //   const user1 = users[0];
-  //   const user2 = users[1];
-
-  //   // Adding new users to database
-  //   await userCollection.insertOne(user1);
-  //   await userCollection.insertOne(user2);
-
-  //   const foundUser = await repository.findByEmail(user1.email);
-
-  //   expect(foundUser?.username).toBe(user1.username);
-  //   expect(foundUser?.name).toBe(user1.name);
-  //   expect(foundUser?.email).toBe(user1.email);
-  //   expect(foundUser?.cpf).toBe(user1.cpf);
-  //   expect(foundUser?.phone).toBe(user1.phone);
-  //   expect(foundUser?.role).toBe(user1.role);
-  // });
 });
