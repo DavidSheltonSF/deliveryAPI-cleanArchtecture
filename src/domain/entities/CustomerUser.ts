33205b526f96426fb7d8dc2bbd4ed9404424dc3e
DavidSheltonSF <david.faria012@gmail.com>
2025-08-03 20:11:21 -0300

    feat(domain): add CustomerUser.createFromPersistence method

--- src/domain/entities/CustomerUser.ts
+++ src/domain/entities/CustomerUser.ts
@@ -1,4 +1,6 @@
+import { UserModel } from '../../infrastructure/models/mongodb/UserModel';
 import { Role } from '../_enums';
+import { Birthday, Cpf, Email, Name, Phone, UserName } from '../value-objects';
 import { Address } from './Address';
 import { Authentication } from './Authentication';
 import { AddressProps } from './props/AddressProps';
@@ -22,6 +24,27 @@ export class CustomerUser extends User {
     authentication: Authentication
   ): CustomerUser {
     return new CustomerUser(props, address, authentication);
+  }
+
+  static createFromPersistence(
+    data: UserModel,
+    address: Address,
+    authentication: Authentication
+  ) {
+    const { _id, username, name, email, cpf, phone, birthday, createdAt } =
+      data;
+
+    const props = {
+      username: UserName.createFromPersistence(username),
+      name: Name.createFromPersistence(name),
+      email: Email.createFromPersistence(email),
+      cpf: Cpf.createFromPersistence(cpf),
+      phone: Phone.createFromPersistence(phone),
+      birthday: Birthday.createFromPersistence(birthday),
+    };
+
+    const customer = new CustomerUser(props, address, authentication, createdAt);
+    customer.id = _id
   }
 
   get address(): Address {
