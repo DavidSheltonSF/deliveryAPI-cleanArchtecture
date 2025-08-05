ffda77e342f213e39080cc3d306c07bd66f6b647
DavidSheltonSF <david.faria012@gmail.com>
2025-08-05 00:08:35 -0300

    feat(presentation/helpers): add MissingFieldsFinder

---
+++ src/presentation/controllers/helpers/MissingFieldsFinder.test.ts
@@ -0,0 +1,82 @@
+import { MissingFieldsFinder } from './MissingFieldsFinder';
+import { requiredFields } from './requiredFields';
+
+describe('Testing MissingFieldsFinder', () => {
+  test('should find all missing customer fields', () => {
+    const requiredFieldsCount = requiredFields.customer.length;
+    const requiredFieldsProvided = {
+      count: 2,
+      data: {
+        username: 'José',
+        name: 'Carlo',
+      },
+    };
+
+    const { data } = requiredFieldsProvided;
+    const missingFields = MissingFieldsFinder.checkCustomerFields(data);
+
+    expect(missingFields.length + requiredFieldsProvided.count).toBe(
+      requiredFieldsCount
+    );
+  });
+
+  test('should find all missing address fields', () => {
+    const requiredFieldsCount = requiredFields.address.length;
+    const requiredFieldsProvided = {
+      count: 2,
+      data: {
+        street: 'Fake  street',
+        state: 'Monte State',
+      },
+    };
+
+    const { data } = requiredFieldsProvided;
+    const missingFields = MissingFieldsFinder.checkAddressFields(data);
+
+    expect(missingFields.length + requiredFieldsProvided.count).toBe(
+      requiredFieldsCount
+    );
+  });
+
+  test('should find all missing authentication fields', () => {
+    const requiredFieldsCount = requiredFields.authentication.length;
+    const requiredFieldsProvided = {
+      length: 0,
+      data: {
+        sessionToken: 'fdafskgnasjdgfajsbngfag', //it's not a required field
+      },
+    };
+
+    const { data } = requiredFieldsProvided;
+    const missingFields = MissingFieldsFinder.checkAuthenticationFields(data);
+
+    expect(missingFields.length + requiredFieldsProvided.length).toBe(
+      requiredFieldsCount
+    );
+  });
+
+  test('should find all missing fields in createCustomer request', () => {
+    const requiredFieldsCount =
+      requiredFields.customer.length +
+      requiredFields.address.length +
+      requiredFields.authentication.length;
+
+    const requiredFieldsProvided = {
+      length: 2,
+      request: {
+        body: {
+          username: 'Joares',
+          birthday: '2000-01-01',
+        },
+      },
+    };
+
+    const { request } = requiredFieldsProvided;
+    const missingFields =
+      MissingFieldsFinder.checkCreateCustomerRequestFields(request);
+
+    expect(missingFields.length + requiredFieldsProvided.length).toBe(
+      requiredFieldsCount
+    );
+  });
+});
---
+++ src/presentation/controllers/helpers/MissingFieldsFinder.ts
@@ -0,0 +1,33 @@
+import { findMissingFields } from '../../../utils/findMissingFields';
+import { HttpRequest } from '../../_ports/http';
+import { requiredFields } from './requiredFields';
+
+export class MissingFieldsFinder {
+  static checkCustomerFields(data: Record<string, any>): Array<string> {
+    return findMissingFields(data, requiredFields.customer);
+  }
+
+  static checkAddressFields(data: Record<string, any>): Array<string> {
+    return findMissingFields(data, requiredFields.address);
+  }
+
+  static checkAuthenticationFields(data: Record<string, any>): Array<string> {
+    return findMissingFields(data, requiredFields.authentication);
+  }
+
+  static checkCreateCustomerRequestFields(request: HttpRequest) {
+    const data = request.body;
+
+    const missingCustomerFields = this.checkCustomerFields(data);
+    const missingAddressFields = this.checkAddressFields(data);
+    const missingAuthFields = this.checkAuthenticationFields(data);
+
+    const allMissingFields = [
+      ...missingCustomerFields,
+      ...missingAddressFields,
+      ...missingAuthFields,
+    ];
+
+    return allMissingFields;
+  }
+}
