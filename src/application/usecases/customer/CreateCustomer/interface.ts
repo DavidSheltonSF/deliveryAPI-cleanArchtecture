729cfc6248add160a6ee4e8a1463b6076611fe30
DavidSheltonSF <david.faria012@gmail.com>
2025-08-05 00:17:25 -0300

    fix(application): remove hasher param from CreateUser interface

--- src/application/usecases/customer/CreateCustomer/interface.ts
+++ src/application/usecases/customer/CreateCustomer/interface.ts
@@ -1,7 +1,6 @@
-import { Hasher } from '../../../../domain/contracts/Hasher';
 import { CreateUserDTO } from '../../../../presentation/dtos/CreateUserDTO';
 import { CreateCustomerResponse } from './response';
 
 export interface CreateUser {
-  execute: (customerDTO: CreateUserDTO, hasher: Hasher) => Promise<CreateCustomerResponse>;
+  execute: (customerDTO: CreateUserDTO) => Promise<CreateCustomerResponse>;
 }
