da7bce2b24945eecb5212ba043464d318cb159ef
DavidSheltonSF <david.faria012@gmail.com>
2025-08-05 00:07:44 -0300

    feat(presentation/helpers): add requiredFields object to provide a global access point to all required fields

---
+++ src/presentation/controllers/helpers/requiredFields.ts
@@ -0,0 +1,15 @@
+export const requiredFields = {
+  customer: [
+    'username',
+    'name',
+    'email',
+    'phone',
+    'role',
+    'cpf',
+    'birthday',
+    'address',
+    'authentication',
+  ],
+  address: ['street', 'city', 'state', 'zipCode'],
+  authentication: ['password'],
+};
