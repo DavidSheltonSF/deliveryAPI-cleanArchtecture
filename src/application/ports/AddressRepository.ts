72f8b4f3880e96ff345dcda35ba5ce0c610bf929
DavidSheltonSF <david.faria012@gmail.com>
2025-08-07 22:08:20 -0300

    refactor(repository): make all methods of AddressRepository return domain entities

--- src/application/ports/AddressRepository.ts
+++ src/application/ports/AddressRepository.ts
@@ -1,10 +1,9 @@
 import { Address } from '../../domain/entities/Address';
-import { AddressModel } from '../../infrastructure/models/mongodb/AddressModel';
 
 export interface AddressRepository {
-  findById: (id: string) => Promise<AddressModel | null>;
-  findByUserId: (userId: string) => Promise<AddressModel | null>;
-  create: (address: Address) => Promise<AddressModel>;
-  update: (address: Address) => Promise<AddressModel>;
-  delete: (id: string) => Promise<AddressModel | null>;
+  findById: (id: string) => Promise<Address | null>;
+  findByUserId: (userId: string) => Promise<Address | null>;
+  create: (address: Address) => Promise<Address>;
+  update: (address: Address) => Promise<Address>;
+  delete: (id: string) => Promise<Address | null>;
 }
--- src/infrastructure/repositories/mongodb/MongodbAddressRepository.ts
+++ src/infrastructure/repositories/mongodb/MongodbAddressRepository.ts
@@ -2,33 +2,55 @@ import { ObjectId } from 'mongodb';
 import { mongoHelper } from './helpers/mongo-helper';
 import { stringToObjectId } from './helpers/stringToObjectId';
 import { AddressRepository } from '../../../application/ports/AddressRepository';
-import { AddressModel } from '../../models/mongodb/AddressModel';
 import { AddressMapper } from '../../../mappers/AddressMapper';
 import { Address } from '../../../domain/entities/Address';
 
 export class MongodbAddressRepository implements AddressRepository {
-  async findById(id: string): Promise<AddressModel | null> {
+  async findById(id: string): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const addressId = new ObjectId(id);
     const foundUser = await addressCollection.findOne({ _id: addressId });
-    return AddressMapper.persistenceToModel(foundUser);
+    return Address.createFromPersistence({
+      _id: foundUser._id.toString(),
+      userId: foundUser.userId.toString(),
+      street: foundUser.street,
+      city: foundUser.city,
+      state: foundUser.state,
+      zipCode: foundUser.zipCode,
+      createdAt: foundUser.createdAt,
+    });
   }
 
-  async findByUserId(id: string): Promise<AddressModel | null> {
+  async findByUserId(id: string): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const userId = new ObjectId(id);
     const foundUser = await addressCollection.findOne({ userId });
-    return AddressMapper.persistenceToModel(foundUser);
+    return Address.createFromPersistence({
+      _id: foundUser._id.toString(),
+      userId: foundUser.userId.toString(),
+      street: foundUser.street,
+      city: foundUser.city,
+      state: foundUser.state,
+      zipCode: foundUser.zipCode,
+      createdAt: foundUser.createdAt,
+    });
   }
 
-  async findByEmail(email: string): Promise<AddressModel | null> {
+  async findByEmail(email: string): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const foundUser = await addressCollection.findOne({ email });
-
-    return AddressMapper.persistenceToModel(foundUser);
+    return Address.createFromPersistence({
+      _id: foundUser._id.toString(),
+      userId: foundUser.userId.toString(),
+      street: foundUser.street,
+      city: foundUser.city,
+      state: foundUser.state,
+      zipCode: foundUser.zipCode,
+      createdAt: foundUser.createdAt,
+    });
   }
 
-  async create(address: Address): Promise<AddressModel | null> {
+  async create(address: Address): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const AddressModel = AddressMapper.entityToModel(address);
     const addressData = {
@@ -44,14 +66,24 @@ export class MongodbAddressRepository implements AddressRepository {
       _id: newAddressId,
     });
 
+    console.log(createdAddress)
+
     if (createdAddress === null) {
       return null;
     }
 
-    return AddressMapper.persistenceToModel(createdAddress);
+    return Address.createFromPersistence({
+      _id: createdAddress._id.toString(),
+      userId: createdAddress.userId.toString(),
+      street: createdAddress.street,
+      city: createdAddress.city,
+      state: createdAddress.state,
+      zipCode: createdAddress.zipCode,
+      createdAt: createdAddress.createdAt,
+    });
   }
 
-  async update(address: Address): Promise<AddressModel | null> {
+  async update(address: Address): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const AddressModel = AddressMapper.entityToModel(address);
     const addreessId = AddressModel._id;
@@ -66,10 +98,18 @@ export class MongodbAddressRepository implements AddressRepository {
       return null;
     }
 
-    return AddressMapper.persistenceToModel(updatedAddress);
+    return Address.createFromPersistence({
+      _id: updatedAddress._id.toString(),
+      userId: updatedAddress.userId.toString(),
+      street: updatedAddress.street,
+      city: updatedAddress.city,
+      state: updatedAddress.state,
+      zipCode: updatedAddress.zipCode,
+      createdAt: updatedAddress.createdAt,
+    });
   }
 
-  async delete(id: string): Promise<AddressModel | null> {
+  async delete(id: string): Promise<Address | null> {
     const addressCollection = mongoHelper.getCollection('addresses');
     const addreessId = new ObjectId(id);
     const deletedAddress = await addressCollection.findOneAndDelete({
@@ -80,6 +120,14 @@ export class MongodbAddressRepository implements AddressRepository {
       return null;
     }
 
-    return AddressMapper.persistenceToModel(deletedAddress);
+    return Address.createFromPersistence({
+      _id: deletedAddress._id.toString(),
+      userId: deletedAddress.userId.toString(),
+      street: deletedAddress.street,
+      city: deletedAddress.city,
+      state: deletedAddress.state,
+      zipCode: deletedAddress.zipCode,
+      createdAt: deletedAddress.createdAt,
+    });
   }
 }
