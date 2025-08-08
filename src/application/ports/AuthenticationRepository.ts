f08c82fa92000a7faad9108421bac374c4b5222e
DavidSheltonSF <david.faria012@gmail.com>
2025-08-08 19:06:29 -0300

    refactor(domain/../repositories): make all find methods in AuthenticationRepository take hasher param

--- src/application/ports/AuthenticationRepository.ts
+++ src/application/ports/AuthenticationRepository.ts
@@ -1,10 +1,17 @@
+import { HashService } from '../../domain/contracts/HashService';
 import { Authentication } from '../../domain/entities/Authentication';
 
 export interface AuthenticationRepository {
-  findBySessionToken: (token: string) => Promise<Authentication | null>;
-  findById: (id: string) => Promise<Authentication | null>;
-  findByUserId: (userId: string) => Promise<Authentication | null>;
+  findBySessionToken: (
+    token: string,
+    hasher: HashService
+  ) => Promise<Authentication | null>;
+  findById: (id: string, hasher: HashService) => Promise<Authentication | null>;
+  findByUserId: (
+    userId: string,
+    hasher: HashService
+  ) => Promise<Authentication | null>;
   create: (auth: Authentication) => Promise<Authentication>;
   update: (auth: Authentication) => Promise<Authentication>;
-  delete: (id: string) => Promise<Authentication | null>;
+  delete: (id: string) => Promise<boolean>;
 }
