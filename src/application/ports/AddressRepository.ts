539252b9954247bba5d380f02c7dd808a609af9f
DavidSheltonSF <david.faria012@gmail.com>
2025-08-12 19:08:09 -0300

    fix(application/ports): make AddressRepository return a promise containing an AddressProps with id or null

--- src/application/ports/AddressRepository.ts
+++ src/application/ports/AddressRepository.ts
@@ -2,7 +2,7 @@ import { AddressProps } from "../../domain/entities/props/AddressProps";
 import { WithId } from "../../utils/types/WithId";
 
 export interface AddressRepository {
-  findById: (id: string) => Promise<null>;
+  findById: (id: string) => Promise<WithId<AddressProps> | null>;
   findByUserId: (userId: string) => Promise<WithId<AddressProps> | null>;
   create: (address: AddressProps) => Promise<WithId<AddressProps>>;
   update: (address: WithId<AddressProps>) => Promise<WithId<AddressProps>>;
