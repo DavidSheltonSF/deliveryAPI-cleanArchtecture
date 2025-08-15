1bf63bd6d0762891cd6541bbfa208de8d599b7c9
DavidSheltonSF <david.faria012@gmail.com>
2025-08-14 22:33:07 -0300

    chore(mappers): remove unused imports from AuthenticationMapper

--- src/mappers/AuthenticationMapper.ts
+++ src/mappers/AuthenticationMapper.ts
@@ -1,11 +1,5 @@
-import { HashService } from '../domain/contracts/HashService';
 import { AuthenticationProps } from '../domain/entities/props/AuthenticationProps';
-import { authenticationErrorType } from '../domain/errors/errorTypes';
-import { Password } from '../domain/value-objects';
-import { AuthenticationDTO } from '../presentation/dtos/AuthenticationDTO';
-import { Either } from '../shared/either';
 import { AuthenticationModel } from '../infrastructure/models/mongodb/AuthenticationModel';
-import { WithId } from '../utils/types/WithId';
 
 export class AuthenticationMapper {
 
