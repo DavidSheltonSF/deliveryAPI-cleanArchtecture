fa28016aa112fadec28a96bfa5c6fdbd59dece1b
DavidSheltonSF <david.faria012@gmail.com>
2025-08-14 22:57:48 -0300

    feat(mappers): add toResponse method in AddressMapper and CustomerMapper
    Added methods to prepare data to response

--- src/mappers/AddressMapper.ts
+++ src/mappers/AddressMapper.ts
@@ -1,9 +1,9 @@
+import { AddressResponseDTO } from '../application/useCaseDtos/AddressResponseDTO';
 import { AddressProps } from '../domain/entities/props/AddressProps';
 import { AddressModel } from '../infrastructure/models/mongodb/AddressModel';
+import { WithId } from '../utils/types/WithId';
 
 export class AddressMapper {
- 
-
   static propsToPersistence(address: AddressProps): AddressModel {
     const { userId, street, city, state, zipCode } = address;
 
@@ -14,6 +14,17 @@ export class AddressMapper {
       state,
       zipCode: zipCode.getValue(),
       createdAt: new Date(),
+    };
+  }
+
+  static toResponse(address: WithId<AddressProps>): AddressResponseDTO {
+    const { userId, street, city, state, zipCode } = address;
+
+    return {
+      street,
+      city,
+      state,
+      zipCode: zipCode.getValue(),
     };
   }
 }
--- src/mappers/CustomerMapper.ts
+++ src/mappers/CustomerMapper.ts
@@ -1,5 +1,8 @@
 import { UserModel } from '../infrastructure/models/mongodb/UserModel';
 import { UserProps } from '../domain/entities/props/UserProps';
+import { UserResponseDTO } from '../application/useCaseDtos/UserResponseDTO';
+import { dateToString } from '../utils/dateToString';
+import { WithId } from '../utils/types/WithId';
 
 export class CustomerMapper {
   static propsToPersistence(customer: UserProps): UserModel {
@@ -14,6 +17,22 @@ export class CustomerMapper {
       role,
       birthday: birthday.getValue(),
       createdAt: new Date(),
+    };
+  }
+
+  static toResponse(customer: WithId<UserProps>): UserResponseDTO {
+    const { id, firstName, lastName, email, phone, role, birthday } = customer;
+
+    const birthdayStr = dateToString(birthday.getValue());
+
+    return {
+      id,
+      firstName: firstName.getValue(),
+      lastName: lastName.getValue(),
+      email: email.getValue(),
+      phone: phone.getValue(),
+      role,
+      birthday: birthdayStr,
     };
   }
 }
