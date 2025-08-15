0d43d16e5043371a8a0d4f1557eb0b30a7f4ced2
DavidSheltonSF <david.faria012@gmail.com>
2025-08-15 18:46:00 -0300

    chore(dtos): remove unused imports from CreateAdminDTO.ts

--- src/presentation/dtos/CreateAdminDTO.ts
+++ src/presentation/dtos/CreateAdminDTO.ts
@@ -1,6 +1,4 @@
-import { AddressDTO } from './AddressDTO';
 import { AuthenticationDTO } from './AuthenticationDTO';
-import { DriverFieldsDTO } from './DriverFieldsDTO';
 import { UserDTO } from './UserDTO';
 
 export interface CreateAdminDTO {
