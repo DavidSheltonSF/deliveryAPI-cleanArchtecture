4bdbcb0e842bb3e5d369e92127b9846b4fde3915
DavidSheltonSF <david.faria012@gmail.com>
2025-08-18 23:29:34 -0300

    fix(application): make CustomerRepository.findById returns a promise containing a User with Id or null

--- src/application/ports/CustomerRepository.ts
+++ src/application/ports/CustomerRepository.ts
@@ -3,7 +3,7 @@ import { UserProps } from '../../domain/entities/props/UserProps';
 
 export interface CustomerRepository {
   findAll: () => Promise<WithId<UserProps>[]>;
-  findById: (id: string) => Promise<WithId<UserProps>>;
+  findById: (id: string) => Promise<WithId<UserProps> | null>;
   findByEmail: (email: string) => Promise<UserProps | null>;
   create: (customer: UserProps) => Promise<WithId<UserProps>>;
   update: (id: string, customer: UserProps) => Promise<WithId<UserProps>>;
