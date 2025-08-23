804f3d04403266a5f32ed8fb51940be6ecf2e002
DavidSheltonSF <david.faria012@gmail.com>
2025-08-22 23:18:36 -0300

    feat(application): add UserRepostory port

---
+++ src/application/ports/UserRepository.ts
@@ -0,0 +1,11 @@
+import { WithId } from '../../utils/types/WithId';
+import { UserProps } from '../../domain/entities/props/UserProps';
+
+export interface UserRepository {
+  findAll: () => Promise<WithId<UserProps>[]>;
+  findById: (id: string) => Promise<WithId<UserProps> | null>;
+  findByEmail: (email: string) => Promise<UserProps | null>;
+  create: (user: UserProps) => Promise<WithId<UserProps>>;
+  update: (id: string, email: UserProps) => Promise<WithId<UserProps>>;
+  delete: (id: string) => Promise<WithId<UserProps>>;
+}
