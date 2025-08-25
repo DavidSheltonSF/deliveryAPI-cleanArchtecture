b6f8b59e7732845ceacec48e8c8cec25b6024b57
DavidSheltonSF <david.faria012@gmail.com>
2025-08-25 18:46:45 -0300

    fix(application/ports): make finByEmail to return a user with id in CstomerRepository and UserRepository

--- src/application/ports/CustomerRepository.ts
+++ src/application/ports/CustomerRepository.ts
@@ -4,7 +4,7 @@ import { UserProps } from '../../domain/entities/props/UserProps';
 export interface CustomerRepository {
   findAll: () => Promise<WithId<UserProps>[]>;
   findById: (id: string) => Promise<WithId<UserProps> | null>;
-  findByEmail: (email: string) => Promise<UserProps | null>;
+  findByEmail: (email: string) => Promise<WithId<UserProps> | null>;
   create: (customer: UserProps) => Promise<WithId<UserProps>>;
   update: (id: string, customer: UserProps) => Promise<WithId<UserProps>>;
   delete: (id: string) => Promise<WithId<UserProps>>;
--- src/application/ports/UserRepository.ts
+++ src/application/ports/UserRepository.ts
@@ -4,7 +4,7 @@ import { UserProps } from '../../domain/entities/props/UserProps';
 export interface UserRepository {
   findAll: () => Promise<WithId<UserProps>[]>;
   findById: (id: string) => Promise<WithId<UserProps> | null>;
-  findByEmail: (email: string) => Promise<UserProps | null>;
+  findByEmail: (email: string) => Promise<WithId<UserProps> | null>;
   create: (user: UserProps) => Promise<WithId<UserProps>>;
   update: (id: string, email: UserProps) => Promise<WithId<UserProps>>;
   delete: (id: string) => Promise<WithId<UserProps>>;
