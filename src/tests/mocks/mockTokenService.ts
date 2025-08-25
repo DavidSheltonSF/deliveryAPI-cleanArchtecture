b3ca73429658f0d59a559b68d987dcd360b74666
DavidSheltonSF <david.faria012@gmail.com>
2025-08-25 18:52:16 -0300

    refactor(tests/mocks): simplify makeMockTokenService

--- src/tests/mocks/mockTokenService.ts
+++ src/tests/mocks/mockTokenService.ts
@@ -2,13 +2,8 @@ import { HashService } from '../../domain/contracts/HashService';
 import { TokenService } from '../../domain/contracts/TokenService';
 
 export const makeMockTokenService = (): TokenService => ({
-  sign: jest.fn((payload: Record<string, any>) => `mock-token_(${payload})`),
+  sign: jest.fn((payload: Record<string, any>) => JSON.stringify(payload)),
   verify: jest.fn((token: string) => {
-    const prefix = token.split('_')[0];
-    if (prefix !== 'mock-token') {
-      return null;
-    }
-
-    return { token };
+    return JSON.parse(token);
   }),
 });
