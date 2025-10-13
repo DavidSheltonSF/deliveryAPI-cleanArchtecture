cca4d944dda56d64dc012811e403295cbc26f429
DavidSheltonSF <david.faria012@gmail.com>
2025-10-12 21:51:48 -0300

    feat(mocks): add fakeDataBases .ts file to store fake  databases and add customerFakeData and addressFakeData to id

---
+++ src/tests/mocks/fakeDatabases.ts
@@ -0,0 +1,38 @@
+import {
+  AddressZipCode,
+  Birthday,
+  Cpf,
+  Email,
+  Name,
+  Password,
+  Phone,
+} from '../../domain/value-objects';
+import { UserProps } from '../../domain/entities/props/UserProps';
+import { Role } from '../../domain/_enums';
+import { WithId } from '../../utils/types/WithId';
+import { AddressProps } from '../../domain/entities/props/AddressProps';
+
+export const customerFakeData: WithId<UserProps>[] = [
+  {
+    id: 'mfakdsnfasnkfjdnskfma-fake',
+    firstName: Name.createFromPersistence('CustomerTest'),
+    lastName: Name.createFromPersistence('CustomerTest'),
+    email: Email.createFromPersistence('customer@email.com'),
+    cpf: Cpf.createFromPersistence('52144858745'),
+    phone: Phone.createFromPersistence('21965855574'),
+    role: Role.admin,
+    birthday: Birthday.createFromPersistence(new Date()),
+    passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
+  },
+];
+
+export const addressFakeDara: WithId<AddressProps>[] = [
+  {
+    id: 'fefasfdasfasdfad',
+    userId: 'sfsdafasdmflsaflasf',
+    street: 'Rua Teste',
+    city: 'São Paulo',
+    state: 'São Paulo',
+    zipCode: AddressZipCode.createFromPersistence('21145587'),
+  },
+];
