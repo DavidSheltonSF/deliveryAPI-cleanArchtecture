0066bf26733fbd1afd2932be8bb43d7ad9843226
DavidSheltonSF <david.faria012@gmail.com>
2025-10-16 15:52:13 -0300

    refactor(presentation errors): simplify the logic used to return a string list of missing fields in MissingFieldsError by usint the toString array method.

--- src/presentation/errors/MissingFieldsError.ts
+++ src/presentation/errors/MissingFieldsError.ts
@@ -2,17 +2,7 @@ import { ControllerError } from './controller-error'
 
 export class MissingFieldsError extends Error implements ControllerError {
   constructor (fields: string[]) {
-    let fieldsStr = "";
-   
-    fields.forEach((fields, i) => {
-      fieldsStr+=fields;
-      
-      if(i+1 < fields.length){
-        fieldsStr + ', '
-      }
-    })
-
-    super(`Missing: ${fieldsStr}`);
+    super(`Missing: ${fields.toString()}`);
     this.name = 'MissingFieldError';
   }
 }
