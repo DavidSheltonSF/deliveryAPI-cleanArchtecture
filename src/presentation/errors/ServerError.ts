69ecd37a150ee949a22de51c51b3b5078b584f99
DavidSheltonSF <david.faria012@gmail.com>
2025-10-17 17:57:25 -0300

    chore(error): modify the message of ServerError

--- src/presentation/errors/ServerError.ts
+++ src/presentation/errors/ServerError.ts
@@ -1,7 +1,7 @@
 import { ControllerError } from './controller-error';
 
 export class ServerError extends Error implements ControllerError {
-  constructor(message: string = 'Something went wrong') {
+  constructor(message: string = 'Internal server error') {
     super(message);
     this.name = 'ServerError';
   }
