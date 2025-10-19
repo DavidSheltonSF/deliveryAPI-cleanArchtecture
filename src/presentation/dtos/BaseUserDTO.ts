6bf0649a8b7905b7437a51adf9bff2f505692e0c
DavidSheltonSF <david.faria012@gmail.com>
2025-10-19 18:57:38 -0300

    fix(presentation): remove role property from BaseUserDTO to avoid type conflict, since UserDTO will intersect 'BaseUserDTO' with other type that already has 'role' field

--- src/presentation/dtos/BaseUserDTO.ts
+++ src/presentation/dtos/BaseUserDTO.ts
@@ -1,9 +1,6 @@
-import { UserRole } from '../../domain/_enums';
-
 export interface BaseUserDTO {
   firstName: string;
   lastName: string;
   email: string;
-  role: UserRole;
   password: string;
 }
