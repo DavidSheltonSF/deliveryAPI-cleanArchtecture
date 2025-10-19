23edaca6c3b8deedd35faca435ee76a647b586fe
DavidSheltonSF <david.faria012@gmail.com>
2025-10-18 22:39:12 -0300

    test(CreateCustomerController): use the new param of mockCustomerRepository to customize methods return for tests flexibility and remove unnecessary tests

--- src/presentation/controllers/customer/CreateCustomerController.test.ts
+++ src/presentation/controllers/customer/CreateCustomerController.test.ts
@@ -5,10 +5,13 @@ import { mockCustomerRepository } from '../../../tests/mocks/mockCustomerReposit
 import { mockAddressRepository } from '../../../tests/mocks/mockAddressRepository';
 import { UserMocker } from '../../../tests/mocks/UserMocker';
 import { AddressMocker } from '../../../tests/mocks/AddressMocker';
-import {customerFakeData, addressFakeDara} from '../../../tests/mocks/fakeDatabases'
+import {
+  customerFakeData,
+  addressFakeDara,
+} from '../../../tests/mocks/fakeDatabases';
+import { Email } from '../../../domain/value-objects';
 
 describe('Testing CreateCustomerController', () => {
-  
   async function makeSut() {
     const userDTO = UserMocker.mockUserDTO();
     const addressDTO = AddressMocker.mockAddressDTO();
@@ -17,6 +20,29 @@ describe('Testing CreateCustomerController', () => {
     const customerRepository = mockCustomerRepository(customerFakeData);
     const addressRepository = mockAddressRepository(addressFakeDara);
     const hasher = makeMockHasher();
+    const useCase = new CreateCustomerUseCase(
+      customerRepository,
+      addressRepository,
+      hasher
+    );
+    const createCustomerController = new CreateCustomerController(useCase);
+
+    return {
+      customerRepository,
+      addressRepository,
+      useCase,
+      createCustomerController,
+      hasher,
+      userDTO,
+    };
+  }
+
+  test('should return created (201) if everything went well', async () => {
+    const { addressRepository, hasher, userDTO } = await makeSut();
+
+    const customerRepository = mockCustomerRepository(customerFakeData, {
+      findByEmail: null,
+    });
 
     const useCase = new CreateCustomerUseCase(
       customerRepository,
@@ -25,85 +51,20 @@ describe('Testing CreateCustomerController', () => {
     );
 
     const createCustomerController = new CreateCustomerController(useCase);
-
-    return {
-      createCustomerController,
-      useCase,
-      customerRepository,
-      addressRepository,
-      hasher,
-      userDTO
-    };
-  }
-
-  test('should return created (201) if everything went well', async () => {
-    const { createCustomerController, userDTO } = await makeSut();
     const httpRequest = {
       body: userDTO,
     };
     const response = await createCustomerController.handle(httpRequest);
-    expect(response.statusCode).toBe(201)
+    expect(response.statusCode).toBe(201);
   });
 
-  
- test('should return bad request if no body is provided', async () => {
-   const { createCustomerController, userDTO } = await makeSut();
+  test('should return bad request if no body is provided', async () => {
+    const { createCustomerController } = await makeSut();
 
-   const httpRequest = {
-   };
+    const httpRequest = {};
 
-   const response = await createCustomerController.handle(httpRequest);
+    const response = await createCustomerController.handle(httpRequest);
 
-   expect(response.statusCode).toBe(400);
- });
-
-  // test('should call CustomerRepository.findByEmail with the provided email', async () => {
-  //   const { useCase, customerRepository } = await makeSut();
-
-  //   await useCase.execute(createUserDTO);
-  //   expect(customerRepository.findByEmail).toHaveBeenCalledWith(userDTO.email);
-  // });
-
-  // test('should call create method of all repositories', async () => {
-  //   const {
-  //     useCase,
-  //     customerRepository,
-  //     addressRepository,
-  //   } = await makeSut();
-
-  //   await useCase.execute(createUserDTO);
-
-  //   expect(customerRepository.create).toHaveBeenCalled();
-  //   expect(addressRepository.create).toHaveBeenCalled();
-  // });
-
-  // test('should return error when a duplicated email is found', async () => {
-  //   const duplicatedEmail = 'jojo@email.com';
-
-  //   const createCustomerData = {
-  //     ...createUserDTO,
-  //   };
-  //   createCustomerData.user.email = duplicatedEmail;
-
-  //   const {
-  //     customerRepository,
-  //     addressRepository,
-  //     hasher,
-  //   } = await makeSut();
-
-  //   // Modify the return of .findByEmail this time
-  //   (customerRepository.findByEmail as jest.Mock).mockResolvedValueOnce({
-  //     email: Email.createFromPersistence(duplicatedEmail),
-  //   });
-
-  //   const useCase = new CreateCustomerUseCase(
-  //     customerRepository,
-  //     addressRepository,
-  //     hasher
-  //   );
-
-  //   const responseOrError = await useCase.execute(createCustomerData);
-
-  //   expect(responseOrError.isLeft()).toBeTruthy();
-  // });
+    expect(response.statusCode).toBe(400);
+  });
 });
