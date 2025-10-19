8ce4f01a5993678efa42ee9637816f74eb29701e
DavidSheltonSF <david.faria012@gmail.com>
2025-10-18 22:24:14 -0300

    refactor(tests): improve flexibilidy mockCustomerRepository by adding an optional way to customize the returns of its methods

--- src/tests/mocks/mockCustomerRepository.ts
+++ src/tests/mocks/mockCustomerRepository.ts
@@ -2,15 +2,38 @@ import { CustomerRepository } from '../../application/ports/CustomerRepository';
 import { WithId } from '../../utils/types/WithId';
 import { UserProps } from '../../domain/entities/props/UserProps';
 
-export function mockCustomerRepository(fakeDataBase: WithId<UserProps>[]): CustomerRepository {
-  const customerRepository = {
-    findAll: jest.fn(async () => fakeDataBase),
-    findById: jest.fn(async (id: string) => fakeDataBase[0]),
-    findByEmail: jest.fn(async (email: string) => fakeDataBase[0]),
-    create: jest.fn(async (customer: UserProps) => fakeDataBase[0]),
-    update: jest.fn(async (id, customer: UserProps) => fakeDataBase[0]),
-    delete: jest.fn(async (id: string) => fakeDataBase[0]),
+interface MockRepoReturnConfig {
+  findAll?: WithId<UserProps>[];
+  findById?: WithId<UserProps> | null;
+  findByEmail?: WithId<UserProps> | null;
+  create?: WithId<UserProps> | null;
+  update?: WithId<UserProps> | null;
+  delete?: WithId<UserProps> | null;
+}
+
+export function mockCustomerRepository(
+  fakeDataBase: WithId<UserProps>[],
+  customReturn?: MockRepoReturnConfig 
+  
+): CustomerRepository {
+  const defaultConfig: MockRepoReturnConfig = {
+    findAll: fakeDataBase,
+    findById: fakeDataBase[0],
+    findByEmail: fakeDataBase[0],
+    create: fakeDataBase[0],
+    update: fakeDataBase[0],
+    delete: fakeDataBase[0],
   };
 
+  let returnConfig = {...defaultConfig, ...customReturn}
+
+  const customerRepository = {
+    findAll: jest.fn(async () => returnConfig.findAll),
+    findById: jest.fn(async (id: string) => returnConfig.findById),
+    findByEmail: jest.fn(async (email: string) => returnConfig.findByEmail),
+    create: jest.fn(async (customer: UserProps) => returnConfig.create),
+    update: jest.fn(async (id, customer: UserProps) => returnConfig.update),
+    delete: jest.fn(async (id: string) => returnConfig.delete),
+  };
   return customerRepository;
 }
