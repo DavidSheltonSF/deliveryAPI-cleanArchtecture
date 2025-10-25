66e0a2e851a5cc6ac9b580def089f9d95a88b899
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 17:16:18 -0300

    feat(tests): use  MockCreateCustomerUseCase to test CreateCusomerController istead of using a real Usecase implementation

--- src/presentation/controllers/customer/CreateCustomerController.test.ts
+++ src/presentation/controllers/customer/CreateCustomerController.test.ts
@@ -1,26 +1,22 @@
 import { CreateCustomerController } from './CreateCustomerController';
-import { CreateCustomerUseCase } from '../../../application/usecases/customer/CreateCustomer/CreateCustomerUseCase';
 import { makeMockHasher } from '../../../tests/mocks/mockHasher';
 import { mockCustomerRepository } from '../../../tests/mocks/mockCustomerRepository';
 import { mockAddressRepository } from '../../../tests/mocks/mockAddressRepository';
 import { UserMocker } from '../../../tests/mocks/UserMocker';
-import { AddressMocker } from '../../../tests/mocks/AddressMocker';
 import {
   customerFakeData,
   addressFakeDara,
 } from '../../../tests/mocks/fakeDatabases';
-import { Email } from '../../../domain/value-objects';
+import { MockedCreateCustomerUseCase } from '../../../tests/MockCreateCustomerUseCase';
 
 describe('Testing CreateCustomerController', () => {
   async function makeSut() {
-    const userDTO = UserMocker.mockUserDTO();
-    const addressDTO = AddressMocker.mockAddressDTO();
-    userDTO.address = addressDTO;
+    const customerDTO = UserMocker.mockCustomerDTO();
 
     const customerRepository = mockCustomerRepository(customerFakeData);
     const addressRepository = mockAddressRepository(addressFakeDara);
     const hasher = makeMockHasher();
-    const useCase = new CreateCustomerUseCase(
+    const useCase = new MockedCreateCustomerUseCase(
       customerRepository,
       addressRepository,
       hasher
@@ -33,18 +29,18 @@ describe('Testing CreateCustomerController', () => {
       useCase,
       createCustomerController,
       hasher,
-      userDTO,
+      customerDTO,
     };
   }
 
   test('should return created (201) if everything went well', async () => {
-    const { addressRepository, hasher, userDTO } = await makeSut();
+    const { addressRepository, hasher, customerDTO } = await makeSut();
 
     const customerRepository = mockCustomerRepository(customerFakeData, {
       findByEmail: null,
     });
 
-    const useCase = new CreateCustomerUseCase(
+    const useCase = new MockedCreateCustomerUseCase(
       customerRepository,
       addressRepository,
       hasher
@@ -52,10 +48,11 @@ describe('Testing CreateCustomerController', () => {
 
     const createCustomerController = new CreateCustomerController(useCase);
     const httpRequest = {
-      body: userDTO,
+      body: customerDTO,
     };
     const response = await createCustomerController.handle(httpRequest);
     expect(response.statusCode).toBe(201);
+    expect(useCase.calledWith(httpRequest.body)).toBeTruthy();
   });
 
   test('should return bad request if no body is provided', async () => {
