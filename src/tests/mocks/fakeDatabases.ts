ae859c597c52ab627481fc8b3e99b81d89e1f8c3
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 16:16:12 -0300

    refactor(fakeDatabases: make update customerFakeData to return CustomerProps

--- src/tests/mocks/fakeDatabases.ts
+++ src/tests/mocks/fakeDatabases.ts
@@ -7,12 +7,12 @@ import {
   Password,
   Phone,
 } from '../../domain/value-objects';
-import { UserProps } from '../../domain/entities/props/UserProps';
+import { CustomerProps } from '../../domain/entities/props/CustomerProps';
 import { UserRole } from '../../domain/_enums';
 import { WithId } from '../../utils/types/WithId';
 import { AddressProps } from '../../domain/entities/props/AddressProps';
 
-export const customerFakeData: WithId<UserProps>[] = [
+export const customerFakeData: WithId<CustomerProps>[] = [
   {
     id: 'mfakdsnfasnkfjdnskfma-fake',
     firstName: Name.createFromPersistence('CustomerTest'),
@@ -20,9 +20,15 @@ export const customerFakeData: WithId<UserProps>[] = [
     email: Email.createFromPersistence('customer@email.com'),
     cpf: Cpf.createFromPersistence('52144858745'),
     phone: Phone.createFromPersistence('21965855574'),
-    role: UserRole.admin,
+    role: UserRole.customer,
     birthday: Birthday.createFromPersistence(new Date()),
     passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
+    address: {
+      street: 'Rua Teste',
+      city: 'São Paulo',
+      state: 'São Paulo',
+      zipCode: AddressZipCode.createFromPersistence('21145587'),
+    },
   },
 ];
 
