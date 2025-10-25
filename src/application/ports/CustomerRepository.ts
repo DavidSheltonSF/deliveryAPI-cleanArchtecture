cc69b74ea39011078e498701b0c34b0a2bd7016f
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 16:01:18 -0300

    chore(application): update name of UserProps to CustomerProps in UserRepository and CustomerRepository interfaces

--- src/application/ports/CustomerRepository.ts
+++ src/application/ports/CustomerRepository.ts
@@ -1,11 +1,14 @@
 import { WithId } from '../../utils/types/WithId';
-import { UserProps } from '../../domain/entities/props/UserProps';
+import { CustomerProps } from '../../domain/entities/props/CustomerProps';
 
 export interface CustomerRepository {
-  findAll: () => Promise<WithId<UserProps>[]>;
-  findById: (id: string) => Promise<WithId<UserProps> | null>;
-  findByEmail: (email: string) => Promise<WithId<UserProps> | null>;
-  create: (customer: UserProps) => Promise<WithId<UserProps>>;
-  update: (id: string, customer: UserProps) => Promise<WithId<UserProps>>;
-  delete: (id: string) => Promise<WithId<UserProps>>;
+  findAll: () => Promise<WithId<CustomerProps>[]>;
+  findById: (id: string) => Promise<WithId<CustomerProps> | null>;
+  findByEmail: (email: string) => Promise<WithId<CustomerProps> | null>;
+  create: (customer: CustomerProps) => Promise<WithId<CustomerProps>>;
+  update: (
+    id: string,
+    customer: CustomerProps
+  ) => Promise<WithId<CustomerProps>>;
+  delete: (id: string) => Promise<WithId<CustomerProps>>;
 }
--- src/application/ports/UserRepository.ts
+++ src/application/ports/UserRepository.ts
@@ -1,11 +1,11 @@
 import { WithId } from '../../utils/types/WithId';
-import { UserProps } from '../../domain/entities/props/UserProps';
+import { CustomerProps } from '../../domain/entities/props/CustomerProps';
 
 export interface UserRepository {
-  findAll: () => Promise<WithId<UserProps>[]>;
-  findById: (id: string) => Promise<WithId<UserProps> | null>;
-  findByEmail: (email: string) => Promise<WithId<UserProps> | null>;
-  create: (user: UserProps) => Promise<WithId<UserProps>>;
-  update: (id: string, email: UserProps) => Promise<WithId<UserProps>>;
-  delete: (id: string) => Promise<WithId<UserProps>>;
+  findAll: () => Promise<WithId<CustomerProps>[]>;
+  findById: (id: string) => Promise<WithId<CustomerProps> | null>;
+  findByEmail: (email: string) => Promise<WithId<CustomerProps> | null>;
+  create: (user: CustomerProps) => Promise<WithId<CustomerProps>>;
+  update: (id: string, email: CustomerProps) => Promise<WithId<CustomerProps>>;
+  delete: (id: string) => Promise<WithId<CustomerProps>>;
 }
