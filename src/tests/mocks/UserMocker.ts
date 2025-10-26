83597aea68daa7ade4b5c2916f63e9fd03d59f26
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 21:15:26 -0300

    refactor(tests): add mockCustomer and mockCustomerWithId to UserMocker

--- src/tests/mocks/UserMocker.ts
+++ src/tests/mocks/UserMocker.ts
@@ -3,6 +3,16 @@ import { CustomerDTO } from '../../presentation/dtos/CustomerDTO';
 import { UserRole } from '../../domain/_enums';
 import { AddressMocker } from './AddressMocker';
 import { CustomerResponseDTO } from '../../application/useCaseDtos/CustomerResponseDTO';
+import { WithId } from '../../utils/types/WithId';
+import { CustomerProps } from '../../domain/entities/props/CustomerProps';
+import {
+  Birthday,
+  Cpf,
+  Email,
+  Name,
+  Password,
+  Phone,
+} from '../../domain/value-objects';
 
 export class UserMocker {
   static mockCustomerDTO(): CustomerDTO {
@@ -30,9 +40,40 @@ export class UserMocker {
       phone: '21547855887',
       birthday: '2000-01-02',
       role: faker.helpers.enumValue(UserRole),
-      address: AddressMocker.mockAddressDTO()
+      address: AddressMocker.mockAddressDTO(),
     };
 
     return customerDTO;
+  }
+
+  static mockCustomerProps(): CustomerProps {
+    const customerProps = {
+      firstName: Name.createFromPersistence('CustomerTest'),
+      lastName: Name.createFromPersistence('CustomerTest'),
+      email: Email.createFromPersistence('customer@email.com'),
+      cpf: Cpf.createFromPersistence('52144858745'),
+      phone: Phone.createFromPersistence('21965855574'),
+      role: UserRole.customer,
+      birthday: Birthday.createFromPersistence(new Date()),
+      passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
+    };
+
+    return customerProps;
+  }
+
+  static mockCustomerPropsWithId(): WithId<CustomerProps> {
+    const customerProps = {
+      id: 'mfakdsnfasnkfjdnskfma-fake',
+      firstName: Name.createFromPersistence('CustomerTest'),
+      lastName: Name.createFromPersistence('CustomerTest'),
+      email: Email.createFromPersistence('customer@email.com'),
+      cpf: Cpf.createFromPersistence('52144858745'),
+      phone: Phone.createFromPersistence('21965855574'),
+      role: UserRole.customer,
+      birthday: Birthday.createFromPersistence(new Date()),
+      passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
+    };
+
+    return customerProps;
   }
 }
