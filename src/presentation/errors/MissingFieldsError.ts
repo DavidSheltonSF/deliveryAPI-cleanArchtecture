c8d0460341be353dcf3c29a68259d21a80205e35
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 22:13:09 -0300

    refactor(MissingFieldsError): format the error message for better readabilidy

--- src/presentation/errors/MissingFieldsError.ts
+++ src/presentation/errors/MissingFieldsError.ts
@@ -2,7 +2,8 @@ import { ControllerError } from './controller-error'
 
 export class MissingFieldsError extends Error implements ControllerError {
   constructor (fields: string[]) {
-    super(`Missing: ${fields.toString()}`);
+    const fieldListStr = fields.toString().split(',').join(', ');
+    super(`Missing: ${fieldListStr}`);
     this.name = 'MissingFieldError';
   }
 }
