254ee09cee3ecc4e99cddb8b4b957d0f1fb5a47f
DavidSheltonSF <david.faria012@gmail.com>
2025-10-25 21:14:50 -0300

    refactor(tests): add mockAddress and mockAddressWithId to AddressMocker

--- src/tests/mocks/AddressMocker.ts
+++ src/tests/mocks/AddressMocker.ts
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker';
 import { AddressDTO } from '../../presentation/dtos/AddressDTO';
+import { WithId } from '../../utils/types/WithId';
+import { AddressProps } from '../../domain/entities/props/AddressProps';
+import { AddressZipCode } from '../../domain/value-objects';
 
 export class AddressMocker {
   static mockAddressDTO(): AddressDTO {
@@ -11,5 +14,32 @@ export class AddressMocker {
     };
 
     return AddressDTO;
+  }
+
+  static mockAddressProps(): AddressProps {
+    const addressProps: AddressProps = {
+      street: faker.location.streetAddress(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      zipCode: AddressZipCode.createFromPersistence(
+        faker.location.zipCode('########')
+      ),
+    };
+
+    return addressProps;
+  }
+
+  static mockAddressPropsWithId(): WithId<AddressProps> {
+    const addressProps: WithId<AddressProps> = {
+      id: 'dfdafdsafa',
+      street: faker.location.streetAddress(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      zipCode: AddressZipCode.createFromPersistence(
+        faker.location.zipCode('########')
+      ),
+    };
+
+    return addressProps;
   }
 }
