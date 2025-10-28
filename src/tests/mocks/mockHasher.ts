9fd7e09945e03237505ea73613d3a5f2b87dd917
DavidSheltonSF <david.faria012@gmail.com>
2025-10-28 15:43:25 -0300

    refactor(mocks): delete jest-based mockHasher, since it is not necessary anymore

--- src/tests/mocks/mockHasher.ts
+++
@@ -1,6 +0,0 @@
-import { HashService } from "../../domain/contracts/HashService"
-
-export const makeMockHasher = (): HashService => ({
-  hash: jest.fn(async (raw) => `mock-hash(${raw})`),
-  compare: jest.fn(async (raw, hash) => hash === `mock-hash(${raw})`),
-});
