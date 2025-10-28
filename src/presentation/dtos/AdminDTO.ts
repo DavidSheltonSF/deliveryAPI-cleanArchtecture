a25a561a4c8574fb15b7e8a8f868881fcd7e8dd7
DavidSheltonSF <david.faria012@gmail.com>
2025-10-28 13:01:36 -0300

    feat(presentation): add permissions: AdminPermissionDTO[] field to AdminPermissionDTO

--- src/presentation/dtos/AdminDTO.ts
+++ src/presentation/dtos/AdminDTO.ts
@@ -1,7 +1,10 @@
+import { AdminPermissionDTO } from "./AdminPermissionDTO";
+
 export interface AdminDTO {
   firstName: string;
   lastName: string;
   email: string;
   role: string;
+  permissions: AdminPermissionDTO[]
   password: string;
 }
