8c63c1b6121d0f5bd3d7d21c4aada0f7f7d72f6e
DavidSheltonSF <david.faria012@gmail.com>
2025-10-28 16:06:30 -0300

    test(mocks): add userRepository dependency to MockedCreateCustomerUseCase and update it to match recent changes

--- src/tests/MockCreateCustomerUseCase.ts
+++ src/tests/MockCreateCustomerUseCase.ts
@@ -1,18 +1,20 @@
 import { DuplicatedDataError } from '../application/errors';
 import { AddressRepository } from '../application/ports/AddressRepository';
 import { CustomerRepository } from '../application/ports/CustomerRepository';
+import { UserRepository } from '../application/ports/UserRepository';
 import { CreateUser } from '../application/usecases/customer/CreateCustomer/interface';
 import { CreateCustomerResponse } from '../application/usecases/customer/CreateCustomer/response';
 import { HashService } from '../domain/contracts/HashService';
 import { CustomerDTO } from '../presentation/dtos/CustomerDTO';
 import { Either } from '../shared/either';
-import { UserMocker } from './mocks/UserMocker';
+import { CustomerMocker } from './mocks/CustomerMocker';
 
 export class MockedCreateCustomerUseCase implements CreateUser {
   readonly executeParams: { customer: CustomerDTO } = {
     customer: null,
   };
   constructor(
+    private readonly userRepository: UserRepository,
     private readonly customerRepository: CustomerRepository,
     private readonly addressRepository: AddressRepository,
     private readonly hashService: HashService
@@ -20,14 +22,14 @@ export class MockedCreateCustomerUseCase implements CreateUser {
 
   async execute(customer: CustomerDTO): Promise<CreateCustomerResponse> {
     this.executeParams.customer = customer;
-    const mockCustomer = UserMocker.mockCustomerResponseDTO();
+    const mockCustomer = CustomerMocker.mockCustomerResponseDTO();
 
     return new Promise(async (resolve) => {
-      const existingCustomer = await this.customerRepository.findByEmail(
+      const existingUser = await this.userRepository.findByEmail(
         customer.email
       );
 
-      if (existingCustomer.email.getValue() === customer.email) {
+      if (existingUser.email.getValue() === customer.email) {
         resolve(
           Either.left(
             new DuplicatedDataError(
