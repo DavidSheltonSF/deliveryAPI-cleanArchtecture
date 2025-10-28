f961c88fb0d3c5750f2637ae0f1e3971b1cf8cce
DavidSheltonSF <david.faria012@gmail.com>
2025-10-28 16:05:11 -0300

    test(mocks): modify UserMocker structure to mock user generic data properly

--- src/tests/mocks/UserMocker.ts
+++ src/tests/mocks/UserMocker.ts
@@ -1,79 +1,32 @@
 import { faker } from '@faker-js/faker';
-import { CustomerDTO } from '../../presentation/dtos/CustomerDTO';
 import { UserRole } from '../../domain/_enums';
-import { AddressMocker } from './AddressMocker';
-import { CustomerResponseDTO } from '../../application/useCaseDtos/CustomerResponseDTO';
 import { WithId } from '../../utils/types/WithId';
-import { CustomerProps } from '../../domain/entities/props/CustomerProps';
-import {
-  Birthday,
-  Cpf,
-  Email,
-  Name,
-  Password,
-  Phone,
-} from '../../domain/value-objects';
+import { UserProps } from '../../domain/entities/props/UserProps';
+import { Email, Name, Password } from '../../domain/value-objects';
 
 export class UserMocker {
-  static mockCustomerDTO(): CustomerDTO {
-    const customerDTO: CustomerDTO = {
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      cpf: '14485874755',
-      phone: '21547855887',
-      birthday: '2000-01-02',
+  static mockUserProps(): UserProps {
+    const userProps = {
+      firstName: Name.createFromPersistence(faker.person.firstName()),
+      lastName: Name.createFromPersistence(faker.person.lastName()),
+      email: Email.createFromPersistence(faker.internet.email()),
       role: faker.helpers.enumValue(UserRole),
-      password: 'R$$osaini888dfa',
-      address: AddressMocker.mockAddressDTO(),
-    };
-
-    return customerDTO;
-  }
-
-  static mockCustomerResponseDTO(): CustomerResponseDTO {
-    const customerDTO: CustomerResponseDTO = {
-      id: faker.string.uuid(),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      phone: '21547855887',
-      birthday: '2000-01-02',
-      role: faker.helpers.enumValue(UserRole),
-      address: AddressMocker.mockAddressDTO(),
-    };
-
-    return customerDTO;
-  }
-
-  static mockCustomerProps(): CustomerProps {
-    const customerProps = {
-      firstName: Name.createFromPersistence('CustomerTest'),
-      lastName: Name.createFromPersistence('CustomerTest'),
-      email: Email.createFromPersistence('customer@email.com'),
-      cpf: Cpf.createFromPersistence('52144858745'),
-      phone: Phone.createFromPersistence('21965855574'),
-      role: UserRole.customer,
-      birthday: Birthday.createFromPersistence(new Date()),
       passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
     };
 
-    return customerProps;
+    return userProps;
   }
 
-  static mockCustomerPropsWithId(): WithId<CustomerProps> {
-    const customerProps = {
+  static mockUserPropsWithId(): WithId<UserProps> {
+    const userProps = {
       id: 'mfakdsnfasnkfjdnskfma-fake',
-      firstName: Name.createFromPersistence('CustomerTest'),
-      lastName: Name.createFromPersistence('CustomerTest'),
-      email: Email.createFromPersistence('customer@email.com'),
-      cpf: Cpf.createFromPersistence('52144858745'),
-      phone: Phone.createFromPersistence('21965855574'),
-      role: UserRole.customer,
-      birthday: Birthday.createFromPersistence(new Date()),
+      firstName: Name.createFromPersistence(faker.person.firstName()),
+      lastName: Name.createFromPersistence(faker.person.lastName()),
+      email: Email.createFromPersistence(faker.internet.email()),
+      role: faker.helpers.enumValue(UserRole),
       passwordHash: Password.createFromPersistence('fakeHas4$$%@$2hemkqmrkq'),
     };
 
-    return customerProps;
+    return userProps;
   }
 }
