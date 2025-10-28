aef40ebd6a99e6df357775a09d1f7faf563bb41f
DavidSheltonSF <david.faria012@gmail.com>
2025-10-28 13:06:11 -0300

    refactor(infrastructure): remove cpf, phone nd birthday fields from UserModel

--- src/infrastructure/models/mongodb/UserModel.ts
+++ src/infrastructure/models/mongodb/UserModel.ts
@@ -2,10 +2,7 @@ export interface UserModel {
   firstName: string;
   lastName: string;
   email: string;
-  cpf: string;
-  phone: string;
   role: string;
-  birthday: Date;
   passwordHash: string
   createdAt: Date;
 }
